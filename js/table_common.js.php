17c57e9395a6c3beeccdbdc2fe3fc4f09e254895
Raul Perusquia <rulovico@gmail.com>
2008-10-09 22:17:38 +0100

    add table_common.js.php to branch

---
+++ js/table_common.js.php
@@ -0,0 +1,91 @@
+
+
+var remove_filter= function (tableid){
+    var Dom   = YAHOO.util.Dom;
+    Dom.get('f_input'+tableid).value='';
+    var table=tables['table'+tableid]
+
+    var datasource=tables['dataSorce'+tableid];
+    table.filter.value=Dom.get('f_input0').value;
+    var request='&f_field=' +table.filter.key + '&f_value=' + table.filter.value;
+    datasource.sendRequest(request,table.onDataReturnInitializeTable, table);       
+}
+
+var myRowsPerPageDropdown = function(){return true};
+var mydoBeforeSortColumn = function(){return true};
+var mydoBeforePaginatorChange = function(){return true};
+var myhandleDataReturnPayload= function(oRequest, oResponse, oPayload) {
+    oPayload = oPayload ||  {
+	totalRecords:0,
+	pagination:{
+	    rowsPerPage:oResponse.meta.rowsPerPage
+	},
+	sortedBy:
+	{key:oResponse.meta.sort_key,
+	 dir:oResponse.meta.sort_dir},
+	SelectedRows:null,
+	SelectedCells:null} ;
+
+    oPayload.filter_msg=oResponse.meta.filter_msg;
+
+
+	
+    YAHOO.util.Dom.get('filter_msg'+oResponse.meta.tableid).innerHTML=oPayload.filter_msg
+    //    oPayload.sortedBy.key=oResponse.meta.sort_key;
+    //oPayload.sortedBy.dir=oResponse.meta.sort_dir;
+    oPayload.totalRecords = parseInt(oResponse.meta.totalRecords);
+    return oPayload;
+};
+
+var myRequestBuilder = function(oState, oSelf) {
+    // Get states or use defaults
+
+    
+    oState = oState || {pagination:null, sortedBy:null};
+
+    var sort = (oState.sortedBy) ? oState.sortedBy.key : "myDefaultColumnKey";
+
+    var dir = (oState.sortedBy && oState.sortedBy.dir === YAHOO.widget.DataTable.CLASS_ASC) ? "" : "desc";
+
+   var startIndex = (oState.pagination) ? oState.pagination.recordOffset : 0;
+    var results = (oState.pagination) ? oState.pagination.rowsPerPage : 5;
+
+    // Build custom request
+    var request= "&o=" + sort +
+    "&od=" + dir +
+    "&sf=" + startIndex +
+    "&nr=" + results;
+
+    return request;
+};
+
+
+var mygetTerms =function (query) {
+    var Dom   = YAHOO.util.Dom;
+    var table=tables.table0;
+    var datasource=tables.dataSorce0;
+    table.filter.value=Dom.get('f_input0').value;
+    var request='&f_field=' +table.filter.key + '&f_value=' + table.filter.value;
+    datasource.sendRequest(request,table.onDataReturnInitializeTable, table);       
+};
+
+// var myFilterChangeValue = function(e,o){
+    
+//     if(o.table.filter.value!=this.value ){
+// 	var current_time=new Date().getTime();
+// 	if(current_time-o.table.filter.lastRequest>200){
+	    
+// 	    o.table.filter.value=this.value;
+// 	    var oCallback = {
+// 		success : o.table.onDataReturnInitializeTable,
+// 		failure : o.table.onDataReturnInitializeTable,
+// 		scope : o.table
+// 	    };
+// 	    var request="&f_field="+o.table.filter.key+"&f_value="+o.table.filter.value
+// 		o.datasource.sendRequest(request, oCallback);
+// 	    o.table.filter.lastRequest=current_time;
+// 	}
+
+//     }
+//     return true
+// }
