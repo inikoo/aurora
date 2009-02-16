6c661374fc4ce42c7bb0a94dcd0dca36d3f576e0
Raul Perusquia <rulovico@gmail.com>
2009-02-16 15:21:52 +0000

    idem

--- mantenence/scripts/Calculate_Products_Sales.php
+++ mantenence/scripts/Calculate_Products_Sales.php
@@ -24,26 +24,64 @@ require_once '../../myconf/conf.php';
 date_default_timezone_set('Europe/London');
 
 
-$sql="select * from `Product Dimension`";
+//$sql="select * from `Product Dimension` where `Product Code`='FO-A1'";
+$sql="select * from `Product Dimension` ";
 $result=mysql_query($sql);
 while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
   $product=new Product($row['Product Key']);
   $product->load('sales');
 
-
+  
+  $sql=sprintf("select * from `Product Dimension` where `Product Code`=%s order by `Product Valid From` limit 1",prepare_mysql($row['Product Code']));
+  $result2=mysql_query($sql);
+  if($row2=mysql_fetch_array($result2, MYSQL_ASSOC)){
+    $same_code_from=$row2['Product Valid From'];
+  }
   $sql=sprintf("select * from `Product Dimension` where `Product Code`=%s order by `Product Valid To` desc",prepare_mysql($row['Product Code']));
   $most_recent='Yes';
   $result2=mysql_query($sql);
   while($row2=mysql_fetch_array($result2, MYSQL_ASSOC)){
-    if($most_recent=='Yes')
+    if($most_recent=='Yes'){
       $most_recent_key=$row2['Product Key'];
-    
-    $sql=sprintf("upadate `Product Dimension` where `Product Same Code Most Recent`=%s,`Product Same Code Most Recent`=%s  where `Product Key`=%s ",$row['Product Key'],prepare_mysql($most_recent),$most_recent_key,$row2['Product Key']);
+      $same_code_to=$row2['Product Valid To'];
+    }
+    $sql=sprintf("update `Product Dimension` set  `Product Same Code Valid From`=%s ,`Product Same Code Valid To`=%s , `Product Same Code Most Recent Key`=%s,`Product Same Code Most Recent`=%s  where `Product Key`=%s ",prepare_mysql($same_code_from),prepare_mysql($same_code_to),$row['Product Key'],prepare_mysql($most_recent),$most_recent_key,$row2['Product Key']);
+    //   print "$sql\n\n";
     mysql_query($sql);
      if($most_recent=='Yes')
        $most_recent=='No';
 
   }
+
+
+
+
+  $sql=sprintf("select * from `Product Dimension` where `Product ID`=%s order by `Product Valid From` limit 1",prepare_mysql($row['Product ID']));
+  
+  $result2=mysql_query($sql);
+  if($row2=mysql_fetch_array($result2, MYSQL_ASSOC)){
+    $same_code_from=$row2['Product Valid From'];
+  }else
+    exit("caca1");
+  $sql=sprintf("select * from `Product Dimension` where `Product ID`=%s order by `Product Valid To` desc limit 1",prepare_mysql($row['Product ID']));
+  
+
+  $most_recent='Yes';
+  $result2=mysql_query($sql);
+  if($row2=mysql_fetch_array($result2, MYSQL_ASSOC)){
+    $same_code_to=$row2['Product Valid To'];
+  }else
+    exit("caca2");
+  $sql=sprintf("update `Product Dimension` set  `Product Same ID Valid From`=%s ,`Product Same ID Valid To`=%s  where `Product Key`=%s ",prepare_mysql($same_code_from),prepare_mysql($same_code_to),$row['Product Key'],$row2['Product Key']);
+  // print "$sql\n\n";
+  mysql_query($sql);
+
+
+ 
+
+
+
+
     
 
 
--- mantenence/scripts/Read_Old_inventory.php
+++ mantenence/scripts/Read_Old_inventory.php
@@ -22,36 +22,111 @@ mysql_query("SET time_zone ='UTC'");
 mysql_query("SET NAMES 'utf8'");
 require_once '../../myconf/conf.php';           
 date_default_timezone_set('Europe/London');
+$not_found=00;
 
+$sql="delete from  `Inventory Transition Fact` where Inventory Transaction Type='Audit' ";
+mysql_query($sql);
 
-$sql="select * from aw_old.in_out left join product on (product.id=product_id) order by product,date limit 100";
+$sql="select code,product_id,aw_old.in_out.date,aw_old.in_out.tipo,aw_old.in_out.quantity from aw_old.in_out left join aw_old.product on (product.id=product_id) where product.code is not null and (aw_old.in_out.tipo=2 or aw_old.in_out.tipo=1)order by product.id,date ";
 $result=mysql_query($sql);
 while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
   
   $date=$row['date'];
   $code=$row['code'];
-  
-
-  $sql=sprintf("select `Product Key` from `Product Dimension` where `Product Code`=%s and `Product Valid From`<=%s and `Product Valid To`=%s order by `Product Valid To` desc ",prepare_mysql($code),prepare_mysql($date),prepare_mysql($date));
+  $tipo=$row['tipo'];
+  $qty=$row['quantity'];
+  $sql=sprintf("select `Product ID` from `Product Dimension` where `Product Most Recent`='Yes' and  `Product Code`=%s and `Product Same ID Valid From`<=%s and `Product Same ID Valid To`>=%s order by `Product Same ID Valid To` desc ",prepare_mysql($code),prepare_mysql($date),prepare_mysql($date));
   $result2=mysql_query($sql);
+  // print "$sql\n";
   if($row2=mysql_fetch_array($result2, MYSQL_ASSOC)   ){
     $product_ID=$row2['Product ID'];
-  }else
-    exit("error 1");
 
+    $sql=sprintf("select `Part SKU`,`Parts Per Product` from `Product Part List` where `Product ID`=%s  ",prepare_mysql($product_ID));
+    // print "$sql\n";
+    $result2=mysql_query($sql);
+    $num = mysql_num_rows($result2);
+    if($num!=1)
+      exit ("no ideal product");
+    
+    if($row2=mysql_fetch_array($result2, MYSQL_ASSOC)   ){
+      $part_sku=$row2['Part SKU'];
+      $parts_per_product=$row2['Parts Per Product'];
+    }
+    
+    
+    $cost_per_part=get_cost($part_sku,$date);
+    
+   
+    //print "$code $date $part_sku\n "; 
+    
+    if($tipo==2){
+      $sql=sprintf("insert into `Inventory Transition Fact` (`Date`,`Part SKU`,`Inventory Transaction Type`,`Inventory Transaction Quantity`,`Inventory Transaction Amount`) values (%s,%s,'Audit',%s,%s)",prepare_mysql($date),prepare_mysql($part_sku),prepare_mysql($qty*$parts_per_product),prepare_mysql($cost_per_part*$qty*$parts_per_product));
+      //  print "$sql\n";
+      if(!mysql_query($sql))
+	exit("$sql can into insert Inventory Transition Fact ");
+    }else{
+      
+      
+      $sql=sprintf("insert into `Inventory Transition Fact` (`Date`,`Part SKU`,`Inventory Transaction Type`,`Inventory Transaction Quantity`,`Inventory Transaction Amount`) values (%s,%s,'In',%s,%s)",prepare_mysql($date),prepare_mysql($part_sku),prepare_mysql($qty*$parts_per_product),prepare_mysql($cost_per_part*$qty*$parts_per_product));
+      // print "$sql\n";
+      if(!mysql_query($sql))
+	exit("$sql can into insert Inventory Transition Fact ");
+
+
+    }
+
+  }else{
+    //print "$sql\n";
+
+   $not_found++;
+   print "$code not found\r";
+
+  }
   
-   $sql=sprintf("select `Part SKU` from `Product Part List` where `Product ID`=%s  ",prepare_mysql($product_ID));
-  $result2=mysql_query($sql);
-  if($row2=mysql_fetch_array($result2, MYSQL_ASSOC)   ){
-    $part_sku=$row2['Part SKU'];
-  }else
-    exit("error 1");
-  
+
 
  }
 
+print "\n$not_found\n";
+
+// select SPD.`Supplier Product ID` from `Supplier Product Dimension` SPD left join `Supplier Product Part List` SPPL on (SPD.`Supplier Product ID`=SPPL.`Supplier Product ID`) left join `Product Part List` PPL on (SPPL.`Part SKU`=PPL.`Part SKU`) where `Product ID`=1;
+
+
+
+function get_cost($part_sku,$date){
+
+
+   $sql=sprintf(" select AVG(SPD.`Supplier Product Cost` * SPPL.`Supplier Product Units Per Part`) as cost from `Supplier Product Dimension` SPD left join `Supplier Product Part List` SPPL on (SPD.`Supplier Product ID`=SPPL.`Supplier Product ID`)  where `Part SKU`=%s  and `Supplier Product Valid To`>=%s and  `Supplier Product Valid From`<=%s    ",prepare_mysql($part_sku),prepare_mysql($date),prepare_mysql($date));
+   // print "\n\n\n\n$sql\n";
+   $result=mysql_query($sql);
+  if($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+    if(is_numeric($row['cost']))
+    return $row['cost'];
+  }
+
+
+  $sql=sprintf(" select AVG(SPD.`Supplier Product Cost` * SPPL.`Supplier Product Units Per Part`) as cost from `Supplier Product Dimension` SPD left join `Supplier Product Part List` SPPL on (SPD.`Supplier Product ID`=SPPL.`Supplier Product ID`)  where `Part SKU`=%s  and `Supplier Product Valid To`<=%s limit 1 ",prepare_mysql($part_sku),prepare_mysql($date));
+
+ $result=mysql_query($sql);
+  if($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+    if(is_numeric($row['cost']))
+      return $row['cost'];
+  }
+
+ $sql=sprintf(" select AVG(SPD.`Supplier Product Cost` * SPPL.`Supplier Product Units Per Part`) as cost from `Supplier Product Dimension` SPD left join `Supplier Product Part List` SPPL on (SPD.`Supplier Product ID`=SPPL.`Supplier Product ID`)  where `Part SKU`=%s  order by  `Supplier Product Valid To` desc ",prepare_mysql($part_sku),prepare_mysql($date));
+ // print "\n\n\n\n$sql\n";
+ $result=mysql_query($sql);
+  if($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+    if(is_numeric($row['cost']))
+      return $row['cost'];
+  }
+
+
+  exit("error can no found supp ciost\n");
+
+
+}
 
 
 ?>
 
-// select SPD.`Supplier Product ID` from `Supplier Product Dimension` SPD left join `Supplier Product Part List` SPPL on (SPD.`Supplier Product ID`=SPPL.`Supplier Product ID`) left join `Product Part List` PPL on (SPPL.`Part SKU`=PPL.`Part SKU`) where `Product ID`=1;
