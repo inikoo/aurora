620cc5a1967608e55f4c9f57c4b0153343ed0885
Raul Perusquia <rulovico@gmail.com>
2009-04-27 22:15:29 +0100

    small changes to cal part stock

--- mantenence/scripts/Calculate_Part_Sales.php
+++ mantenence/scripts/Calculate_Part_Sales.php
@@ -85,18 +85,12 @@ while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
   }
 
   $part->load('sales');
-  
-    $part->load('used in');
-
-    $part->load('supplied by');
-    
-
-    
-    $part->load('stock');
-        $part->load('stock_history');
-
-    $part->load('future costs');
-    print $row['Part Key']."\r";
+  $part->load('used in');
+  $part->load('supplied by');
+  $part->load('stock');
+  $part->load('stock_history');
+  $part->load('future costs');
+  print $row['Part Key']."\r";
 
  }
 
