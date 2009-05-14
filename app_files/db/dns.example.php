59e235da3faf5b254ded9d496e4a300d620d0cac
Raul Perusquia <rulovico@gmail.com>
2009-05-14 18:48:12 +0100

    adding dns and key exmaples

---
+++ app_files/db/dns.example.php
@@ -0,0 +1,21 @@
+<?
+/*
+Change:
+PASSWORD
+USER
+HOST
+
+to suit your database configuration
+and save as to dns.php
+
+
+
+*/
+
+$dns_pwd=PASSWORD;
+$dns_db='dw';
+$dns_user=USER;
+$dns_host=HOST;
+$dsn = 'mysql://'.$dns_user.':'.$dns_pwd.'@'.$dns_host.'/'.$dns_db;
+
+?>
