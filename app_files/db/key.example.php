886c1fe141c56162691304cf28aee57f1b0a4581
Raul Perusquia <rulovico@gmail.com>
2009-05-14 18:48:12 +0100

    adding dns and key exmaples

---
+++ app_files/db/key.example.php
@@ -0,0 +1,10 @@
+<?
+/* Replace RANDOM_STRING_32_CHARACTERS and RANDON_STRING_64_CHARACTERS
+ * and save as key.php 
+ * 
+ * /
+
+
+define("IKEY",RANDOM_STRING);
+define("SKEY", ANOTHER_RANDON_STRING);
+?>
