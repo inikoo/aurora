db7e75035c25c51d15f59f2aa4fb767c300835b3
Raul Perusquia <raul@tunder>
2009-10-30 17:07:44 +0000

    delte iputex files

--- external_libs/inputex/build/makeZip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ../..
-zip -vr inputex-0.2.2.zip inputex-0.2.2 -x "*.zip" "*.git*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf" "*build/tools*"
