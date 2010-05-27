4db905011011ff959ca187fa5ba00f9ab1d45541
Raul Perusquia <raul@alu.local>
2010-05-27 13:37:38 +0200

    changes structure costa

--- mantenence/scripts/ci_create_corparation_staff_struncture.php
+++ mantenence/scripts/ci_create_corparation_staff_struncture.php
@@ -43,11 +43,11 @@ global $myconf;
 
 $data=array(
           'editor'=>array('Date'=>'2003-08-28 09:00:00')
-	  ,'Company Name'=>'AW Regalos'
-	  ,'Company Fiscal Name'=>'Costa Imports'
-	  ,'Company Tax Number'=>''
-	  ,'Company Registration Number'=>''
-	  ,'Company Main Plain Email'=>'mail@aw-regalos.com'
+                   ,'Company Name'=>'AW Regalos'
+                                   ,'Company Fiscal Name'=>'Costa Imports'
+                                                          ,'Company Tax Number'=>''
+                                                                                ,'Company Registration Number'=>''
+                                                                                                               ,'Company Main Plain Email'=>'mail@aw-regalos.com'
       );
 
 
@@ -73,13 +73,7 @@ $areas=array(
                'Company Area Description'=>'House of Picking,Packing and Stock Departments',
 
            )
-           ,array(
-               'Company Key'=>$company->id,
-               'Company Area Code'=>'PRD',
-               'Company Area Name'=>'Production',
-               'Company Area Description'=>'House of the Manufacture Departments',
 
-           )
        );
 
 foreach($areas as $areas_data) {
@@ -90,40 +84,22 @@ foreach($areas as $areas_data) {
 
 $departments=array(
                  'OFC'=>array(
-			        array(
+                           array(
                                'Company Department Code'=>'DIR',
                                'Company Department Name'=>'Direction',
                                'Company Department Description'=>'Director Office')
-                           , 
-			      
+                           ,
+
                            array(
-                               'Company Department Code'=>'CUS.UK',
-                               'Company Department Name'=>'Customer Services UK',
+                               'Company Department Code'=>'CUS',
+                               'Company Department Name'=>'Customer Services',
                                'Company Department Description'=>'Customer Services')
-                           ,array(
-                               'Company Department Code'=>'CUS.DE',
-                               'Company Department Name'=>'Customer Services Germany',
-                               'Company Department Description'=>'Customer Services')
-                           ,array(
-                               'Company Department Code'=>'CUS.FR',
-                               'Company Department Name'=>'Customer Services France',
-                               'Company Department Description'=>'Customer Services')
-                           ,array(
-                               'Company Department Code'=>'CUS.PL',
-                               'Company Department Name'=>'Customer Services Poland',
-                               'Company Department Description'=>'Customer Services')
+
                            ,array(
                                'Company Department Code'=>'MRK',
                                'Company Department Name'=>'Marketing',
                                'Company Department Description'=>'Marketing Department')
-                           ,array(
-                               'Company Department Code'=>'ACC',
-                               'Company Department Name'=>'Accounting',
-                               'Company Department Description'=>'Accounting Department')
-                           ,array(
-                               'Company Department Code'=>'SMA',
-                               'Company Department Name'=>'Store Product Management',
-                               'Company Department Description'=>'Department where we order stock and put it on the webpage for selling')
+
 
                        ),'WAH'=>array(
                                    array(
@@ -135,14 +111,7 @@ $departments=array(
                                        'Company Department Name'=>'Stock Keeping',
                                        'Company Department Description'=>'Dealing with Deliveries and stock movements')
 
-                               ),'PRD'=>array(
-                                           array(
-                                               'Company Department Code'=>'GEN',
-                                               'Company Department Name'=>'General Production',
-                                               'Company Department Description'=>'Product all kinds of products')
-
-
-                                       )
+                               )
 
 
              );
@@ -157,118 +126,69 @@ foreach($departments as $area_code=>$departments_data) {
 
 
 $positions=array(
-		 'MRK'=>array(
-			      array(
-				    'Company Position Code'=>'MRK.O',
-				    'Company Position Title'=>'Marketing',
-				    'Company Position Description'=>'Marketing'
-				    )
-			      ,array(
-				    'Company Position Code'=>'WEB',
-				    'Company Position Title'=>'Web Designer',
-				    'Company Position Description'=>'Web Designer'
-				    )
-			      
-			      )
-		 ,'DIR'=>array(
-			      array(
-				    'Company Position Code'=>'DIR',
-				    'Company Position Title'=>'Director',
-				    'Company Position Description'=>'General Director'
-				    )
-			      )
-		 	 
-		 ,'ACC'=>array(
-			      array(
-				    'Company Position Code'=>'ACC',
-				    'Company Position Title'=>'Accounts',
-				    'Company Position Description'=>'General Accounts '
-				    )
-			      )
-		 	 
-		 
-		 	 
-		 
-		 	 
-		 ,'STK'=>array(
-			       
-                          array(
-				'Company Position Code'=>'WAH.SK',
-				'Company Position Title'=>'Warehouse Stock Keeper',
-                             'Company Position Description'=>'Stock Receaving & Handing'
-                         ),array(
-                             'Company Position Code'=>'OFC.SK',
-                             'Company Position Title'=>'Stock Controller',
-                             'Company Position Description'=>'Stock Control'
-                         ),array(
-                             'Company Position Code'=>'BUY',
-                             'Company Position Title'=>'Buyer',
-                             'Company Position Description'=>'Buyer'
-                         )
-                          
-                          
-                         
-                     )
-		 
-               ,'OHA'=>array(
+               'MRK'=>array(
                          array(
-                             'Company Position Code'=>'PICK',
-                             'Company Position Title'=>'Picker',
-                             'Company Position Description'=>'Warehouse Parts Picker'
-                         ),
-                          array(
-                             'Company Position Code'=>'PACK',
-                             'Company Position Title'=>'Packer',
-                             'Company Position Description'=>'Orders Packer'
-                         ),
-                     
-                          array(
-                             'Company Position Code'=>'OHA.DM',
-                             'Company Position Title'=>'Dispatch Supervisor',
-                             'Company Position Description'=>'Dispatch Supervisor'
-                         ),
-                          array(
-                             'Company Position Code'=>'OHA.M',
-                             'Company Position Title'=>'Warehouse Manager',
-                             'Company Position Description'=>'Warehouse Supervisor'
+                             'Company Position Code'=>'WEB',
+                             'Company Position Title'=>'Web Designer',
+                             'Company Position Description'=>'Web Designer'
                          )
-                          
-                         
+
                      )
-	       ,'GEN'=>array(
-                         array(
-                             'Company Position Code'=>'PROD.M',
-                             'Company Position Title'=>'Production Commander in Chief',
-                             'Company Position Description'=>'Production Supervisor'
-                         ),
-                          array(
-                             'Company Position Code'=>'PROD.O',
-                             'Company Position Title'=>'Production Operative',
-                             'Company Position Description'=>'Production Associate'
-                         )
-			     )
-	       ,'CUS.UK,CUS.PL,CUS.DE,CUS.FR'=>array(
-			array(
-                             'Company Position Code'=>'CUS',
-                             'Company Position Title'=>'Customer Service',
-                             'Company Position Description'=>'Customer Service'
-                         )	   
-						     )
-						     
-		 );
+                     ,'DIR'=>array(
+                                array(
+                                    'Company Position Code'=>'DIR',
+                                    'Company Position Title'=>'General Manager',
+                                    'Company Position Description'=>'Director'
+                                )
+                            )
+
+
+
+
+
+
+
+
+
+
+                            ,'OHA'=>array(
+                                       array(
+                                           'Company Position Code'=>'PICK',
+                                           'Company Position Title'=>'Picker & Packer',
+                                           'Company Position Description'=>'Warehouse Parts Picker'
+                                       )
+
+
+
+                                   )
+
+                                   ,'CUS'=>array(
+                                    array(
+                                                  'Company Position Code'=>'CUSM',
+                                                  'Company Position Title'=>'Manager Customer Service',
+                                                  'Company Position Description'=>'Manager Customer Service'
+                                              )
+                                              ,array(
+                                                  'Company Position Code'=>'CUS',
+                                                  'Company Position Title'=>'Customer Service',
+                                                  'Company Position Description'=>'Customer Service'
+                                              )
+                                          )
+
+           );
 
 foreach($positions as $department_codes=>$positions_data) {
-  foreach(preg_split('/,/',$department_codes) as $key =>$department_code ){
+    foreach(preg_split('/,/',$department_codes) as $key =>$department_code ) {
 
-    $department=new CompanyDepartment('code',$department_code);
-    if(!$department->id){
-      print_r($department);
-    exit;
+        $department=new CompanyDepartment('code',$department_code);
+        if (!$department->id) {
+            print_r($department);
+            exit;
+        }
+        foreach($positions_data as $data) {
+            $department->add_position($data);
+        }
     }
-    foreach($positions_data as $data) {
-      $department->add_position($data);
-    }
-  }
 
 }
 
@@ -278,96 +198,52 @@ foreach($positions as $department_codes=>$positions_data) {
 
 
 $staff=array(
-	      'PROD.O'=>array(
-	     
-			      array('Staff Name'=>'Joanna Ciba','Staff Alias'=>'joana','Staff Type'=>'Temporal Worker')
-			      ,array('Staff Name'=>'Magdalena Dawiskiba','Staff Alias'=>'magda')
-			      ,array('Staff Name'=>'Dusan Belan','Staff Alias'=>'dusan')
-			      ,array('Staff Name'=>'Lucie Sicova','Staff Alias'=>'lucie')
-			      ,array('Staff Name'=>'Daniela Matovlava','Staff Alias'=>'daniela')
-			      ,array('Staff Name'=>'Olga Belanova','Staff Alias'=>'olga')
-			      ,array('Staff Name'=>'Dana Marsallova','Staff Alias'=>'dana')
-			      ,array('Staff Name'=>'Danielle Cox','Staff Alias'=>'danielle','Staff Type'=>'Temporal Worker')
-			      )
-	      ,'PROD.M'=>array(
-	     
-			      array('Staff Name'=>'Neal','Staff Alias'=>'neal')
-			      )
 
-	      ,'PICK'=>array(
-			    array('Staff Name'=>'Steffanie Cox','Staff Alias'=>'stephanie','Staff Type'=>'Temporal Worker')
-			    ,array('Staff Name'=>'Adriana Bobokova','Staff Alias'=>'adriana')
-			    ,array('Staff Name'=>'Janet Walker','Staff Alias'=>'janet')
-			    )
-	      ,'PACK'=>array(
-			     array('Staff Name'=>'Andrew Barry','Staff Alias'=>'andy')
-			     ,array('Staff Name'=>'Lucy Adams','Staff Alias'=>'lucy','Staff Type'=>'Temporal Worker')
-			     )
-	       ,'WAH.SK'=>array(
-			     array('Staff Name'=>'Michael Wragg','Staff Alias'=>'michael')
-			     ,array('Staff Name'=>'Craige Blakemore','Staff Alias'=>'craige')
-			     )
-	       ,'OHA.M'=>array(
-			     array('Staff Name'=>'Tomas Belam','Staff Alias'=>'tomas')
-			   
-			     )
-	       ,'OHA.DM'=>array(
-			     array('Staff Name'=>'Lenka Ondrisova','Staff Alias'=>'lenka')
-			     
-				)
+           'PICK'=>array(
+                       array('Staff Name'=>'David','Staff Alias'=>'david')
+                         ,array('Staff Name'=>'Borja','Staff Alias'=>'borja')
+                         ,array('Staff Name'=>'Dany','Staff Alias'=>'dany')
+                                                
 
-	      ,'BUY'=>array(
-			     array('Staff Name'=>'Alan W','Staff Alias'=>'alan')
-			     
-				)
-	      ,'OFC.SK'=>array(
-			     array('Staff Name'=>'Eric Zee','Staff Alias'=>'eric')
-				)
-	      ,'WEB'=>array(
-			     array('Staff Name'=>'Raul Perusquia','Staff Alias'=>'raul')
-				)
-	      ,'DIR'=>array(
-			     array('Staff Name'=>'David Hardy','Staff Alias'=>'david')
-				)
-	       ,'ACC'=>array(
-			     array('Staff Name'=>'Slavka Hardy','Staff Alias'=>'slavka')
-			     )
-	       ,'MRK.O'=>array(
-			     array('Staff Name'=>'Katka Buchy','Staff Alias'=>'katka')
-			     )
+                    
+                   )
+                   
+                                                                         ,'WEB'=>array(
+                                                                                    array('Staff Name'=>'Helena','Staff Alias'=>'helena')
+                                                                                )
+                                                                                ,'DIR'=>array(
+                                                                                           array('Staff Name'=>'Carlos Lopez','Staff Alias'=>'carlos')
+                                                                                       )
+                                                                                      
+  ,'CUSM'=>array(
+                                                                                                                  array('Staff Name'=>'Lucia','Staff Alias'=>'lucia')
+                                                                                                              
+
+                                                                                                              )
+
+                                                                                                       ,'CUS'=>array(
+                                                                                                                  array('Staff Name'=>'Trini','Staff Alias'=>'trini')
+                                                                                                              
+
+                                                                                                              )
 
 
-	      ,'CUS'=>array(
-			    array('Staff Name'=>'Kerry Miskelly','Staff Alias'=>'kerry')
-			    ,array('Staff Name'=>'Sarka Doubravova','Staff Alias'=>'sarka')
-			    ,array('Staff Name'=>'Zoe','Staff Alias'=>'zoe','Staff Currently Working'=>'No')
-			    ,array('Staff Name'=>'Philippe Buchy','Staff Alias'=>'philippe')
-			    ,array('Staff Name'=>'Martina Otte','Staff Alias'=>'martina')
-			    ,array('Staff Name'=>'Nassim','Staff Alias'=>'nassim')
-			    ,array('Staff Name'=>'Nabil','Staff Alias'=>'nabil','Staff Currently Working'=>'No')
-			    ,array('Staff Name'=>'Amanda Fray','Staff Alias'=>'amanda')
-			    ,array('Staff Name'=>'Urszula Baka','Staff Alias'=>'urszula')
-			    
-			    
-			    )
-
-
-	     );
+       );
 
 
 foreach($staff as $position_codes=>$staff_data) {
-  foreach(preg_split('/,/',$position_codes) as $key =>$position_code ){
+    foreach(preg_split('/,/',$position_codes) as $key =>$position_code ) {
 
-    $position=new CompanyPosition('code',$position_code);
-    if(!$position->id){
-      print "$position_code\n";
-      //print_r($position);
-    exit;
+        $position=new CompanyPosition('code',$position_code);
+        if (!$position->id) {
+            print "$position_code\n";
+            //print_r($position);
+            exit;
+        }
+        foreach($staff_data as $data) {
+            $position->add_staff($data);
+        }
     }
-    foreach($staff_data as $data) {
-      $position->add_staff($data);
-    }
-  }
 
 }
 
