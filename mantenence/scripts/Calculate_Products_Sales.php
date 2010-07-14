3f55b48bf3e441a249c454688c059582268214b5
Raul Perusquia <rulovico@gmail.com>
2010-07-14 14:06:12 +0100

    fix scripts

--- mantenence/scripts/Calculate_Products_Sales.php
+++ mantenence/scripts/Calculate_Products_Sales.php
@@ -33,7 +33,7 @@ $result=mysql_query($sql);
 while($row=mysql_fetch_array($result)   ){
   $product=new Product('pid',$row['Product ID']);
  $product->update_sales_data();
- $product->update_full_search();
+ // $product->update_full_search();
   print $row['Product ID']."\t\t ".$product->data['Product Code']." \r";
 
 }
