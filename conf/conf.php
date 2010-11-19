53004513fe46853dc40b3410bb3f1edacb38ccd2
primediart <primediart@primediart-desktop.(none)>
2010-11-19 14:50:19 +0530

    kaktus-269:not completed now

--- conf/conf.php
+++ conf/conf.php
@@ -1264,7 +1264,7 @@ $default_state=array(
                                                         )
                         ),
                    'users'=>array(
-                               'staff'=>array(
+                               'staff'=>array('id'=>'',
                                            'display'=>'all',
                                            'order'=>'alias',
                                            'order_dir'=>'',
@@ -2634,7 +2634,8 @@ $default_state=array(
                              'f_field'=>'country_name',
                              'f_value'=>'',
                                  ),
-        )
+                            )
+
 
                );
 
