3faa21a4ae4de97b0b1934efc23db0136e173413
primediart <primediart@primediart-desktop.(none)>
2010-11-25 19:21:29 +0530

    testing

--- edit_each_staff.php
+++ edit_each_staff.php
@@ -16,8 +16,6 @@ $_SESSION['state']['company_staff']['id']=$company_staff_id;
 
 
 $company_staff=new Staff($company_staff_id);
-
-
 if(!$company_staff->id){
  //print_r();
  header('Location: index.php');
@@ -47,14 +45,7 @@ if($modify){
   $general_options_list[]=array('tipo'=>'url','url'=>'edit_each_staff?edit=1','label'=>_('Edit Staff'));
 }
 }
-
-
 $smarty->assign('general_options_list',$general_options_list);
-
-
-
-
-
 $css_files=array(
 		 $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
 		 $yui_path.'build/assets/skins/sam/skin.css',
@@ -79,50 +70,15 @@ $js_files=array(
 $smarty->assign('parent','staff');
 $smarty->assign('sub_parent','staff');
 
-if(!$edit){
-
-$js_files[]='edit_each_staff.js.php';
-$smarty->assign('css_files',$css_files);
-$smarty->assign('js_files',$js_files);
-
-$smarty->assign('title', _('Company Staff'));
-
-$tipo_filter=$_SESSION['state']['hr']['staff']['f_field'];
-
-$smarty->assign('filter',$tipo_filter);
-$smarty->assign('filter_value',$_SESSION['state']['hr']['staff']['f_value']);
-
-$smarty->assign('view',$_SESSION['state']['hr']['view']);
-
-$filter_menu=array(
-		   'name'=>array('db_key'=>'staff.alias','menu_label'=>'Staff name <i>*x*</i>','label'=>'Name'),
-		   'position_id'=>array('db_key'=>'position_id','menu_label'=>'Position Id','label'=>'Position Id'),
-		   'staff_id'=>array('db_key'=>'staff_id','menu_label'=>'Staff Id','label'=>'Staff Id'),
-		   );
-$smarty->assign('filter_menu0',$filter_menu);
-
-$smarty->assign('filter_name0',$filter_menu[$tipo_filter]['label']);
-
-
-$paginator_menu=array(10,25,50,100,500);
-$smarty->assign('paginator_menu0',$paginator_menu);
-
-
-$smarty->display('edit_each_staff.tpl');
-}else{
+if($edit){
 $smarty->assign('edit',$_SESSION['state']['edit_each_staff']['edit'] );
-
 $css_files[]='css/edit.css';
-
 $js_files[]='js/edit_common.js';
 $js_files[]='edit_each_staff.js.php?staff_key='.$company_staff->data['Staff Key'];
 $smarty->assign('css_files',$css_files);
 $smarty->assign('js_files',$js_files);
 $smarty->assign('title', _('Editing Staff'));
 $smarty->assign('editing',true);
-
 $smarty->display('edit_each_staff.tpl');
-
-
 }
 ?>
