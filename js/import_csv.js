2707adfb0f6d377f13373dac4b6e0fa6f986ba30
Manojit Nandi <manojit.primediart@gmail.com>
2011-01-08 21:07:21 +0530

    csv import

---
+++ js/import_csv.js
@@ -0,0 +1,19 @@
+function popup(url) 
+{
+ var width  = 500;
+ var height = 400;
+ var left   = (screen.width  - width)/2;
+ var top    = (screen.height - height)/2;
+ var params = 'width='+width+', height='+height;
+ params += ', top='+top+', left='+left;
+ params += ', directories=no';
+ params += ', location=no';
+ params += ', menubar=no';
+ params += ', resizable=no';
+ params += ', scrollbars=no';
+ params += ', status=no';
+ params += ', toolbar=no';
+ newwin=window.open('import.php','windowname5', params);
+ if (window.focus) {newwin.focus()}
+ return false;
+}
