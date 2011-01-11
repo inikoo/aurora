ca6f450a998eb89ee9aa19926e0d21c87e3a4000
Raul Perusquia <rulovico@gmail.com>
2011-01-11 20:06:11 +0000

    KAKTUS-8

--- marketing.js.php
+++ marketing.js.php
@@ -2,13 +2,39 @@
 include_once('common.php');
 ?>
 
-    var Dom   = YAHOO.util.Dom;
+var Dom   = YAHOO.util.Dom;
 var Event = YAHOO.util.Event;
-Event.addListener('product_submit_search', "click",submit_search,'product');
-Event.addListener('product_search', "keydown", submit_search_on_enter,'product');
 
-Event.addListener('order_submit_search', "click",submit_search,'order');
-Event.addListener('order_search', "keydown", submit_search_on_enter,'order');
+function change_block(e){
+    var ids = ["metrics","newsletter","email","web_internal","web","other"]; 
+    var block_ids = ["block_metrics","block_newsletter","block_email","block_web_internal","block_web","block_other"];
+	Dom.setStyle(block_ids,'display','none');
+	Dom.setStyle('block_'+this.id,'display','');
+	Dom.removeClass(ids,'selected');
+	Dom.addClass(this, 'selected');
+	YAHOO.util.Connect.asyncRequest('POST','ar_sessions.php?tipo=update&keys=marketing-view&value='+this.id ,{});
+}
 
-Event.addListener('customer_submit_search', "click",submit_search,'customer');
-Event.addListener('customer_search', "keydown", submit_search_on_enter,'customer');
+  
+function init(){
+
+
+    var ids = ["metrics","newsletter","email","web_internal","web","other"]; 
+    YAHOO.util.Event.addListener(ids, "click", change_block);
+   
+}
+
+YAHOO.util.Event.onDOMReady(init);
+
+YAHOO.util.Event.onContentReady("rppmenu1", function () {
+	 var oMenu = new YAHOO.widget.ContextMenu("rppmenu1", {trigger:"rtext_rpp1" });
+	 oMenu.render();
+	 oMenu.subscribe("show", oMenu.focus);
+    });
+YAHOO.util.Event.onContentReady("filtermenu1", function () {
+	 var oMenu = new YAHOO.widget.ContextMenu("filtermenu1", {trigger:"filter_name1"});
+	 oMenu.render();
+	 oMenu.subscribe("show", oMenu.focus);
+	 
+    });
+
