9adb9c1a6676f7e2d4ef125dad5571aa90e7e1fe
primediart <primediart@primediart-desktop.(none)>
2011-01-15 18:59:15 +0530

    test

--- external_libs/PDF/template_customer.php
+++ external_libs/PDF/template_customer.php
@@ -12,21 +12,20 @@ if(mysql_num_rows($result) > 0)
 
 <center>
 
-<table width="100%" border="0"  bgcolor="#FFFFFF" style="font:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; font-size:25px;">
+<table width="100%" border="0"  bgcolor="#FFFFFF" style="font:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; font-size:25px;" cellspacing="5" cellpadding="5">
 	 <tr>
     <td colspan="3" align="center" style="font-size:48px;font-weight:bold;">CUSTOMER ADDRESS<hr></td>
   </tr>
 
   <tr>
-    <td align="left" style="border: 1px solid #000;margin:10px;">
+    <td align="left" style="border: 1px solid #000000;margin:10px;padding:10px;">
 <span style="font-size:35px;font-weight:bold;"><?php echo $row['Customer Company Name']; ?></span><br> 
 <span style="font-weight:bold;"><?php echo $row['Customer Main Contact Name'];?></span><br><?php echo $row['Customer Main XHTML Address']; ?></td>
     <td width="5%">&nbsp;</td>
     <td width="50%"></td>
   </tr>
+</table>
   
-		</table>
-   
 </center>
 <br />
 <br />
