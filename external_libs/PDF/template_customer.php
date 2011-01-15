71f8262e843461f0425cb61238680d6252d4468d
primediart <primediart@primediart-desktop.(none)>
2011-01-15 18:49:35 +0530

    kaktus-336 changes done

--- external_libs/PDF/template_customer.php
+++ external_libs/PDF/template_customer.php
@@ -11,34 +11,18 @@ if(mysql_num_rows($result) > 0)
 ?>
 
 <center>
-<table width="100%" border="0"  bgcolor="#FFFFFF" style="font:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; font-size:32px;">
+
+<table width="100%" border="0"  bgcolor="#FFFFFF" style="font:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; font-size:25px;">
 	 <tr>
     <td colspan="3" align="center" style="font-size:48px;font-weight:bold;">CUSTOMER ADDRESS<hr></td>
   </tr>
-  <tr height="35%">
-    <td align="left" style="font-size:40px;font-weight:bold;"><?php echo $row['Customer Name']; ?> &nbsp;<span style="color:SteelBlue"><?php echo $customer_id;?></span></td>
-  </tr>
-  <tr height="35%">
-    <td align="left" style="font-size:40px;font-weight:bold;"><?php echo $row['Customer Tax Number']; ?><br></td>
-  </tr>
-  <tr>
-    <td width="45%">
-<?php echo $row['Customer Main XHTML Address']; ?></td>
-    <td width="5%">&nbsp;</td>
-    <td width="50%"><table border="0">
-                        <tr> 
-				<td width="18%">Name</td><td align="right" width="70%"><?php echo $row['Customer Main Contact Name'];?></td><td width="12%"></td>
-			</tr>
-			<tr>
-				<td width="18%">Email</td><td align="right" width="70%"><?php echo $row['Customer Main XHTML Email']; ?></td><td width="12%"></td>
-			</tr>
-			<tr>
-				<td width="18%">Telephone</td><td align="right" width="70%"><?php echo $row['Customer Main XHTML Telephone']; ?></td><td width="12%"></td>
-			</tr>
-			
-		</table>
 
-     </td>
+  <tr>
+    <td align="left" style="border: 1px solid #000;margin:10px;">
+<span style="font-size:35px;font-weight:bold;"><?php echo $row['Customer Company Name']; ?></span><br> 
+<span style="font-weight:bold;"><?php echo $row['Customer Main Contact Name'];?></span><br><?php echo $row['Customer Main XHTML Address']; ?></td>
+    <td width="5%">&nbsp;</td>
+    <td width="50%"></td>
   </tr>
   
 		</table>
