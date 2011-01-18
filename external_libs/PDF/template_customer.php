86ec059dcc130cb2c3dad589f8576b4fccd3d7e8
primediart <primediart@primediart-desktop.(none)>
2011-01-18 21:14:54 +0530

    pdf files of invoice,delivery address,customer address completed

--- external_libs/PDF/template_customer.php
+++ external_libs/PDF/template_customer.php
@@ -5,6 +5,7 @@ $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
 $sql = "select * from `Customer Dimension` where `Customer Key` = '".$id."'";
 $result = mysql_query($sql);
 $row = mysql_fetch_array($result);
+
 $customer_id=$myconf['customer_id_prefix'].sprintf("%05d",$row['Customer Key']);
 if(mysql_num_rows($result) > 0)
 {
@@ -12,23 +13,10 @@ if(mysql_num_rows($result) > 0)
 
 <center>
 
-<table width="100%" border="0"  bgcolor="#FFFFFF" style="font:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; font-size:25px;" cellspacing="5" cellpadding="5">
-	 <tr>
-    <td colspan="3" align="center" style="font-size:48px;font-weight:bold;">CUSTOMER ADDRESS<hr></td>
-  </tr>
-
-  <tr>
-    <td align="left" style="border: 1px solid #000000;margin:10px;padding:10px;">
-<span style="font-size:35px;font-weight:bold;"><?php echo $row['Customer Company Name']; ?></span><br> 
-<span style="font-weight:bold;"><?php echo $row['Customer Main Contact Name'];?></span><br><?php echo $row['Customer Main XHTML Address']; ?></td>
-    <td width="5%">&nbsp;</td>
-    <td width="50%"></td>
-  </tr>
-</table>
-  
-</center>
-<br />
-<br />
+<table style="font-size:22px;" width="100%" align="center"><tr><td>
+<span style="font-size:22px;font-weight:bold;"><?php echo $row['Customer Company Name']; ?></span><br> 
+<span style="font-size:20px;font-weight:bold;"><?php echo $row['Customer Main Contact Name'];?></span><br><?php echo $row['Customer Main XHTML Address']; ?>
+</td></tr></table>
 
 <?php
 }
