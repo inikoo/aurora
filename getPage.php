f5c7f0e0e2e70b407e3a4d6390781144fa310bb2
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-02-12 15:39:18 +0530

    selected box in import data completed

--- getPage.php
+++ getPage.php
@@ -10,10 +10,12 @@
 	$index = $_REQUEST['v'];
 	$r = $csv->getRow($index);
 	
+	$tt = explode(',',$_REQUEST['myArray']);
+
+	$prev = explode(',',$_REQUEST['prevArray']);
+	
 ?>
 <table class="recordList">
-		
-		
 		<!--<th class="list-column-left" style="text-align: left; width: 20%; padding-left:5px;">
 		    Column
 		</th>-->
@@ -32,14 +34,16 @@
               			 if($index > 0)
               			 {
        			    ?>
-       				<a href="#" class="subtext" id="prev" onclick="getPrev(<?php echo $index; ?>)">Previous</a>&nbsp;|&nbsp;
+       				<a href="#" class="subtext" id="prev" onclick="getPrev(<?php echo $index; ?>,<?php echo count($h);?>)">Previous</a>&nbsp;|&nbsp;
 		       	   <?php
 			         }
 			       if($index < $count_rows-1)        
 			         {
+					$i=0;
 		       	   ?>
-       				<a href="#"  class="subtext" id="next" onclick="getNext(<?php echo $index; ?>)">Next</a>
+<a href="#" class="subtext" id="next" onclick="getNext(<?php echo $index; ?>,<?php echo count($h);?>)">Next</a>
 		          <?php
+					$i++;
 			         }
 		          ?>
 		&nbsp;&nbsp;&nbsp;&nbsp; <a href="#" onclick="getIgnore(<?php echo $index; ?>)" id="result" class="subtext">Ignore Result</a>
@@ -51,34 +55,30 @@
 	<?php
 	for($j=0; $j<count($h); $j++)
 	{ 
+		
 	?>
 	<tr>
+	
 		<!--<td width=150 align=center >
 		
 		<?php //echo $h[$j]; 	$leftColumnArray = $h; ?>
 		</td>-->
-		<td align=center>
-		<select name="assign_field[]" id="assign_field">
-		    <option value="0">Ignore</option>
-		    <option value="Customer Main Contact Name">Contact Name</option>
-		    <option value="Customer Name">Name</option>
-		    <option value="Customer Type">Type</option>
-		    <option value="Customer Company Name">Company Name</option>
-		    <option value="Customer Main Plain Email">Email</option>$j
-		    <option value="Contact Main Plain Mobile">Mobile</option>
-		    <option value="Customer Main Plain Telephone">Telephone</option>	
-		    <option value="Customer Main Plain FAX">FAX</option>
-		    <option value="Customer Main Plain Address">Address</option>
-		    <option value="Customer Address Line 1">Address Line1</option>
-		    <option value="Customer Address Line 2">Address Line2</option>
-		    <option value="Customer Address Line 3">Address Line3</option>
-		   <option value="Customer Address Town">Town</option>
-		    <option value="Customer Address Postal Code">Postal Code</option>
-		    <option value="Customer Address Country Name">Country Name</option>
-		    <option value="Customer Address Country First Division">First Division</option>
-		    <option value="Customer Address Country Second Division">Second Division</option>
-		    <option value="Customer Tax Number">Tax Number</option>
-		 </select>
+		<td align=center id="middle_column">
+		<select name="assign_field[]" id="assign_field_<?php echo $j; ?>">
+		<?php 
+			$selectBox = array("0"=>'Ignore',"1"=>'Contact Name',"2"=>'Name',"3"=>'Type',"4"=>'Company Name',"5"=>'Email',"6"=>'Mobile',"7"=>'Telephone',"8"=>'FAX',"9"=>'Address',"10"=>'Address Line1',"11"=>'Address Line2',"12"=>'Address Line3',"13"=>'Town',"14"=>'Postal Code',"15"=>'Country Name',"16"=>'First Division',"17"=>'Second Division',"18"=>'Tax Number');
+
+		foreach($selectBox as $key=>$value) { 
+
+
+		
+?>
+
+		   
+			<option value="<?php echo $key;?>" <?php if($key==$tt[$j] || $key==$prev[$j]) { ?>selected="selected"<?php } ?> ><?php echo $value;?></option>
+		
+		<?php } ?>
+		</select>
 		</td>
 		<td>
 			<!-- Value Output -->
