bf07cf245fe559fc63b7931c1561dc9ebe7db935
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-02-14 19:30:43 +0530

    print

--- getPage.php
+++ getPage.php
@@ -18,7 +18,9 @@
 
 	if(isset($_REQUEST['prevArray'])) { $prev = explode(',',$_REQUEST['prevArray']); }
 	
-	$selectBox = array();
+	
+	print_r($tt);
+	print_r($prev);
 
 ?>
 <table class="recordList">
@@ -76,14 +78,14 @@
 			
 			//$selectBox = array('0'=>'Ignore','1'=>'Contact Name','2'=>'Name','3'=>'Type','4'=>'Company Name','5'=>'Email','6'=>'Mobile','7'=>'Telephone','8'=>'FAX','9'=>'Address','10'=>'Address Line1','11'=>'Address Line2','12'=>'Address Line3','13'=>'Town','14'=>'Postal Code','15'=>'Country Name','16'=>'First Division','17'=>'Second Division','18'=>'Tax Number');
 
-			$selectBox = array("Ignore"=>'Ignore',"Customer Main Contact Name"=>'Contact Name',"Customer Name"=>'Name',"Customer Type"=>'Type',"Customer Company Name"=>'Company Name',"Customer Main Plain Email"=>'Email',"Contact Main Plain Mobile"=>'Mobile',"Customer Main Plain Telephone"=>'Telephone',"Customer Main Plain FAX"=>'FAX',"Customer Main Plain Address"=>'Address',"Customer Address Line 1"=>'Address Line1',"Customer Address Line 2"=>'Address Line2',"Customer Address Line 3"=>'Address Line3',"Customer Address Town"=>'Town',"Customer Address Postal Code"=>'Postal Code',"Customer Address Country Name"=>'Country Name',"Customer Address Country First Division"=>'First Division',"Customer Address Country Second Division"=>'Second Division',"Customer Tax Number"=>'Tax Number');
+			$selectBox = array('Ignore'=>'Ignore','Customer Main Contact Name'=>'Contact Name','Customer Name'=>'Name','Customer Type'=>'Type','Customer Company Name'=>'Company Name','Customer Main Plain Email'=>'Email','Contact Main Plain Mobile'=>'Mobile','Customer Main Plain Telephone'=>'Telephone','Customer Main Plain FAX'=>'FAX','Customer Main Plain Address'=>'Address','Customer Address Line 1'=>'Address Line1','Customer Address Line 2'=>'Address Line2','Customer Address Line 3'=>'Address Line3','Customer Address Town'=>'Town','Customer Address Postal Code'=>'Postal Code','Customer Address Country Name'=>'Country Name','Customer Address Country First Division'=>'First Division','Customer Address Country Second Division'=>'Second Division','Customer Tax Number'=>'Tax Number');
 
 
 
 		foreach($selectBox as $key=>$value) { ?>
 
 		   
-	<option value="<?php echo $key;?>" <?php if($key==$tt[$j] || $key==$prev[$j]) { ?>selected="selected"<?php } ?> ><?php echo $value;?></option>
+	<option value="<?php echo $key;?>" <?php if($tt[$j]==$key || $prev[$j]==$key) { ?>selected="selected"<?php } ?> ><?php echo $value;?></option>
 		
 		<?php } ?>
 		</select>
