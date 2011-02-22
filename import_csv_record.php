12fc31428b58f70bea1736cb6e9905692f9a0d98
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-02-22 15:58:12 +0530

    asdasd

---
+++ import_csv_record.php
@@ -0,0 +1,141 @@
+<?php
+	include('common.php');
+
+	require_once 'csvparser.php';
+	$csv = new CSV_PARSER;
+
+	//loading the CSV File
+	$csv->load($_SESSION['file_path']);
+
+	//extracting the HEADERS
+	$h = $csv->getHeaders();
+	$count_rows = $csv->countRows();
+	$index = $_REQUEST['v'];
+	
+	//$r =  $csv->getRow($index);
+	$raw = $csv->getrawArray();
+	
+	
+	
+	$tt = array();
+
+	$prev = array();	
+
+	if(isset($_REQUEST['myArray'])) { $tt = explode(',',$_REQUEST['myArray']); }
+
+	if(isset($_REQUEST['prevArray'])) { $prev = explode(',',$_REQUEST['prevArray']); }
+	
+	
+
+?>
+
+<table class="recordList">
+		<!--<th class="list-column-left" style="text-align: left; width: 20%; padding-left:5px;">
+		    Column
+		</th>-->
+		<th class="list-column-left" style="text-align: left; width: 20%;padding-left:10px;">
+		Assigned Field
+		</th>
+		<th class="list-column-left" style="text-align: left; width: 30%;">
+		<span style="float: left;">
+		<?php 
+			echo 'Value '.($index + 1).' of '.$count_rows;
+
+		?>
+		</span>
+		<span style="float: right;padding-right:5px;"> 
+			     <?php
+              			 if($index > 0)
+              			 {
+       			    ?>
+       				<a href="#" class="subtext" id="prev" onclick="getPrev(<?php echo $index; ?>,<?php echo count($h);?>)">Previous</a>&nbsp;|&nbsp;
+		       	   <?php
+			         }
+			       if($index < $count_rows-1)        
+			         {
+					$i=0;
+		       	   ?>
+<a href="#" class="subtext" id="next" onclick="getNext(<?php echo $index; ?>,<?php echo count($h);?>)">Next</a>
+		          <?php
+					$i++;
+			         }
+		          ?>
+		&nbsp;&nbsp;&nbsp;&nbsp; <a href="#" onclick="getIgnore(<?php echo $index; ?>,<?php echo count($h); ?>)" id="result" class="subtext">Ignore Record</a>
+		</span>
+		</th>
+		
+<tr>&nbsp;</tr>
+		
+	<?php
+	for($j=0; $j<count($h); $j++)
+	{ 
+		
+	?>
+	<tr>
+	
+		<!--<td width=150 align=center >
+		
+		<?php //echo $h[$j]; 	$leftColumnArray = $h; ?>
+		</td>-->
+
+		<td align=center id="middle_column">
+		<select name="assign_field[]" id="assign_field_<?php echo $j; ?>">
+		<?php 
+			
+			$selectBox = array('Ignore'=>'Ignore','Customer Main Contact Name'=>'Contact Name','Customer Name'=>'Name','Customer Type'=>'Type','Customer Company Name'=>'Company Name','Customer Main Plain Email'=>'Email','Contact Main Plain Mobile'=>'Mobile','Customer Main Plain Telephone'=>'Telephone','Customer Main Plain FAX'=>'FAX','Customer Main Plain Address'=>'Address','Customer Address Line 1'=>'Address Line1','Customer Address Line 2'=>'Address Line2','Customer Address Line 3'=>'Address Line3','Customer Address Town'=>'Town','Customer Address Postal Code'=>'Postal Code','Customer Address Country Name'=>'Country Name','Customer Address Country First Division'=>'First Division','Customer Address Country Second Division'=>'Second Division','Customer Tax Number'=>'Tax Number');
+
+
+
+		foreach($selectBox as $key=>$value) { 
+			if((isset($tt[$j]) == TRUE))
+			{
+		?>
+		<option value="<?php echo $key;?>" <?php if($tt[$j]==$key) { ?>selected="selected"<?php } ?> ><?php echo $value;?></option>
+		<?php
+			}
+			else
+				if((isset($prev[$j]) == TRUE))
+				{
+		?>
+		<option value="<?php echo $key;?>" <?php if($prev[$j]==$key) { ?>selected="selected"<?php } ?> ><?php echo $value;?></option>
+		<?php 		} 
+				else
+				{
+		?>
+		<option value="<?php echo $key;?>"><?php echo $value;?></option>
+		<?php
+				}
+
+		}?>
+		</select>
+		</td>
+		<td>
+			<!-- Value Output -->
+			<h4 id="changecolor_<?php echo $j; ?>" ><?php echo $raw[$index][$j]; ?></h4>
+		</td>
+	</tr>
+		<input type="hidden" name="values[]" value="<?php echo $raw[$index][$j]; ?>">
+		
+	<?php
+		
+	}
+	
+	?>
+	
+</table>
+<div id="display">
+<?php 
+	for($p=0; $p<count($h); $p++)
+	{
+	  if(isset($_SESSION[$p]))
+		{
+			echo $_SESSION[$i];
+		}
+	  else
+		{
+			echo $_REQUEST['color_array'][$i];
+		}
+		
+	}
+	?>
+</div>
---
+++ insert_csv.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ File: store.php 
+
+ UI store page
+
+ About: 
+ Autor: Raul Perusquia <rulovico@gmail.com>
+ 
+ Copyright (c) 2009, Kaktus 
+ 
+ Version 2.0
+*/
+include_once('common.php');
+
+
+
+$css_files=array(
+		 $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+		 $yui_path.'menu/assets/skins/sam/menu.css',
+		 $yui_path.'button/assets/skins/sam/button.css',
+		 $yui_path.'assets/skins/sam/autocomplete.css',
+
+		 //	 $yui_path.'assets/skins/sam/autocomplete.css',
+		 'common.css',
+		 'container.css',
+		 'button.css',
+		 'table.css',
+		 'css/dropdown.css',
+		 'css/import_data.css'
+		 );
+$js_files=array(
+		$yui_path.'utilities/utilities.js',
+		$yui_path.'json/json-min.js',
+		$yui_path.'paginator/paginator-min.js',
+		$yui_path.'dragdrop/dragdrop-min.js',
+		$yui_path.'datasource/datasource-min.js',
+		$yui_path.'autocomplete/autocomplete-min.js',
+		$yui_path.'datatable/datatable.js',
+		$yui_path.'container/container-min.js',
+		$yui_path.'menu/menu-min.js',
+		$yui_path.'uploader/uploader-debug.js',
+		'js/php.default.min.js',
+		'common.js.php',
+		'table_common.js.php',
+		'js/dropdown.js',
+        	);
+
+	
+
+	//value of the assigned field
+	$assign = isset($_REQUEST['assign_field'])?$_REQUEST['assign_field']:'0';
+
+
+	//value of the right column
+	$values = isset($_REQUEST['values'])?$_REQUEST['values']:'';
+
+	
+	//removed list of array
+	$hidden_array = isset($_REQUEST['hidden_array'])?$_REQUEST['hidden_array']:'0';
+
+	
+	
+
+	//code to generate the final array		
+	for($i = 0; $i < count($assign);  $i++) 
+	{
+		
+		//restrict whether any ignore field is there 
+		if($assign[$i] != 'Ignore')
+		{
+
+		 	$rows[$assign[$i]] = $values[$i];
+
+		}		
+		
+	}
+
+	
+	require_once 'csvparser.php';
+	$csv = new CSV_PARSER;
+	//loading the CSV File
+	$csv->load($_SESSION['file_path']);
+	//extracting the HEADERS
+	$h = $csv->getHeaders();
+	$count_rows = $csv->countRows();
+	
+	//count the removed array
+	
+		$numIndex = count($hidden_array);	
+	
+	
+		$new_arr = array();
+
+		for($y=0; $y<$count_rows; $y++)
+		{
+			$total_row = $csv->getRow($y);
+			array_push($new_arr, $total_row);
+
+		}
+	
+
+		if($hidden_array != '0')
+		{
+			foreach($hidden_array as $value)
+			{
+
+
+				unset($new_arr[$value - 1]);
+
+			}
+		}
+
+		
+	
+
+	print_r($new_arr);
+
+$smarty->assign('js_files',$js_files);
+$smarty->assign('css_files',$css_files);
+$smarty->assign('assign',$assign);
+$smarty->assign('values',$values);
+
+
+$smarty->display('insert_csv.tpl');
+	unset($_SESSION['getQueryString']);
+
+?>
