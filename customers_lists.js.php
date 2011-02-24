e0e1f74a94b3b07a2fefc040e147990c2bf92613
primediart <primediart@primediart-desktop.(none)>
2011-02-24 21:31:27 +0530

    kaktus-385 resolved

--- customers_lists.js.php
+++ customers_lists.js.php
@@ -24,6 +24,98 @@ if(Dom.get('direct_store_key').value){
 }
 }
 
+
+
+YAHOO.util.Event.addListener(window, "load", function() {
+    tables = new function() {
+
+
+
+
+
+	     //START OF THE TABLE =========================================================================================================================
+
+	    var tableid=0; // Change if you have more the 1 table
+	    var tableDivEL="table"+tableid;
+	    var CustomersColumnDefs = [
+                                        {key:"customer_list_name", label:"<?php echo _('List Name')?>", width:150,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+                                     ,{key:"customer_list_creation_date", label:"<?php echo _('List Created')?>", width:120,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+                                       // ,{key:"id", label:"<?php echo _('Customer Id')?>",  width:80,sortable:true,className:"aright",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+					//,{key:"no_of_customer", label:"<?php echo _('No. Of Customer')?>",  width:180,sortable:true,className:"aright",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+					,{key:"customer_list_type", label:"<?php echo _('List Type')?>",  width:180,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+				    ,{key:"name", label:"<?php echo _('Customer Name')?>", width:190,sortable:true,hidden:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+                                       ,{key:"customer_list_key", label:"<?php echo _('Create Campaign')?>", width:155,sortable:true,className:"aright",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}}
+				   ];
+	    //?tipo=customers&tid=0"
+	    this.dataSource0 = new YAHOO.util.DataSource("ar_customers_list.php?tipo=customers_lists");
+	    this.dataSource0.responseType = YAHOO.util.DataSource.TYPE_JSON;
+	    this.dataSource0.connXhrMode = "queueRequests";
+	    this.dataSource0.responseSchema = {
+		resultsList: "resultset.data", 
+		metaFields: {
+		    rowsPerPage:"resultset.records_perpage",  rtext:"resultset.rtext",
+		    sort_key:"resultset.sort_key",
+		    sort_dir:"resultset.sort_dir",
+		    tableid:"resultset.tableid",
+		    filter_msg:"resultset.filter_msg",
+		    totalRecords: "resultset.total_records" // Access to value in the server response
+		},
+		
+		fields: ["customer_list_name","customer_list_key","customer_list_creation_date","no_of_customer","name","customer_list_type"]};
+		//////fields: [ "id","name","departments","positions","customer_list_name","customer_list_key","customer_list_creation_date","no_of_customer"]};
+             // fields: [ "id","name","departments","positions"]};
+
+
+	    //__You shouls not change anything from here
+
+	    //this.dataSource.doBeforeCallback = mydoBeforeCallback;
+
+
+
+	  this.table0 = new YAHOO.widget.DataTable(tableDivEL, CustomersColumnDefs,
+								   this.dataSource0
+								 , {
+							 renderLoopSize: 50,generateRequest : myRequestBuilder
+							 //,initialLoad:false
+								       ,paginator : new YAHOO.widget.Paginator({
+									      rowsPerPage    : <?php echo$_SESSION['state']['customers']['list']['nr']?>,containers : 'paginator0', 
+ 									      pageReportTemplate : '(<?php echo _('Page')?> {currentPage} <?php echo _('of')?> {totalPages})',
+									      previousPageLinkLabel : "<",
+ 									      nextPageLinkLabel : ">",
+ 									      firstPageLinkLabel :"<<",
+ 									      lastPageLinkLabel :">>",rowsPerPageOptions : [10,25,50,100,250,500]
+									      ,template : "{FirstPageLink}{PreviousPageLink}<strong id='paginator_info0'>{CurrentPageReport}</strong>{NextPageLink}{LastPageLink}"
+
+
+
+									  })
+								     
+								     ,sortedBy : {
+									 key: "<?php echo$_SESSION['state']['customers']['list']['order']?>",
+									 dir: "<?php echo$_SESSION['state']['customers']['list']['order_dir']?>"
+								     },
+								     dynamicData : true
+
+								  }
+								   
+								 );
+	    
+	    this.table0.handleDataReturnPayload =myhandleDataReturnPayload;
+	    this.table0.doBeforeSortColumn = mydoBeforeSortColumn;
+	    this.table0.doBeforePaginatorChange = mydoBeforePaginatorChange;
+
+	    
+	    this.table0.subscribe("dataReturnEvent", data_returned);  
+
+
+	    this.table0.filter={key:'<?php echo$_SESSION['state']['customers']['list']['f_field']?>',value:'<?php echo$_SESSION['state']['customers']['list']['f_value']?>'};
+
+	
+	};
+    });
+
+
+
 function init(){
 
 init_search('customers_store');
