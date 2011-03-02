9bf43070456d953ca2c76661ce9a54316d8237ee
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-03-02 17:01:36 +0530

    templates

--- newsletter_template2_design.php
+++ newsletter_template2_design.php
@@ -1,12 +1,3 @@
-{include file='header.tpl'}
-<div id="bd" >
-
-
-      <h2 style="clear:both">{t}Newsletter1 Template Preview{/t} </h2>
-<div style="border:1px solid #ccc;padding:50px;width:690px">
-<div id="campaign_div">{$msg}</div>
-
-{literal}
 	<STYLE>
 	 .headerTop { background-color:#FFCC66; border-top:0px solid #000000; border-bottom:1px solid #FFFFFF; text-align:center; }
 	 .adminText { font-size:10px; color:#996600; line-height:200%; font-family:verdana; text-decoration:none; }
@@ -18,11 +9,8 @@
 	 .footerText { font-size:10px; color:#996600; line-height:100%; font-family:verdana; }
 	 a { color:#FF6600; color:#FF6600; color:#FF6600; }
 	</STYLE>
-{/literal}
 
-
-<form action="send_mail.php" name="newsletter1_form" id="newsletter1_form" method="POST">	
-<table cellpadding="10" cellspacing="0" bgcolor='#99CC00'style="min-width:600px;" >
+<table width="600" cellpadding="10" cellspacing="0" bgcolor='#fdf4d7' >
 <tr>
 <td valign="top" align="center">
 
@@ -30,7 +18,7 @@
 
 
 <tr>
-<td align="left" valign="middle" style="background-color:#FFFFFF;border-top:0px solid #333333;border-bottom:10px solid #FFFFFF;"><center><h1>{$header}</h1></center></td>
+<td align="left" valign="middle" style="background-color:#FFFFFF;border-top:0px solid #333333;border-bottom:10px solid #FFFFFF;"><center><a href=""><IMG id=editableImg1 SRC="img/email_header_UK_tris.jpg" BORDER="0" title=""  alt="" align="center"></a><h1 style="font-weight:bold;"><?php echo $_SESSION['header']; ?></h1></center></td>
 </tr>
 
 
@@ -39,12 +27,38 @@
 <table width="600" cellpadding="20" cellspacing="0" bgcolor="#FFFFFF">
 <tr>
 
-<td width="200" valign="top" style="background-color:#FFFFFF;border-right:1px dashed #CCCCCC;text-align:left;">
+<td bgcolor="#FFFFFF" valign="top" width="400" style="font-size:12px;color:#000000;line-height:150%;font-family:trebuchet ms;">
+
+<p>
+<span style="font-size:20px;font-weight:bold;color:#CC6600;font-family:arial;line-height:110%;"><?php echo $_SESSION['contenttitle']; ?></span><br>
+
+<span style="font-size:11px;font-weight:normal;color:#666666;font-style:italic;font-family:arial;"><br>
+<img src="<?php echo $_SESSION['image1'] ?>" width=150/>
+<img src="<?php echo $_SESSION['image2'] ?>" width=150/>
+</p>
+
+
+
+<p>
+<br>
+<?php echo $_SESSION['block1']?>
+</p>
+
+</td>
+
+
+<td width="200" valign="top" style="background-color:#FFFFFF;border-left:1px dashed #CCCCCC;text-align:left;">
 <span style="font-size:11px;font-weight:normal;color:#999999;font-family:arial;line-height:150%;">
 
-<span style="font-size:15px;font-weight:bold;color:#222222;font-family:arial;line-height:150%;"></span>
 
-{$block3}
+<?php echo $_SESSION['block2']?>
+<img src="<?php echo $_SESSION['image3'] ?>" width=50/>
+
+
+<br>
+<br>
+
+<?php echo $_SESSION['block3']?>
 
 
 
@@ -52,33 +66,16 @@
 </td>
 
 
-<td bgcolor="#FFFFFF" valign="top" width="400" style="font-size:12px;color:#000000;line-height:150%;font-family:trebuchet ms;">
-
-<p>
-<span style="font-size:20px;font-weight:bold;color:#CC6600;font-family:arial;line-height:110%;">{$contenttitle}</span><br></p>
-<p style="width:450px;">
-{$block1}
-</p>
-<p class="basic_template_img"><img height="107" width="350" src="{$image1}"></p>
-
-
-<p style="width:450px;">
-
-{$block2}
-</p>
-<p class="basic_template_img"><img height="107" width="450" src="{$image2}"></p>
-</td>
-
-
 </tr>
+
 
 <tr>
 <td style="background-color:#FFFFCC;border-top:10px solid #FFFFFF;" valign="top" colspan="2">
 <span style="font-size:10px;color:#996600;line-height:100%;font-family:verdana;">
 *|LIST:DESCRIPTION|* <br />
-
 <br />
 <a href="*|UNSUB|*">Unsubscribe</a> *|EMAIL|* from this list.<br />
+
 <br />
 Our mailing address is:<br />
 *|LIST:ADDRESS|*<br />
@@ -87,7 +84,6 @@ Our telephone:<br />
 *|LIST:PHONE|*<br />
 <br />
 Copyright (C) 2007 *|LIST:COMPANY|* All rights reserved.<br />
-
 <br />
 <a href="*|FORWARD|*">Forward</a> this email to a friend
   
@@ -97,17 +93,6 @@ Copyright (C) 2007 *|LIST:COMPANY|* All rights reserved.<br />
 
 </table>
 
-
-
 </td>
 </tr>
 </table>
-    <input type="submit" name="send_mail" value="Send Mail">
-	<input type="hidden" name="template" value="newsletter1">
-</form>
-</div> 
-
-
-</div>
-
-{include file='footer.tpl'}
