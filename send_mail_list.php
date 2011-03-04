ed4032ef2a6641f72745f61240599f5fef8635e9
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-03-04 18:48:25 +0530

    sendmail

---
+++ send_mail_list.php
@@ -0,0 +1,47 @@
+<?php
+include_once('common.php');
+ini_set('display_errors',1);
+error_reporting(E_ALL|E_STRICT|E_NOTICE);
+
+$general_options_list=array();
+
+$smarty->assign('general_options_list',$general_options_list);
+
+$css_files=array(
+		 $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+		 $yui_path.'build/assets/skins/sam/skin.css',
+		 $yui_path.'menu/assets/skins/sam/menu.css',
+		 'common.css',
+		 'container.css',
+		 'table.css',
+		'css/marketing_campaigns.css'
+		 );
+$js_files=array(
+		$yui_path.'utilities/utilities.js',
+		$yui_path.'json/json-min.js',
+		$yui_path.'paginator/paginator-min.js',
+		$yui_path.'datasource/datasource-min.js',
+		$yui_path.'autocomplete/autocomplete-min.js',
+		$yui_path.'datatable/datatable-min.js',
+		$yui_path.'container/container-min.js',
+		$yui_path.'menu/menu-min.js',
+		'common.js.php',
+		'table_common.js.php','js/edit_common.js','js/csv_common.js',
+		'js/jquery-1.4.4.js'
+		);
+
+
+	if(isset($_REQUEST['check']))
+	{
+			
+	
+	}
+
+
+
+$smarty->assign('css_files',$css_files);
+$smarty->assign('js_files',$js_files);
+
+
+$smarty->display('send_mail_list.tpl');
+?>
---
+++ templates/send_mail_list.tpl
@@ -0,0 +1,43 @@
+{include file='header.tpl'}
+<div id="bd" >
+
+<div class="general_options"></div>
+
+<div style="clear: both; margin: 0pt 0px; padding: 0pt 20px; border-bottom: 1px solid rgb(153, 153, 153);"></div>
+
+<div style="clear: both; margin: 0pt 0px; padding: 0pt 20px; border-bottom: 1px solid #4682b4;"></div>
+
+<div style="clear: both; margin: 0pt 0px;margin-top:-7px; margin-bottom:10px; border-bottom: 1px solid #4682b4;"></div>
+
+<br><br>
+ <form action="" method="">
+	<table border="0" align="center" width="700px">
+		<tr>
+			<td>Select Post Type : </td> <td>
+							<select name="" style="width:150px;"> 
+								<option value="">Catalogue</option> 
+								<option value="">Advert</option>
+								<option value="">Letter</option>
+							</select></td>
+		</tr>
+
+		<tr>
+			<td colspan="2">Enter Subject of Type : </td> 
+				
+		</tr>
+
+		<tr>
+
+			<td colspan="2"><textarea name="content" rows="30" style="width:500px;"></textarea></td>
+		</tr>
+
+		<tr>
+			<td> <input type="submit" name="submit" value="Send"> </td>
+		</tr>
+	</table>
+ </form>
+
+</div>
+
+  
+  {include file='footer.tpl'}
