1b42974afd7922dfa6acc19fd793450164340293
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-03-05 18:58:03 +0530

    asdasfd

---
+++ check_list_table.php
@@ -0,0 +1,16 @@
+<?php
+	include('common.php');
+	
+	$sql = "select * from `Customer List Dimension`";
+	$res = mysql_query($sql);
+	$rw = mysql_fetch_array($res);
+	
+	if(mysql_num_rows($res) == 0)
+	{
+	  echo 0;
+	}
+	else
+	{
+	  echo 1;
+	}
+?>
