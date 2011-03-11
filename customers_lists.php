a9ea81545b65b4b7c38cdefe6555a900c4f90158
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-03-11 16:51:33 +0530

    customer_list

---
+++ customers_lists.php
@@ -0,0 +1,90 @@
+<?php
+include_once('common.php');
+
+if (!$user->can_view('customers') or count($user->stores)==0 ) {
+	
+    header('Location: index.php');
+    exit;
+}
+if (isset($_REQUEST['store']) and is_numeric($_REQUEST['store']) ) {
+    $store_id=$_REQUEST['store'];
+
+} else {
+    $store_id=$_SESSION['state']['customers']['store'];
+
+}
+
+if (!($user->can_view('stores') and in_array($store_id,$user->stores)   ) ) {
+	
+    header('Location: index.php');
+    exit;
+}
+
+
+
+
+
+$css_files=array(
+               $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+               $yui_path.'menu/assets/skins/sam/menu.css',
+               $yui_path.'calendar/assets/skins/sam/calendar.css',
+               $yui_path.'button/assets/skins/sam/button.css',
+               $yui_path.'assets/skins/sam/autocomplete.css',
+               'common.css',
+               'button.css',
+               'container.css',
+               'table.css',
+               'css/marketing_menu.css',
+               'css/marketing_campaigns.css'
+           );
+
+$js_files=array(
+              $yui_path.'utilities/utilities.js',
+              $yui_path.'json/json-min.js',
+              $yui_path.'paginator/paginator-min.js',
+              $yui_path.'datasource/datasource-min.js',
+              $yui_path.'autocomplete/autocomplete-min.js',
+              $yui_path.'datatable/datatable.js',
+              $yui_path.'container/container-min.js',
+              $yui_path.'menu/menu-min.js',
+              'common.js.php',
+              'table_common.js.php',
+              'js/search.js',
+              'customers_lists.js.php',
+              'js/list_function.js',
+              'customer_list_marketing.js.php',
+              'js/menu.js'
+          );
+/*
+$store_options=array();
+$sql=sprintf("select `Store Key`,`Store Code` from `Store Dimension` where `Store Key` in (%s)",join(',',$user->stores));
+$res=mysql_query($sql);
+while($row=mysql_fetch_assoc($res)){
+$store_options[$row['Store Key']]=array('code'=>$row['Store Code']);
+}
+if(count($user->stores)==1){
+$smarty->assign('direct_store_key',array_pop($user->stores));
+
+}
+
+$smarty->assign('store_options',$store_options);
+*/
+
+
+$smarty->assign('parent','customers');
+$smarty->assign('title', _('Customers Lists'));
+$smarty->assign('css_files',$css_files);
+$smarty->assign('js_files',$js_files);
+
+
+$general_options_list[]=array('tipo'=>'url','url'=>'new_customers_list.php?store='.$store_id,'label'=>_('New Customer List'));
+$smarty->assign('general_options_list',$general_options_list);
+$smarty->assign('search_label',_('Customers'));
+$smarty->assign('search_scope','customers');
+
+
+$paginator_menu=array(10,25,50,100,500);
+$smarty->assign('paginator_menu0',$paginator_menu);
+
+$smarty->display('customers_lists.tpl');
+?>
