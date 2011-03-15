3bf8642a08a19bbe7c298ca0d47fd53817f72d8d
primediart <primediart@primediart-desktop.(none)>
2011-03-15 11:05:19 +0530

    changes in staff user login history in users_staff.php

--- ar_users.php
+++ ar_users.php
@@ -41,6 +41,9 @@ case('customer_users'):
  case('loginhistory'):
     list_loginhistory();
     break;
+ case('staff_loginhistory'):
+    list_staff_loginhistory();
+    break;
 
  case('groups'):
     list_groups();
@@ -272,7 +275,7 @@ function list_loginhistory(){
 
 
   $where=sprintf('where true ');
-
+ 
 
   $filter_msg='';
   $wheref='';
@@ -280,7 +283,6 @@ function list_loginhistory(){
     $wheref.=" and  `User Handle` like '".addslashes($f_value)."%'";
   elseif($f_field=='ip' and $f_value!='')
     $wheref.=" and  `IP Address` like '%".addslashes($f_value)."%'";
-  
   $sql="select count(*) as total from `User Log Dimension` UL left join `User Dimension` U on (U.`User Key`=UL.`User Key`)  $where $wheref   ";
   
      $res=mysql_query($sql);
@@ -323,7 +325,7 @@ $order='`Start Date`';
 
    $adata=array();
    $sql="Select *  from `User Log Dimension` UL left join `User Dimension` U on (U.`User Key`=UL.`User Key`)  $where $wheref  order by $order $order_direction limit $start_from,$number_results;";
-   
+// print $sql;  
    $res=mysql_query($sql);
    
    while($row=mysql_fetch_array($res)) {
@@ -367,6 +369,163 @@ $order='`Start Date`';
      
    echo json_encode($response);
 }
+
+function list_staff_loginhistory(){
+ $conf=$_SESSION['state']['users']['loginhistory'];
+  if(isset( $_REQUEST['sf']))
+     $start_from=$_REQUEST['sf'];
+   else
+     $start_from=$conf['sf'];
+   if(isset( $_REQUEST['nr']))
+     $number_results=$_REQUEST['nr'];
+   else
+     $number_results=$conf['nr'];
+  if(isset( $_REQUEST['o']))
+    $order=$_REQUEST['o'];
+  else
+    $order=$conf['order'];
+  if(isset( $_REQUEST['od']))
+    $order_dir=$_REQUEST['od'];
+  else
+    $order_dir=$conf['order_dir'];
+    if(isset( $_REQUEST['f_field']))
+     $f_field=$_REQUEST['f_field'];
+   else
+     $f_field=$conf['f_field'];
+
+  if(isset( $_REQUEST['f_value']))
+     $f_value=$_REQUEST['f_value'];
+   else
+     $f_value=$conf['f_value'];
+  if(isset( $_REQUEST['where']))
+     $where=$_REQUEST['where'];
+   else
+     $where=$conf['where'];
+  
+   if(isset( $_REQUEST['tableid']))
+    $tableid=$_REQUEST['tableid'];
+  else
+    $tableid=0;
+
+   
+ if(isset( $_REQUEST['type']))
+    $type=$_REQUEST['type'];
+  else
+    $type=$conf['type'];
+
+
+ $order_direction=(preg_match('/desc/',$order_dir)?'desc':'');
+   $_order=$order;
+   $_dir=$order_direction;
+   $filter_msg='';
+
+
+  $_SESSION['state']['users']['loginhistory']=array(
+						 'type'=>$type
+						 ,'order'=>$order
+						 ,'order_dir'=>$order_direction
+						 ,'nr'=>$number_results,'sf'=>$start_from,'where'=>$where,'f_field'=>$f_field,'f_value'=>$f_value);
+
+
+
+  $where=sprintf('where true ');
+ 
+
+  $filter_msg='';
+  $wheref='';
+  if($f_field=='user' and $f_value!='')
+    $wheref.=" and  `User Handle` like '".addslashes($f_value)."%'";
+  elseif($f_field=='ip' and $f_value!='')
+    $wheref.=" and  `IP Address` like '%".addslashes($f_value)."%'";
+  $where.=" and `User Type`='Staff'";
+  $sql="select count(*) as total from `User Log Dimension` UL left join `User Dimension` U on (U.`User Key`=UL.`User Key`)  $where $wheref   ";
+ 
+     $res=mysql_query($sql);
+     if($row=mysql_fetch_array($res, MYSQL_ASSOC)) {
+       $total=$row['total'];
+     }
+     mysql_free_result($res);
+     if($wheref==''){
+       $filtered=0;
+       $total_records=$total;
+     } else{
+       $sql="select count(*) as total from `User Log Dimension`  $where   ";
+       $res=mysql_query($sql);
+       if($row=mysql_fetch_array($res, MYSQL_ASSOC)) {
+	 $total_records=$row['total'];
+	 $filtered=$total_records-$total;
+       }
+  mysql_free_result($res);
+   }
+
+     
+   $rtext=$total_records." ".ngettext('user','users',$total_records);
+     if($total_records>$number_results)
+       $rtext_rpp=sprintf("(%d%s)",$number_results,_('rpp'));
+     else
+       $rtext_rpp=_('(Showing all)');
+
+
+     $translations=array('handle'=>'`User Handle`');
+     if(array_key_exists($order,$translations))
+       $order=$translations[$order];
+     
+       
+     switch($order){
+case 'login_date':
+default:
+$order='`Start Date`';
+
+}
+
+   $adata=array();
+   $sql="Select *  from `User Log Dimension` UL left join `User Dimension` U on (U.`User Key`=UL.`User Key`)  $where $wheref  order by $order $order_direction limit $start_from,$number_results;";
+// print $sql;  
+   $res=mysql_query($sql);
+   
+   while($row=mysql_fetch_array($res)) {
+     if($row['Logout Date']==""){
+              $adata[]=array(
+		   'user'=>$row['User Handle'],
+		   'ip'=>$row['IP'],
+                   'login_date'=>strftime("%c",strtotime($row['Start Date'])),
+                   'logout_date'=>"",
+		   );}
+   else
+      { $adata[]=array(
+		   'user'=>$row['User Handle'],
+		   'ip'=>$row['IP'],
+                   'login_date'=>strftime("%c",strtotime($row['Start Date'])),
+                   'logout_date'=>strftime("%c",strtotime($row['Logout Date'])),
+		   );}
+
+   }
+  mysql_free_result($res);
+
+   $response=array('resultset'=>
+		   array('state'=>200,
+			 'data'=>$adata,
+	 'sort_key'=>$_order,
+			 'sort_dir'=>$_dir,
+			 'tableid'=>$tableid,
+			 'filter_msg'=>$filter_msg,
+			 'total_records'=>$total,
+			 'records_offset'=>$start_from,
+			 'records_returned'=>$total,
+			 'records_perpage'=>$number_results,
+			 'records_text'=>$rtext,
+			 'records_order'=>$order,
+			 'records_order_dir'=>$order_dir,
+			 'filtered'=>$filtered,
+			 'rtext'=>$rtext,
+			'rtext_rpp'=>$rtext_rpp
+			 )
+		   );
+     
+   echo json_encode($response);
+}
+
+
 function list_groups(){
 $conf=$_SESSION['state']['users']['groups'];
   if(isset( $_REQUEST['sf']))
