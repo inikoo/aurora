b7be491b2b32ce3cb9f63d42216955b20d1bdc76
Satyajit Mondal <satyajit.primediart@gmail.com>
2011-04-09 18:45:55 +0530

    change

--- warehouse.php
+++ warehouse.php
@@ -45,25 +45,10 @@ $css_files=array(
 		 //		 $yui_path.'datatable/assets/skins/sam/datatable.css',
 		 
 		 'button.css',
-		 'container.css',
-		
-		 'css/dropdown.css'
+		 'container.css'
 		 );
 
-$theme="";
-if($theme)
-{
-array_push($css_files, 'themes_css/'.$Themecss1);   
-array_push($css_files, 'themes_css/'.$Themecss2);
-array_push($css_files, 'themes_css/'.$Themecss3);
-}    
-  
-
-else{
-array_push($css_files, 'common.css'); 
-
-array_push($css_files, 'table.css');
-}
+include_once('Theme.php');
 
 $js_files=array(
 
