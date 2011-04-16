c2930f3849496702b50c304eb047fcb75647ca85
Amit Das Gupta <amit.primediart@gmail.com>
2011-04-16 16:06:38 +0530

    change

--- Theme.php
+++ Theme.php
@@ -3,16 +3,15 @@ $user_key=$_SESSION['user_key'];
 $themeSql="select * from  `User Dimension` inner join `Theme Dimension`  on (`User Dimension`.`User Themes`=`Theme Dimension`.`Theme Key`) where `User Key`=$user_key";
 
 $themeResult=mysql_query($themeSql);
-
+$background_status='';
+$themeRow='';
+$Theme_css='';
+$bg='';
 //print_r(mysql_fetch_array($themeResult));
 
 if ($themeRow=mysql_fetch_array($themeResult)) 
 {
-$ThemeCommon=$themeRow['Theme Common Css'];
-$ThemeTable=$themeRow['Theme Table Css'];
-$ThemeIndex=$themeRow['Theme Index Css'];
-$ThemeDropdown=$themeRow['Theme Dropdown Css'];
-$ThemeCampaign=$themeRow['Theme Campaign Css'];
+$Theme_css=$themeRow['Theme Css'];
 $background_status=$themeRow['User Theme Background Status'];
 }
 $bg=$user_key.".png";
@@ -20,22 +19,24 @@ if($themeRow)
 {
 if($background_status)
 {
-
-
-array_push($css_files, 'themes_css/'.$ThemeCommon.'?c='.$bg);   
-array_push($css_files, 'themes_css/'.$ThemeTable);
-array_push($css_files, 'themes_css/'.$ThemeIndex); 
-array_push($css_files, 'themes_css/'.$ThemeDropdown);
-array_push($css_files, 'themes_css/'.$ThemeCampaign);
-
+array_push($css_files, 'table.css'); 
+array_push($css_files, 'css/index.css');
+array_push($css_files, 'css/dropdown.css');
+array_push($css_files, 'common.css'); 
+array_push($css_files, 'css/'.$Theme_css.'?c='.$bg);   
 }
 else
 {
-array_push($css_files, 'themes_css/'.$ThemeCommon);   
-array_push($css_files, 'themes_css/'.$ThemeTable);
-array_push($css_files, 'themes_css/'.$ThemeIndex); 
-array_push($css_files, 'themes_css/'.$ThemeDropdown);
-array_push($css_files, 'themes_css/'.$ThemeCampaign);
+//array_push($css_files, 'themes_css/'.$ThemeCommon);  
+array_push($css_files, 'common.css');
+array_push($css_files, 'table.css'); 
+//array_push($css_files, 'themes_css/'.$ThemeTable);
+array_push($css_files, 'css/index.css');
+array_push($css_files, 'css/dropdown.css'); 
+//array_push($css_files, 'themes_css/'.$ThemeIndex); 
+//array_push($css_files, 'themes_css/'.$ThemeDropdown);
+array_push($css_files, 'css/'.$Theme_css);
+
 
 }
 }    
@@ -46,5 +47,6 @@ array_push($css_files, 'common.css');
 array_push($css_files, 'css/dropdown.css'); 
 array_push($css_files, 'css/index.css');
 array_push($css_files, 'table.css');
+array_push($css_files, 'marketing_campaigns.css');
 }
 ?>
