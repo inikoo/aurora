c11201ee1ab7155dcc8e2813173ddb3e6f91faed
Raul Perusquia <rulovico@gmail.com>
2011-06-10 12:13:56 +0100

    request emails tris

---
+++ app_files/keys/bugs_key.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$conection_data=array(
+    'email'=>"bugs@inikoo.com",
+    'smtp_host'=>"smtp.gmail.com",
+    'smtp_user'=>"bugs@inikoo.com",
+    'smtp_password'=>"NKcyvqzoH2",
+    'smtp_port'=>465,
+    'smtp_ssl'=>1
+);
+
+
+?>
