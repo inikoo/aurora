86f0c06f48eb7a575cd0b5860686eb1b0768692e
Raul Perusquia <rulovico@gmail.com>
2011-06-13 10:23:51 +0100

    report issues bug fixing 2

--- report_issue.js.php
+++ report_issue.js.php
@@ -23,7 +23,7 @@ type:
  YAHOO.util.Connect.asyncRequest('POST',request , {
   
 success:function(o) {
-  alert(o.responseText)
+  //alert(o.responseText)
             var r =  YAHOO.lang.JSON.parse(o.responseText);
             if (r.state==200) {
                 Dom.get('send_from').style.display='none';
