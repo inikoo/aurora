c224606f2d5d5878a253d566e83a1936d676c1ab
Raul Perusquia <rulovico@gmail.com>
2011-07-26 12:43:22 +0100

    fix bug in reacording deliver address

--- edit_address.js.php
+++ edit_address.js.php
@@ -247,7 +247,7 @@ var create_address=function(options) {
   
   YAHOO.util.Connect.asyncRequest('POST',request , {
 success:function(o) {
-             //	alert(o.responseText);
+         //   	alert(o.responseText);
             var r =  YAHOO.lang.JSON.parse(o.responseText);
             if (r.action=='created') {
 
