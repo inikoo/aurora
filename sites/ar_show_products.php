e8515e59920d53c43baca80a7489e09925eb384b
Migara <migara@inikoo.com>
2011-08-04 14:58:46 +0100

    info functions

--- sites/ar_show_products.php
+++ sites/ar_show_products.php
@@ -1,5 +1,108 @@
 <?php
 
+function show_products_in_family_info($data){
+
+		$_form=sprintf('<div style="position:absolute; left:0px; top:487px; width:192px; height:26px;">
+						<div style="text-align:left;">
+						<link rel="stylesheet" type="text/css" href="../order.css" />
+						<link rel="stylesheet" type="text/css" href="order.css" />
+						<style type="text/css">.nophp{display:none}</style>
+						<style type="text/css">
+						table.order {width:22em}td.first{width:8 em}
+						table.order{font-size:11px;font-family:arial;}
+						span.price{float:right;margin-right:5px}
+						span.desc{margin-left:5px}
+						span.outofstock{color:red;font-weight:800;float:right;margin-right:5px;}
+						input.qty{width:100%%}td.qty{width:3em}
+						</style>
+
+						<table class="order" border=0 cellpadding="0" cellspacing="0">');
+				
+				
+		$form=sprintf('<table class="order" border=0 cellpadding="0" cellspacing="0">'
+						);
+					
+	foreach($data as $product){
+		$i=1;
+		$locale=$product['Product Locale'];
+		if ($locale=='de_DE') {
+			$out_of_stock='nicht vorrv§tig';
+			$discontinued='ausgelaufen';
+		}if ($locale=='de_DE') {
+			$out_of_stock='nicht vorrv§tig';
+			$discontinued='ausgelaufen';
+		}
+		elseif($locale=='es_ES') {
+			$out_of_stock='Fuera de Stock';
+			$discontinued='Fuera de Stock';
+		}
+		elseif($locale=='fr_FR') {
+			$out_of_stock='Rupture de stock';
+			$discontinued='Rupture de stock';
+		}
+		else {
+			$out_of_stock='Out of Stock';
+			$discontinued='Discontinued';
+		}
+		  
+		
+		if ($product['Product Web State']=='Online Force Out of Stock'){
+					$_form.=sprintf('<tr class="nophp">
+							<td colspan=2 style="height:20px;padding:0;margin:0;"><span  style="float:right;font-size:8pt;color:red;font-weight:800;">%s</span>%s</td>
+							<td><span class="desc">%s</span></td>
+							</tr>'
+							,$out_of_stock
+							,addslashes($product['Product Code'])
+							,clean_accents(addslashes($product['Product Name']))
+							);
+							
+					$form.=sprintf('<tr ><td colspan=2 style="height:20px;padding:0;margin:0;"><span style="float:right;font-size:8pt;color:red;font-weight:800;">%s</span>%s</td>
+							<td><span class="desc">%s</span></td></tr>'
+							,$out_of_stock
+							,addslashes($product['Product Code'])
+							,clean_accents(addslashes($product['Product Name']))
+
+							);
+			}
+			else{
+					$_form.=sprintf('<tr class="nophp">
+							<td class="first"><span class="price">%.2f</span>%s</td>
+							<td><span class="desc">%s</span></td>
+							</tr>'
+							,$product['Product Price']
+							,addslashes($product['Product Code'])
+							,clean_accents(addslashes($product['Product Name']))
+							);
+							
+					$form.=sprintf('<tr ><td class="first"><span class="price">%.2f</span>%s</td>
+							
+							<td><span class="desc">%s</span></td></tr>'
+							,$product['Product Price']
+							,addslashes($product['Product Code'])
+							,clean_accents(addslashes($product['Product Name']))
+
+
+							);
+			
+			 }
+
+
+
+			  
+			
+			$i++;		
+		}
+	
+		$_form.=sprintf('</table>');
+	
+	
+		$form.=sprintf('</table>');
+	
+		  //print $form;exit;
+		  return $_form.$form.'</div></div>';
+}
+
+
 function show_products_in_family($type, $data, $conf){
 	//print_r($data);
 	
