364f23ce4460965d8858cbe6aba83aea52a0d3b4
Migara <migara@inikoo.com>
2011-08-12 11:59:45 +0100

    sites

---
+++ sites/footer.php
@@ -0,0 +1,14 @@
+<?php
+$footer=<<<EOD
+<div id="footer"><span class="footer_last_update">Last Update: 11Aug11</span>
+<div id="contact">
+<span class="footer_address">Ancient Wisdom, BLOCK B,</span><br>
+<span class="footer_address">Parkwood Business Park, Sheffield, S3 8AL, UK</span><br>
+<span class="footer_address">Tel -<wbr> 0114 2729 165</span>
+</div>
+</div>
+<span class="footer_description">Wholesale giftware supplier. Please note this is a wholesale site we supply wholesale to the trade.</span>
+EOD;
+
+?>
+
