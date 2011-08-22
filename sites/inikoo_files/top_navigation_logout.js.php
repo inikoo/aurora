7d9dbcd6c96aee772e0b878e6c0aac451f915691
Migara <migara@inikoo.com>
2011-08-22 15:12:54 +0100

    remember me

--- sites/inikoo_files/top_navigation_logout.js.php
+++ sites/inikoo_files/top_navigation_logout.js.php
@@ -330,10 +330,10 @@ function login(){
 //Dom.get('login_password').value='';
     //Dom.get('loginform').submit();
      var request='../../inikoo_files/ar_login.php?ep='+encodeURIComponent(epwd)+'&login_handle='+input_login+'&store_key='+store_key+'&site_key='+site_key+'&remember_me='+remember_me;
-      alert(request);
+      //alert(request);
     	YAHOO.util.Connect.asyncRequest('POST',request ,{
 		success:function(o) {
-		alert(o.responseText)
+		//alert(o.responseText)
 		var r =  YAHOO.lang.JSON.parse(o.responseText);
 		    if(r.state=='200'){
 			
