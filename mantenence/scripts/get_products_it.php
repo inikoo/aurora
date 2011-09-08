fac9b47e5e94425e95bf8cf204a2fb3a0ae5f3cd
Raul Perusquia <rulovico@gmail.com>
2011-09-08 20:20:16 +0100

    fix small bug

--- mantenence/scripts/get_products_it.php
+++ mantenence/scripts/get_products_it.php
@@ -397,7 +397,7 @@ $codigos[$code]=1;
 		'product record type'=>'Normal',
 		'Product Web Configuration'=>'Online Auto',
 
-		  
+		    'product stage'=>'Normal',
 		'product price'=>sprintf("%.2f",$price),
 		'product rrp'=>$rrp,
 		'product units per case'=>$units,
