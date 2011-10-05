949894cc0142e87e44c609ce96b2dd18d0c861f7
Raul Perusquia <rulovico@gmail.com>
2011-10-05 17:07:48 +0100

    xx

--- mantenence/scripts/get_orders_db.php
+++ mantenence/scripts/get_orders_db.php
@@ -123,7 +123,7 @@ $sql="select *,replace(   replace(replace(replace(replace(replace(replace(replac
 //$sql="select * from  orders_data.orders where filename like '%/122384ref%.xls'   order by filename";
 //120239
 //120217
-$sql="select * from  orders_data.orders where filename like '%/134868.xls'   order by filename";
+//$sql="select * from  orders_data.orders where filename like '%/134868.xls'   order by filename";
 
 //$sql="select * from  orders_data.orders where filename like '%/%ref%.xls'   order by filename";
 //$sql="select * from  orders_data.orders  where filename like '/mnt/%/Orders/93284.xls' order by filename";
