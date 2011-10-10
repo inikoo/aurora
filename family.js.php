008140fdd0da296347d9b515c59460f94597caad
Raul Perusquia <rulovico@gmail.com>
2011-10-10 14:33:53 +0100

    fix bug page list

--- family.js.php
+++ family.js.php
@@ -202,7 +202,7 @@ YAHOO.util.Event.addListener(window, "load", function() {
 				    
 				     ];
 
-	    this.dataSource4 = new YAHOO.util.DataSource("ar_sites.php?tipo=pages&parent=family&tableid=4&parent_key="+Dom.get('family_key').value);
+	    this.dataSource4 = new YAHOO.util.DataSource("ar_sites.php?tipo=pages&sf=0&parent=family&tableid=4&parent_key="+Dom.get('family_key').value);
 	    this.dataSource4.responseType = YAHOO.util.DataSource.TYPE_JSON;
 	    this.dataSource4.connXhrMode = "queueRequests";
 	    this.dataSource4.responseSchema = {
