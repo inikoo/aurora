4ae54f78b304c3c1bb8c217033ad891ef8957ea1
Raul Perusquia <rulovico@gmail.com>
2011-10-10 20:25:14 +0100

    suppleirs sales data

--- mantenence/scripts/update_assets_sales.php
+++ mantenence/scripts/update_assets_sales.php
@@ -15,30 +15,50 @@ date_default_timezone_set('UTC');
 
 $con=@mysql_connect($dns_host,$dns_user,$dns_pwd );
 
-if(!$con){print "Error can not connect with database server\n";exit;}
+if (!$con) {
+    print "Error can not connect with database server\n";
+    exit;
+}
 //$dns_db='dw_avant';
 $db=@mysql_select_db($dns_db, $con);
-if (!$db){print "Error can not access the database\n";exit;}
-  
+if (!$db) {
+    print "Error can not access the database\n";
+    exit;
+}
+
 
 require_once '../../common_functions.php';
 mysql_query("SET time_zone ='+0:00'");
 mysql_query("SET NAMES 'utf8'");
-require_once '../../conf/conf.php';           
+require_once '../../conf/conf.php';
 setlocale(LC_MONETARY, 'en_GB.UTF-8');
 
 global $myconf;
+
+$sql="select * from `Supplier Dimension`";
+
+$result=mysql_query($sql);
+while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+
+    $supplier=new Supplier($row['Supplier Key']);
+   
+    $supplier->update_up_today_sales();
+    $supplier->update_interval_sales();
+    $supplier->update_last_period_sales();
+    print "Supplier ".$supplier->data['Supplier Code']."\r";
+}
+
 
 $sql="select * from `Product Family Dimension`";
 $result=mysql_query($sql);
 while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 
     $family=new Family($row['Product Family Key']);
-      $family->update_product_data();
+    $family->update_product_data();
     $family->update_up_today_sales();
     $family->update_interval_sales();
     $family->update_last_period_sales();
-  print "Family ".$family->data['Product Family Code']."\r";
+    print "Family ".$family->data['Product Family Code']."\r";
 
 }
 
@@ -59,30 +79,30 @@ while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 
 $sql="select * from `Product Department Dimension`  ";
 $result=mysql_query($sql);
-while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
-  
-$department=new Department($row['Product Department Key']);
+while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+
+    $department=new Department($row['Product Department Key']);
 
     $department->update_sales_default_currency();
 
-  $department->update_customers();
-  $department->update_sales_data();
-  $department->update_product_data();
-  $department->update_families();
- 
+    $department->update_customers();
+    $department->update_sales_data();
+    $department->update_product_data();
+    $department->update_families();
 
-  print "Department ".$department->data['Product Department Code']."\r";
- }
+
+    print "Department ".$department->data['Product Department Code']."\r";
+}
 
 
 $sql="select * from `Part Dimension`  ";
 $result=mysql_query($sql);
-while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
-  $part=new Part('sku',$row['Part SKU']);
+while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+    $part=new Part('sku',$row['Part SKU']);
 
-  $part->update_up_today_sales();
-$part->update_interval_sales();
-$part->update_last_period_sales();
+    $part->update_up_today_sales();
+    $part->update_interval_sales();
+    $part->update_last_period_sales();
 
 }
 
@@ -100,35 +120,26 @@ while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 
 }
 
-$sql="select * from `Supplier Dimension`";
+
+
+$sql="select `Product ID` from `Product Dimension` ";
 $result=mysql_query($sql);
-while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
-
-    $supplier=new Supplier($row['Supplier Key']);
-    $supplier->update_up_today_sales();
-    $supplier->update_interval_sales();
-    $supplier->update_last_period_sales();
-
-}
-
- $sql="select `Product ID` from `Product Dimension` ";
-$result=mysql_query($sql);
-while($row=mysql_fetch_array($result)   ){
- $product=new Product('pid',$row['Product ID']);
+while ($row=mysql_fetch_array($result)   ) {
+    $product=new Product('pid',$row['Product ID']);
 //$product=new Product('pid',37949);
- $product->update_sales_data();
- // $product->update_full_search();
+    $product->update_sales_data();
+// $product->update_full_search();
 //  print $row['Product ID']."\t\t ".$product->data['Product Code']." \r";
 
 }
 
 
- $sql="select * from `Product History Dimension` PH  order by `Product Key` desc  ";
+$sql="select * from `Product History Dimension` PH  order by `Product Key` desc  ";
 $result=mysql_query($sql);
-while($row=mysql_fetch_array($result)   ){
-  $product=new Product('id',$row['Product Key']);
- $product->update_historic_sales_data();
-  //print "PH ".$row['Product Key']."\t\t ".$product->data['Product Code']." \r";
+while ($row=mysql_fetch_array($result)   ) {
+    $product=new Product('id',$row['Product Key']);
+    $product->update_historic_sales_data();
+    //print "PH ".$row['Product Key']."\t\t ".$product->data['Product Code']." \r";
 
 }
 
