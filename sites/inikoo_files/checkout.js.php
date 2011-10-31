969d8331fe2ff1b4889a0288fb26981271d928d5
Migara <migara@inikoo.com>
2011-10-31 17:57:54 +0000

    paypal

--- sites/inikoo_files/checkout.js.php
+++ sites/inikoo_files/checkout.js.php
@@ -1,13 +1,89 @@
 var order_key="<?php echo $_REQUEST['order_key']?>"
 
+var data={ 
+    "firstName":''
+    ,"lastName":''
+    ,"company":''
+	,"address1":''
+	,"address2":''
+	,"city":''
+	,"state":''
+	,"zip":''
+	,"country":''
+	,"phone":''
+	,"CCNo":''
+	,"CVV2":''
+	,"CCType":''
+	,"CCExpiresMonth":''
+	,"CCExpiresYear":''
+	,"contactEmail":''
+	,"notes":''
+    };
+
+
 function init(){
 
 Event.addListener("payment_option", "click", payment_option);
+Event.addListener("Submit_CC", "click", Submit_CC);
+Event.addListener("Save_CC", "click", Save_CC);
 
 
 
 
+}
 
+function Save_CC(){
+	alert('save');
+}
+
+function Submit_CC(){
+	data['firstName']=Dom.get('firstName').value;
+	data['lastName']=Dom.get('lastName').value;
+	data['company']=Dom.get('company').value;
+	data['address1']=Dom.get('address1').value;
+	data['address2']=Dom.get('address2').value;
+	data['city']=Dom.get('city').value;
+	data['state']=Dom.get('state').value;
+	data['zip']=Dom.get('zip').value;
+	data['country']=Dom.get('country').value;
+	data['phone']=Dom.get('phone').value;
+	data['CCNo']=Dom.get('CCNo').value;
+	data['CVV2']=Dom.get('CVV2').value;
+	data['CCType']=Dom.get('CCType').value;
+	data['CCExpiresMonth']=Dom.get('CCExpiresMonth').value;
+	data['CCExpiresYear']=Dom.get('CCExpiresYear').value;
+	data['contactEmail']=Dom.get('contactEmail').value;
+	data['notes']=Dom.get('notes').value;
+
+
+	var json_value = my_encodeURIComponent(YAHOO.lang.JSON.stringify(data)); 
+	var ar_file=path+'inikoo_files/ar_edit_orders.php';
+	request='tipo=cc_payment&json_values='+json_value;
+//alert(request)
+	YAHOO.util.Connect.asyncRequest(
+				    'POST',
+				    ar_file, {
+					success:function(o) {
+					    alert(o.responseText);
+					    var r = YAHOO.lang.JSON.parse(o.responseText);
+					    if (r.state == 200) {
+						alert('ok');
+							
+				
+					    } else {
+						alert(r.L_LONGMESSAGE0);
+						//	callback();
+					    }
+					},
+					    failure:function(o) {
+					    alert(o.statusText);
+					    // callback();
+					},
+					    scope:this
+					    },
+				    request
+				    
+				    );  	
 }
 
 function getCheckedValue(radioObj) {
