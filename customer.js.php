50a253dbe18b07c7ce7b86af9027a8d5622d6fe5
Migara <migara@inikoo.com>
2011-11-01 17:23:45 +0000

    quick edit

--- customer.js.php
+++ customer.js.php
@@ -1537,20 +1537,26 @@ Event.addListener('quick_edit_main_fax', "click", dialog_quick_edit_Customer_Mai
 
 <?php
 
-	foreach($customer->get_other_telephones_data() as $key=>$value)
+	foreach($customer->get_other_telephones_data() as $key=>$value){
 	printf("Event.addListener('quick_edit_other_telephone%d', \"click\", dialog_quick_edit_Customer_Telephone%d.show,dialog_quick_edit_Customer_Telephone%d , true);", $key, $key, $key);	
+	printf("Event.addListener('close_quick_edit_telephone%d', \"click\", dialog_quick_edit_Customer_Telephone%d.hide,dialog_quick_edit_Customer_Telephone%d , true);", $key, $key, $key);	
+	}
 ?>
 
 <?php
 
-	foreach($customer->get_other_mobiles_data() as $key=>$value)
+	foreach($customer->get_other_mobiles_data() as $key=>$value){
 	printf("Event.addListener('quick_edit_other_mobile%d', \"click\", dialog_quick_edit_Customer_Mobile%d.show,dialog_quick_edit_Customer_Mobile%d , true);", $key, $key, $key);	
+	printf("Event.addListener('close_quick_edit_other_mobile%d', \"click\", dialog_quick_edit_Customer_Mobile%d.hide,dialog_quick_edit_Customer_Mobile%d , true);", $key, $key, $key);	
+	}
 ?>
 
 <?php
 
-	foreach($customer->get_other_faxes_data() as $key=>$value)
+	foreach($customer->get_other_faxes_data() as $key=>$value){
 	printf("Event.addListener('quick_edit_other_fax%d', \"click\", dialog_quick_edit_Customer_FAX%d.show,dialog_quick_edit_Customer_FAX%d , true);", $key, $key, $key);	
+	printf("Event.addListener('close_quick_edit_other_fax%d', \"click\", dialog_quick_edit_Customer_FAX%d.hide,dialog_quick_edit_Customer_FAX%d , true);", $key, $key, $key);	
+	}
 ?>
 	
 /*
@@ -1585,8 +1591,13 @@ Event.addListener('save_quick_edit_email', "click", save_quick_edit_email, true)
 Event.addListener('close_quick_edit_email', "click", dialog_quick_edit_Customer_Main_Email.hide,dialog_quick_edit_Customer_Main_Email , true);
 
 Event.addListener('save_quick_edit_telephone', "click", save_quick_edit_telephone, true);
+Event.addListener('close_quick_edit_telephone', "click", dialog_quick_edit_Customer_Main_Telephone.hide,dialog_quick_edit_Customer_Main_Telephone , true);
+
 Event.addListener('save_quick_edit_mobile', "click", save_quick_edit_mobile, true);
+Event.addListener('close_quick_edit_mobile', "click", dialog_quick_edit_Customer_Main_Mobile.hide,dialog_quick_edit_Customer_Main_Mobile , true);
+
 Event.addListener('save_quick_edit_fax', "click", save_quick_edit_fax, true);
+Event.addListener('close_quick_edit_fax', "click", dialog_quick_edit_Customer_Main_FAX.hide,dialog_quick_edit_Customer_Main_FAX , true);
 
 	var customer_email_oACDS = new YAHOO.util.FunctionDataSource(validate_customer_email);
     customer_email_oACDS.queryMatchContains = true;
