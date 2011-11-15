9e6183828fb28a6391a71e5357ccd658e36a9ec7
Migara <migara@inikoo.com>
2011-11-15 17:07:59 +0000

    files

--- sites/inikoo_files/common_splinter.php
+++ sites/inikoo_files/common_splinter.php
@@ -28,6 +28,14 @@ $smarty->compile_dir = 'server_files/smarty/templates_c';
 $smarty->cache_dir = 'server_files/smarty/cache';
 $smarty->config_dir = 'server_files/smarty/configs';
 
+putenv('LC_ALL=es_ES');
+setlocale(LC_ALL, 'es_ES');
+
+// Specify location of translation tables
+bindtextdomain("inikoo", "./locale");
+
+
+
 global $version, $version_flag;
 $version = ieversion();
 
