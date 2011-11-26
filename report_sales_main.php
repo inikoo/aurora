4ff9262b29852c259037350fe679d7359ff06028
Raul Perusquia <rulovico@gmail.com>
2011-11-26 18:39:51 +0000

    fix display errros

--- report_sales_main.php
+++ report_sales_main.php
@@ -8,17 +8,12 @@ $css_files=array(
                $yui_path.'menu/assets/skins/sam/menu.css',
                $yui_path.'calendar/assets/skins/sam/calendar.css',
                $yui_path.'button/assets/skins/sam/button.css',
-               //		 $yui_path.'datatable/assets/skins/sam/datatable.css',
-
-               'button.css',
+               'common.css',
                'container.css',
-
-               'css/calendar.css',
-
-
+               'button.css',
+               'table.css',
+               'theme.css.php'
            );
-
-$css_files[]='theme.css.php';
 
 
 $js_files=array(
