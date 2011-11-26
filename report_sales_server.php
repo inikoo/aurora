e1228e14ee9ba9ec32575838750bdb5bc790ad2e
Raul Perusquia <rulovico@gmail.com>
2011-11-26 18:39:51 +0000

    fix display errros

--- report_sales_server.php
+++ report_sales_server.php
@@ -28,15 +28,16 @@ else
 
 
 $css_files=array(
-		 $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
-		 $yui_path.'menu/assets/skins/sam/menu.css',
-		 $yui_path.'calendar/assets/skins/sam/calendar.css',
-		 $yui_path.'button/assets/skins/sam/button.css',
-		
-		 'container.css'
-		 );
-
-$css_files[]='theme.css.php';
+               $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+               $yui_path.'menu/assets/skins/sam/menu.css',
+               $yui_path.'calendar/assets/skins/sam/calendar.css',
+               $yui_path.'button/assets/skins/sam/button.css',
+               'common.css',
+               'container.css',
+               'button.css',
+               'table.css',
+               'theme.css.php'
+           );
 
 
 $js_files=array(
