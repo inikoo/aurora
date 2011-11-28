6028ad91b3a4ec949b030a9de2cf407fca857dfb
Raul Perusquia <rulovico@gmail.com>
2011-11-28 09:11:11 +0000

    xx2

--- terms_use.php
+++ terms_use.php
@@ -24,14 +24,17 @@ $smarty->assign('cancel_email',$cancel_email);
 
 
 $css_files=array(
-		 $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
-		 $yui_path.'menu/assets/skins/sam/menu.css',
-		// $yui_path.'calendar/assets/skins/sam/calendar.css',
-		// $yui_path.'button/assets/skins/sam/button.css',
-		 'common.css',
-		 //'button.css',
-		//'container.css',
-		 'css/terms_use.css'
+              $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+               $yui_path.'menu/assets/skins/sam/menu.css',
+               $yui_path.'assets/skins/sam/autocomplete.css',
+               $yui_path.'calendar/assets/skins/sam/calendar.css',
+               'common.css',
+               'container.css',
+               'button.css',
+               'table.css',
+            
+		 'css/terms_use.css',
+		    'theme.css.php'
 		 );
 $js_files=array(
 
