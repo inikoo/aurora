0bdfb7f01bfb1a5761852e83221ae46a581c4f86
Raul Perusquia <rulovico@gmail.com>
2011-12-03 17:59:31 +0000

    x

---
+++ class.DummyPage.php
@@ -0,0 +1,32 @@
+<?php
+class Dummy_Page {
+
+
+
+    public function get($key) {
+        return $key;
+
+
+    }
+
+
+    function get_found_in() {
+
+        return array(array('link'=>'Parent Page'));
+
+    }
+
+    function get_see_also() {
+        return array(array('link'=>'<a href="#">Link 1</a>'),array('link'=>'<a href="#">Link 2</a>'),array('link'=>'<a href="#">Link 3</a>'),array('link'=>'<a href="#">Link 4</a>'));
+    }
+
+    function display_title() {
+        return 'Header Title';
+    }
+
+    function display_label() {
+        return 'Code';
+    }
+    function display_top_bar() {}
+}
+?>
