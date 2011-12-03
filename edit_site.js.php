c2b70396c4bb227972482da211678f45353525d3
Raul Perusquia <rulovico@gmail.com>
2011-12-03 17:15:03 +0000

    footer & header manegments 2

--- edit_site.js.php
+++ edit_site.js.php
@@ -446,12 +446,12 @@ function upload_footer(e){
     
    var uploadHandler = {
       upload: function(o) {
-	   //alert(o.responseText)
+	   alert(o.responseText)
 	    var r =  YAHOO.lang.JSON.parse(o.responseText);
 	   
 	    if(r.state==200){
 	     
-         window.location.reload()
+         //window.location.reload()
                 
 	    }
 	    else{
