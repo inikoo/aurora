dfa73973e0054374d7b9941297457065bf258268
Migara <migara@inikoo.com>
2012-01-25 14:56:55 +0000

    dashboard

--- edit_dashboards.js.php
+++ edit_dashboards.js.php
@@ -80,20 +80,20 @@ Event.addListener(window, "load", function() {
             var Event = YAHOO.util.Event;
             var DDM = YAHOO.util.DragDropMgr;
 		OrdersColumnDefs = [
-				   //    {key:"id", label:"<?php echo _('Widget ID')?>", width:100,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}},
+				      // {key:"id", label:"<?php echo _('Widget ID')?>", width:100,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}},
 				       {key:"name", label:"<?php echo _('Widget Name')?>", width:100,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_DESC}},
 				     //  {key:"widget_block",label:"<?php echo _('Widget Block')?>", width:240,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}},
 				      // {key:"widget_dimesnion", label:"<?php echo _('Widget Dimesnion')?>", width:205,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_ASC}},
 				       {key:"description", label:"<?php echo _('Widget Description')?>", width:110,sortable:true,className:"aleft",sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_DESC}},
 				       {key:"delete", label:"",width:12,sortable:false,action:'delete',object:'widget_list'},
-					{key:"key", label:"",width:12,sortable:false, isPrimaryKey:true,hidden:true},
+					 {key:"id", label:"",width:100,hidden:true}
 
 					 ];
 
 //var ids =Array("restrictions_orders_cancelled","restrictions_orders_suspended","restrictions_orders_unknown","restrictions_orders_dispatched","restrictions_orders_in_process","restrictions_all_orders") ;
 
-		//alert("ar_edit_dashboard.php?tipo=list_widgets&user_id="+Dom.get('user_key').value+"&where=");
-	    this.dataSource0 = new YAHOO.util.DataSource("ar_edit_dashboard.php?tipo=list_widgets&user_id="+Dom.get('user_key').value+"&where=");
+		//alert("ar_dashboard.php?tipo=list_widgets&user_id="+Dom.get('user_key').value+"&where=");
+	    this.dataSource0 = new YAHOO.util.DataSource("ar_dashboard.php?tipo=list_widgets&user_id="+Dom.get('user_key').value+"&where=");
 	    this.dataSource0.responseType = YAHOO.util.DataSource.TYPE_JSON;
 	    this.dataSource0.connXhrMode = "queueRequests";
 	    this.dataSource0.responseSchema = {
@@ -143,116 +143,7 @@ Event.addListener(window, "load", function() {
 
 						     }
 						     );
-//////////////////
-	    var myDTDTargets = {};
-            var onRowSelect = function(ev) {
-                    var par = this.table0.getTrEl(Event.getTarget(ev)),
-                        srcData,
-                        srcIndex,
-                        tmpIndex = null,
-                        ddRow = new YAHOO.util.DDProxy(par.id);
 
-                    ddRow.handleMouseDown(ev.event);
-
-
-                    /**
-                    * Once we start dragging a row, we make the proxyEl look like the src Element. We get also cache all the data related to the
-                    * @return void
-                    * @static
-                    * @method startDrag
-                    */
-                    ddRow.startDrag = function () {
-                        proxyEl  = this.getDragEl();
-                        srcEl = this.getEl();
-                        srcData = this.table0.getRecord(srcEl).getData();
-                        srcIndex = srcEl.sectionRowIndex;
-                        // Make the proxy look like the source element
-                        Dom.setStyle(srcEl, "visibility", "hidden");
-                        proxyEl.innerHTML = "<table><tbody>"+srcEl.innerHTML+"</tbody></table>";
-                    };
-
-                    /**
-                    * Once we end dragging a row, we swap the proxy with the real element.
-                    * @param x : The x Coordinate
-                    * @param y : The y Coordinate
-                    * @return void
-                    * @static
-                    * @method endDrag
-                    */
-                    ddRow.endDrag = function(x,y) {
-                        Dom.setStyle(proxyEl, "visibility", "hidden");
-                        Dom.setStyle(srcEl, "visibility", "");
-                    };
-
-
-                    /**
-                    * This is the function that does the trick of swapping one row with another.
-                    * @param e : The drag event
-                    * @param id : The id of the row being dragged
-                    * @return void
-                    * @static
-                    * @method onDragOver
-                    */
-                    ddRow.onDragOver = function(e, id) {
-                        // Reorder rows as user drags
-
-                        var destEl = Dom.get(id),
-                            destIndex = destEl.sectionRowIndex;
-
-
-
-                        if (destEl.nodeName.toLowerCase() === "tr") {
-                            if(tmpIndex !==null) {
-                                this.table0.deleteRow(tmpIndex);
-                            }
-                            else {
-                                this.table0.deleteRow(srcIndex);
-                            }
-
-                        this.table0.addRow(srcData, destIndex);
-                        tmpIndex = destIndex;
-
-
-                        DDM.refreshCache();
-                        }
-                    };
-            };
-		
-	    this.table0.subscribe('cellMousedownEvent', onRowSelect);
-
-
-        //////////////////////////////////////////////////////////////////////////////
-        // Create DDTarget instances when DataTable is initialized
-        //////////////////////////////////////////////////////////////////////////////
-        this.table0.subscribe("initEvent", function() {
-
-            var i, id,
-                allRows = this.getTbodyEl().rows;
-
-
-            for(i=0; i<allRows.length; i++) {
-                id = allRows[i].id;
-                // Clean up any existing Drag instances
-                if (myDTDTargets[id]) {
-                     myDTDTargets[id].unreg();
-                     delete myDTDTargets[id];
-                }
-                // Create a Drag instance for each row
-                myDTDTargets[id] = new YAHOO.util.DDTarget(id);
-            }
-
-        });
-
-        //////////////////////////////////////////////////////////////////////////////
-        // Create DDTarget instances when new row is added
-        //////////////////////////////////////////////////////////////////////////////
-        this.table0.subscribe("rowAddEvent",function(e){
-            var id = e.record.getId();
-            myDTDTargets[id] = new YAHOO.util.DDTarget(id);
-	  
-        });
-
-///////////////////
 
 
 	    this.table0.handleDataReturnPayload =myhandleDataReturnPayload;
@@ -261,7 +152,7 @@ Event.addListener(window, "load", function() {
 	    
 	   this.table0.table_id=tableid;
      this.table0.subscribe("renderEvent", myrenderEvent);
-
+this.table0.subscribe("rowClickEvent", select_widget_from_list);
 	    
        	    this.table0.subscribe("cellClickEvent", onCellClick);  
 	    this.table0.filter={key:'<?php echo$_SESSION['state']['dashboards']['active_widgets']['f_field']?>',value:'<?php echo$_SESSION['state']['dashboards']['active_widgets']['f_value']?>'};
@@ -271,6 +162,32 @@ Event.addListener(window, "load", function() {
     });
 
 
+function select_widget_from_list(oArgs){
+
+widget_id=tables.table0.getRecord(oArgs.target).getData('id');
+//alert(widget_id);
+dialog_widget_list.hide();
+
+
+	var request='ar_edit_dashboard.php?tipo=add_widget_to_dashboard&dashboard_key='+Dom.get('dashboard_key').value+'&widget_key='+widget_id;
+	//window.location=request;
+       //alert(request);
+      YAHOO.util.Connect.asyncRequest('POST',request ,{
+	      
+	      success:function(o) {
+		  	 // alert(o.responseText)
+		      var r =  YAHOO.lang.JSON.parse(o.responseText);
+		  if (r.state == 200) {
+			window.location.reload();
+		  }else{
+		      alert(r.msg);
+		  }
+	      }
+	  });   
+
+    
+
+}
 
 function add_dashboard(){
       var request='ar_edit_dashboard.php?tipo=add_dashboard&user_key='+Dom.get('user_key').value;
@@ -334,28 +251,27 @@ function edit_dashboard(key){
      datasource.sendRequest(request,table.onDataReturnInitializeTable, table);       
 }
 
-function add_widget(key){
-	var request='add_widgets.php?tipo=add_widget&user_id='+Dom.get('user_key').value+'&dashboard_id='+key;
-	window.location=request;
-       //alert(request);
-      YAHOO.util.Connect.asyncRequest('POST',request ,{
-	      
-	      success:function(o) {
-		  	  //alert(o.responseText)
-		      var r =  YAHOO.lang.JSON.parse(o.responseText);
-		  if (r.state == 200) {
-			window.location.reload();
-		  }else{
-		      alert(r.msg);
-		  }
-	      }
-	  });   
+function add_widget(e, key){
+	region1 = Dom.getRegion(e); 
+	region2 = Dom.getRegion('dialog_widget_list'); 
+
+	var pos =[region1.right-region2.width-20,region1.bottom]
+
+	Dom.setXY('dialog_widget_list', pos);
+
+	dialog_widget_list.show();
+
+	Dom.get('dashboard_key').value=key;
 }
+
+
 
 function init(){
 
  YAHOO.util.Event.addListener('add_dashboard', "click",add_dashboard);
 
+dialog_widget_list = new YAHOO.widget.Dialog("dialog_widget_list", {context:["widget_add","tr","tl"]  ,visible : false,close:true,underlay: "none",draggable:false});
+    dialog_widget_list.render();
 
 }
 
