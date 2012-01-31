3977a0bec27c7a71c173bccdfd7d70fe20f6e00e
Raul Perusquia <rulovico@gmail.com>
2012-01-31 19:01:46 +0000

    xx

--- mantenence/scripts/fix_orders.php
+++ mantenence/scripts/fix_orders.php
@@ -30,18 +30,68 @@ require_once '../../conf/conf.php';
 date_default_timezone_set('UTC');
 
 
-//$sql="select * from `Product Dimension` where `Product Code`='FO-A1'";
-$sql="select replace(`Order Original Metadata`,'U','') as id from `Order Dimension` where `Order Current Dispatch State`='Unknown' and `Order Store Key`=1  ";
+$sql="select replace(`Order Original Metadata`,'U','') as id from `Order Dimension` where `Order Current Dispatch State` in ('Unknown','In Process','Submitted by Customer','Ready to Pick','Cancelled','Suspended')   and `Order Store Key`=1  ";
 $result=mysql_query($sql);
 while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
-
-
 $sql=sprintf("update orders_data.orders set last_transcribed=null where orders_data.orders.id =%d",$row['id']);
+//print "$sql\n";
+mysql_query($sql);
+ // print $row['id']."\r";
+ }
+ 
+ 
+$sql="select replace(`Order Original Metadata`,'D','') as id from `Order Dimension` where `Order Current Dispatch State` in ('Unknown','In Process','Submitted by Customer','Ready to Pick','Cancelled','Suspended')   and `Order Store Key`=3  ";
+$result=mysql_query($sql);
+while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+$sql=sprintf("update de_orders_data.orders set last_transcribed=null where orders_data.orders.id =%d",$row['id']);
+print "$sql\n";
+mysql_query($sql);
+ // print $row['id']."\r";
+ }
+ 
+ 
+$sql="select replace(`Order Original Metadata`,'F','') as id from `Order Dimension` where `Order Current Dispatch State` in ('Unknown','In Process','Submitted by Customer','Ready to Pick','Cancelled','Suspended')   and `Order Store Key`=5  ";
+$result=mysql_query($sql);
+while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+$sql=sprintf("update fr_orders_data.orders set last_transcribed=null where orders_data.orders.id =%d",$row['id']);
+//print "$sql\n";
+mysql_query($sql);
+ // print $row['id']."\r";
+ }
+ 
+ $sql="select replace(`Order Original Metadata`,'I','') as id from `Order Dimension` where `Order Current Dispatch State` in ('Unknown','In Process','Submitted by Customer','Ready to Pick','Cancelled','Suspended')   and `Order Store Key`=8  ";
+$result=mysql_query($sql);
+while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+$sql=sprintf("update it_orders_data.orders set last_transcribed=null where orders_data.orders.id =%d",$row['id']);
+//print "$sql\n";
+mysql_query($sql);
+ // print $row['id']."\r";
+ }
+ 
+ 
+ $sql="select replace(`Order Original Metadata`,'P','') as id from `Order Dimension` where `Order Current Dispatch State` in ('Unknown','In Process','Submitted by Customer','Ready to Pick','Cancelled','Suspended')   and `Order Store Key`=7  ";
+$result=mysql_query($sql);
+while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+$sql=sprintf("update pl_orders_data.orders set last_transcribed=null where orders_data.orders.id =%d",$row['id']);
+//print "$sql\n";
+mysql_query($sql);
+ // print $row['id']."\r";
+ }
+ 
+ 
+ exit;
+ 
+$sql="select replace(`Invoice Metadata`,'U','') as id from `Invoice Dimension` where  `Invoice Title`='Refund' and `Invoice Store Key`=1  ";
+$result=mysql_query($sql);
+while($row=mysql_fetch_array($result, MYSQL_ASSOC)   ){
+$sql=sprintf("update orders_data.orders set last_transcribed=null where orders_data.orders.id =%d",$row['id']);
+//print "$sql\n";
 mysql_query($sql);
   print $row['id']."\r";
-  
-  
-}
+ }
+
+
+
 
 
 ?>
