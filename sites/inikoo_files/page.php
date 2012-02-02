7bc14fe9488347df087da55c717c3100fc3269a8
Raul Perusquia <rulovico@gmail.com>
2012-02-02 19:04:01 +0000

    x

--- sites/inikoo_files/page.php
+++ sites/inikoo_files/page.php
@@ -18,7 +18,7 @@ $page=new Page($page_key);
 
 if ($page->data['Page Site Key']!=$site->id) {
 //    header('Location: index.php');
-    exit("No site found");
+    exit("No site/page not match");
 }
 
 if (!$page->id) {
