2adbedfba2a3e84b1f9a350edb1f3a83136bed89
Raul Perusquia <rulovico@gmail.com>
2012-02-29 10:17:49 +0000

    xx

--- mantenence/scripts/read_web_state_from_old_db.php
+++ mantenence/scripts/read_web_state_from_old_db.php
@@ -46,13 +46,13 @@ while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 }
 */
 
-$sql=sprintf("select id,code,stock,condicion,web_tipo  from aw_old.product order by code  ");
+$sql=sprintf("select id,code,stock,condicion,web_tipo  from aw_old.product where code='StarRS-01' order by code  ");
 $result2a=mysql_query($sql,$con2);
 $codes=array();
 while ($row2a=mysql_fetch_array($result2a, MYSQL_ASSOC)   ) {
 
 //	print $row2a['code']."\r";
-	$codes[]=$row2a['code'];
+	$codes[$row2a['code']]=$row2a['web_tipo'];
 
 }
 
@@ -70,7 +70,7 @@ if (!$db) {
 }
 
 
-foreach ($codes as $code) {
+foreach ($codes as $code=>$web_tipo) {
 
 	$sql=sprintf('select `Product ID` from `Product Dimension` where `Product Code`=%s',prepare_mysql($code));
 	$result=mysql_query($sql,$con);
@@ -79,7 +79,7 @@ foreach ($codes as $code) {
 //print $product->code."\n";
 //print_r($row['Product ID']);
 //continue;
-		switch ($row2a['web_tipo']) {
+		switch ($web_tipo) {
 		case 'HD':
 			$product->update_web_configuration('Offline');
 			break;
