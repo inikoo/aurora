c74c0fd0e69db7c12b8a5b3a8940aeb1a7c7bdd9
Raul Perusquia <rulovico@gmail.com>
2012-03-05 13:54:19 +0000

    xx

---
+++ mantenence/scripts/fix_part_transactions4.php
@@ -0,0 +1,65 @@
+<?php
+//@author Raul Perusquia <rulovico@gmail.com>
+//Copyright (c) 2009 LW
+include_once '../../app_files/db/dns.php';
+include_once '../../class.Department.php';
+include_once '../../class.Family.php';
+include_once '../../class.Product.php';
+include_once '../../class.Supplier.php';
+include_once '../../class.Part.php';
+include_once '../../class.PartLocation.php';
+
+include_once '../../class.SupplierProduct.php';
+error_reporting(E_ALL);
+
+date_default_timezone_set('UTC');
+include_once '../../set_locales.php';
+require '../../locale.php';
+$_SESSION['locale_info'] = localeconv();
+
+$con=@mysql_connect($dns_host,$dns_user,$dns_pwd );
+
+if (!$con) {print "Error can not connect with database server\n";exit;}
+
+$db=@mysql_select_db($dns_db, $con);
+if (!$db) {print "Error can not access the database\n";exit;}
+
+
+require_once '../../common_functions.php';
+mysql_query("SET time_zone ='+0:00'");
+mysql_query("SET NAMES 'utf8'");
+require_once '../../conf/conf.php';
+date_default_timezone_set('UTC');
+
+
+
+
+//$sql="select * from `Product Dimension` where `Product Code`='FO-A1'";
+$sql="select * from `Part Dimension` where `Part SKU`=749 order by `Part SKU`";
+$sql="select * from `Part Dimension` order by `Part SKU` desc ";
+
+$result=mysql_query($sql);
+while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+	$part=new Part('sku',$row['Part SKU']);
+	$part->wrap_transactions();
+	$locations=array();
+	$was_associated=array();
+	$sql=sprintf("select ITF.`Location Key`  from `Inventory Transaction Fact` ITF    where `Inventory Transaction Type`='Associate' and  `Part SKU`=%d group by `Location Key` ",$row['Part SKU']);
+	// print $sql;
+
+	$result2=mysql_query($sql);
+
+	while ($row2=mysql_fetch_array($result2, MYSQL_ASSOC)   ) {
+		$part_location=new PartLocation($row['Part SKU'].'_'.$row2['Location Key']);
+		$part_location->redo_adjusts();
+
+	}
+
+
+	print $row['Part SKU']."\r";
+
+
+}
+
+
+?>
