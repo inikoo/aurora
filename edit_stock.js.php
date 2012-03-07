39c9000d3481e6afe3667c6c23ed947c50295f6e
Raul Perusquia <rulovico@gmail.com>
2012-03-07 22:35:40 +0000

    xx

--- edit_stock.js.php
+++ edit_stock.js.php
@@ -595,7 +595,7 @@ Dom.setStyle('move_items_btn','display','none');
 //alert(request);
     YAHOO.util.Connect.asyncRequest('POST',request , {
 success:function(o) {
-       //  alert(o.responseText);
+  //       alert(o.responseText);
             var r =  YAHOO.lang.JSON.parse(o.responseText);
             if (r.action=='ok') {
                 close_lost_dialog();
