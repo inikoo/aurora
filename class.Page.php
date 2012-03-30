2bbb92b35858a2346c1bba7c0803771e321a16a9
Raul Perusquia <rulovico@gmail.com>
2012-03-30 14:52:17 +0100

    xx

--- class.Page.php
+++ class.Page.php
@@ -873,12 +873,12 @@ class Page extends DB_Table {
 				$url=$site->data['Site URL'].'/'.strtolower($value);
 
 				$sql=sprintf("update `Page Dimension`  set  `Page URL`=%s  where `Page Key`=%d",prepare_mysql($value),$this->id);
-				// print $sql;
-
-
+			
 				mysql_query($sql);
 
-
+$sql=sprintf("update `Page Redirection Dimension`  set  `Page Target URL`=%s  where `Page Target Key`=%d",prepare_mysql($value),$this->id);
+			
+				mysql_query($sql);
 			}
 
 
@@ -1131,6 +1131,9 @@ class Page extends DB_Table {
 		mysql_query($sql);
 		$sql=sprintf("delete from `Page Store Dimension` where `Page Key`=%d",$this->id);
 		mysql_query($sql);
+		$sql=sprintf("delete from `Page Redirection Dimension` where `Page Target Key`=%d",$this->id);
+		mysql_query($sql);
+		
 		$this->deleted=true;
 
 
