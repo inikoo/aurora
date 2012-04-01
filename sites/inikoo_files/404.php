729f4464a9b97b0ea597e876f76ef2449b2fa2b1
Raul Perusquia <rulovico@gmail.com>
2012-04-01 20:04:20 +0100

    xx

--- sites/inikoo_files/404.php
+++ sites/inikoo_files/404.php
@@ -1,5 +1,10 @@
 <?php
-$not_found_current_page='x'.$_REQUEST['original_url'];
+$not_found_current_page=$_REQUEST['original_url'];
+
+if(preg_match('/\.(jpg|png|gif|xml|txt|ico|css|js)$/i',$not_found_current_page)){
+	header("HTTP/1.0 404 Not Found");
+	exit();
+}
 
 include_once('common.php');
 $page_key=$site->get_not_found_page_key();
