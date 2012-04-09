c7320166e39fb685efc6543ca3a39d1242c7ad39
Migara <migara@inikoo.com>
2012-04-09 11:18:18 +0100

    new

--- ip_to_location.php
+++ ip_to_location.php
@@ -23,6 +23,28 @@ mysql_query("SET time_zone ='+0:00'");
 mysql_query("SET NAMES 'utf8'");
 require_once 'conf/conf.php';
 
+//Database Extraction Script
+//Populating IP Geo Dimension in kbase
+/*
+for($i=0; $i<=255; $i++){
+	$table="ip4_".$i;
+	$sql=sprintf("select * from host_ip_test.`$table`");
+	$result=mysql_query($sql);
+	while($row=mysql_fetch_assoc($result)){
+		$sql=sprintf("select * from host_ip_test.`countries` where `id`=%d", $row['country']);
+		$result1=mysql_query($sql);
+		$row1=mysql_fetch_assoc($result1);
+		
+		$country=$row1['name'];
+		$code=$row1['code'];
+
+		$sql=sprintf("insert into kbase.`IP Geo Dimension` (`IP Geo A`, `IP Geo B`, `IP Geo C`, `Country`, `Country Code`) values (%d, %d, %d, '%s', '%s')", $i, $row['b'], $row['c'], $country, $code);
+		//print $sql; exit;
+		mysql_query($sql);
+	}
+}
+*/
+
 
 if(isset($_REQUEST['ip']))
 	$ip=$_REQUEST['ip'];
@@ -52,17 +74,15 @@ if(!$valid_ip){
 	exit;
 }
 
-//print "IP Address: ".$ip;
 
-$table="ip4_".$octet[0];
 
-$sql=sprintf("select * from host_ip_test.`$table` ip left join host_ip_test.`countries` c on (ip.`Country`= c.`id`) where ip.`b`=%d and ip.`c`=%d", $octet[1], $octet[2]);
+$sql=sprintf("select * from kbase.`IP Geo Dimension` where `IP Geo A`=%d and `IP Geo B`=%d and `IP Geo C`=%d ", $octet[0], $octet[1], $octet[2]);
 
 $result=mysql_query($sql);
 
 if($row=mysql_fetch_assoc($result)){
 	//print_r($row);
-	$country=$row['name'];
+	$country=$row['Country'];
 }
 else{
 	//print "Not found";	
