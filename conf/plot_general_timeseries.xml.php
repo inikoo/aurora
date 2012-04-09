8958c3be7d19a5c88bcbf6bbf1b09bff4a708b76
Raul Perusquia <rulovico@gmail.com>
2012-04-09 11:27:14 +0100

    xx

--- conf/plot_general_timeseries.xml.php
+++ conf/plot_general_timeseries.xml.php
@@ -9,25 +9,6 @@ if (!isset($_REQUEST['tipo'])) {
 $tipo=$_REQUEST['tipo'];
 switch ($tipo) {
 
-case('site_requests'):
-    if (!isset($_REQUEST['site_key'])) {
-        exit;
-    }
-
-    $sql=sprintf("select *  from `Site Dimension` where `Site Key` in (%s)",addslashes($_REQUEST['site_key']));
-    $res=mysql_query($sql);
-    $graphs_data=array();
-    $gid=0;
-    while ($row=mysql_fetch_assoc($res)) {
-        $graphs_data[]=array(
-                           'gid'=>$gid,
-                           'title'=>$row['Site Name'].' '._('Hits'),
-
-                       );
-        $gid++;
-    }
-    $data_args='tipo=site_requests&site_key='.$_REQUEST['site_key'];
-    break;
 case('number_of_contacts'):
  if (!isset($_REQUEST['store_key']) or $_REQUEST['store_key']=='') {
         exit;
