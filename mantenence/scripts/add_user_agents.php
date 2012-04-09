2d848d551937d2e7a1d1f9e03dab39584163a999
Migara <migara@inikoo.com>
2012-04-09 11:50:57 +0100

    new

--- mantenence/scripts/add_user_agents.php
+++ mantenence/scripts/add_user_agents.php
@@ -1,4 +1,10 @@
 <?php
+//**********************************************//
+//User Agent Download Link
+//http://www.user-agents.org/allagents.xml
+//**********************************************//
+
+
 //@author Raul Perusquia <rulovico@gmail.com>
 //Copyright (c) 2009 LW
 include_once '../../app_files/db/dns.php';
