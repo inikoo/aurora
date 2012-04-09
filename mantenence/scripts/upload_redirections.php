001ca027c1ac1e86294fe30a35a62d2a6d65cc8a
Raul Perusquia <rulovico@gmail.com>
2012-04-09 12:25:24 +0100

    xx

--- mantenence/scripts/upload_redirections.php
+++ mantenence/scripts/upload_redirections.php
@@ -49,6 +49,7 @@ $site=new Site(1);
 $sql="select * from `Page Store Dimension` PS  left join `Page Dimension` P on (P.`Page Key`=PS.`Page Key`) where PS.`Page Key`=2205  ";
 
 $sql="select `Source Path`,`Source Host` from `Page Redirection Dimension` R  left join `Page Store Dimension` P on (R.`Page Target Key`=P.`Page Key`) where `Page Site Key`=".$site->id." group by `Source Path`";
+print $sql;
 $result=mysql_query($sql);
 while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 
