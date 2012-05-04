d85301c8876f67878201b95da8b49c36479b4e7d
Raul Perusquia <rulovico@gmail.com>
2012-05-04 18:08:46 +0100

    xx

--- ar_quick_tables.php
+++ ar_quick_tables.php
@@ -631,7 +631,7 @@ function part_list() {
     elseif($f_field=='supplied_by' and $f_value!='')
     $wheref.=" and  `Part XHTML Currently Supplied By` like '%".addslashes($f_value)."%'";
     elseif($f_field=='sku' and $f_value!='')
-    $wheref.=" and  `Part SKU` ='".addslashes($f_value)."'";
+    $wheref.=" and  `Part SKU` like '".addslashes($f_value)."%'";
 
     $sql="select count(DISTINCT `Part SKU`) as total from `Part Dimension` $where $wheref  ";
 //print $sql;
