7dd74aefcde4908ecec874a2cc10635862719906
Raul Perusquia <rulovico@gmail.com>
2012-05-04 17:36:00 +0100

    xx

--- mantenence/scripts/get_orders_db.php
+++ mantenence/scripts/get_orders_db.php
@@ -1243,7 +1243,7 @@ unset($part);
 					
 				} else {
 					print_r($product);
-					print "->End.(GO UK) ".date("r")."\n";
+					print "->End.(GO xxx UK) ".date("r")."\n";
 					exit("error: $sql");
 
 				}
