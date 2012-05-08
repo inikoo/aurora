51e483b52b636b20f6f95515fccc0dc605f6c723
Raul Perusquia <rulovico@gmail.com>
2012-05-08 12:49:52 +0200

    x

--- mantenence/scripts/get_products_es.php
+++ mantenence/scripts/get_products_es.php
@@ -41,7 +41,7 @@ $Data_Audit_ETL_Software="$software $version";
 
 $file_name='AWorder2002-spain.xls';
 $csv_file='es_tmp.csv';
-//exec('/usr/local/bin/xls2csv    -s cp1252   -d 8859-1   '.$file_name.' > '.$csv_file);
+exec('/usr/local/bin/xls2csv    -s cp1252   -d 8859-1   '.$file_name.' > '.$csv_file);
 
 $handle_csv = fopen($csv_file, "r");
 $column=0;
