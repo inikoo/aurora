c121fdc03bceaf5872d1134822947f477fdfc6a9
Raul Perusquia <rulovico@gmail.com>
2012-05-11 12:09:32 +0200

    xx

--- class.Page.php
+++ class.Page.php
@@ -1068,7 +1068,6 @@ $sql=sprintf("update `Page Redirection Dimension`  set  `Page Target URL`=%s  wh
 		$see_also=array();
 		$sql=sprintf("select `Page Store See Also Key`,`Correlation Type`,`Correlation Value` from  `Page Store See Also Bridge` where `Page Store Key`=%d order by `Correlation Value` desc ",
 			$this->id);
-
 		$res=mysql_query($sql);
 
 		while ($row=mysql_fetch_assoc($res)) {
