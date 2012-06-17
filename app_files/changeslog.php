132e55ba9b2d0df8a29ba32dba00c95befa605f3
Raul Perusquia <rulovico@gmail.com>
2012-06-17 08:57:53 +0100

    xx

--- app_files/changeslog.php
+++ app_files/changeslog.php
@@ -5764,3 +5764,5 @@ ALTER TABLE `Dashboard Widget Dimension` CHANGE `Dashboard Widget Key` `Dashboar
 DROP TABLE `Order Public ID Dimension`;
 ALTER TABLE `User Dimension` CHANGE `User Theme Key` `User Theme Key` SMALLINT( 5 ) UNSIGNED NULL DEFAULT '1',CHANGE `User Theme Background Key` `User Theme Background Key` SMALLINT( 5 ) UNSIGNED NULL DEFAULT '1';
 
+INSERT INTO `dw`.`Widget Dimension` (`Widget Key`, `Widget Name`, `Widget Block`, `Widget Dimension`, `Widget URL`, `Widget Description`, `Widget Metadata`) VALUES (NULL, 'Top Products', 'block_3', NULL, 'dashboard_block.php?tipo=top_products', 'Top products', '');
+INSERT INTO `dw`.`Widget Dimension` (`Widget Key`, `Widget Name`, `Widget Block`, `Widget Dimension`, `Widget URL`, `Widget Description`, `Widget Metadata`) VALUES (NULL, 'Average Order Value', 'block_1', NULL, 'dashboard_block.php?tipo=average_order_value', '', '');
