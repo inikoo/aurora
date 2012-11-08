af7f3265027e814872f5e57a9225844228e71a54
Raul Perusquia <rulovico@gmail.com>
2012-11-08 10:39:20 +0000

    xx

--- customers_pending_orders.php
+++ customers_pending_orders.php
@@ -1,22 +1,22 @@
 <?php
-include_once('common.php');
-include_once('class.Store.php');
-include_once('class.CompanyArea.php');
+include_once 'common.php';
+include_once 'class.Store.php';
+include_once 'class.CompanyArea.php';
 
 
 
 if (!$user->can_view('customers')) {
-    header('Location: index.php');
-    exit();
+	header('Location: index.php');
+	exit();
 }
 if (isset($_REQUEST['store']) and is_numeric($_REQUEST['store']) ) {
-    $store_id=$_REQUEST['store'];
+	$store_id=$_REQUEST['store'];
 } else {
-    $store_id=$_SESSION['state']['customers']['store'];
+	$store_id=$_SESSION['state']['customers']['store'];
 }
 if (!($user->can_view('stores') and in_array($store_id,$user->stores)   ) ) {
-    header('Location: index.php');
-    exit;
+	header('Location: index.php');
+	exit;
 }
 $store=new Store($store_id);
 $currency=$store->data['Store Currency Code'];
@@ -32,36 +32,37 @@ $smarty->assign('search_scope','customers');
 
 
 $css_files=array(
-               $yui_path.'reset-fonts-grids/reset-fonts-grids.css',
-               $yui_path.'menu/assets/skins/sam/menu.css',
-               $yui_path.'assets/skins/sam/autocomplete.css',
-               $yui_path.'calendar/assets/skins/sam/calendar.css',
-               'common.css',
-               'css/container.css',
-               'button.css',
-               'table.css',
-               'theme.css.php'
-           );
+	$yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+	$yui_path.'menu/assets/skins/sam/menu.css',
+	$yui_path.'assets/skins/sam/autocomplete.css',
+	$yui_path.'calendar/assets/skins/sam/calendar.css',
+	'common.css',
+	'css/container.css',
+	'button.css',
+	'table.css',
+	'theme.css.php'
+);
 
 $js_files=array(
 
-              $yui_path.'utilities/utilities.js',
-              $yui_path.'json/json-min.js',
-              $yui_path.'paginator/paginator-min.js',
-              $yui_path.'datasource/datasource-min.js',
-              $yui_path.'autocomplete/autocomplete-min.js',
-              $yui_path.'datatable/datatable.js',
-              $yui_path.'container/container-min.js',
-              $yui_path.'menu/menu-min.js',
-              $yui_path.'calendar/calendar-min.js',
-              'js/common.js',
-              'js/table_common.js',
-              'js/edit_common.js',
-              'customers_pending_orders.js.php',
-              'js/edit_common.js',
-              'js/csv_common.js',
-              
-          );
+	$yui_path.'utilities/utilities.js',
+	$yui_path.'json/json-min.js',
+	$yui_path.'paginator/paginator-min.js',
+	$yui_path.'datasource/datasource-min.js',
+	$yui_path.'autocomplete/autocomplete-min.js',
+	$yui_path.'datatable/datatable.js',
+	$yui_path.'container/container-min.js',
+	$yui_path.'menu/menu-min.js',
+	$yui_path.'calendar/calendar-min.js',
+	'js/common.js',
+	'js/table_common.js',
+		'js/search.js',
+	'js/edit_common.js',
+	'customers_pending_orders.js.php',
+	'js/edit_common.js',
+	'js/csv_common.js',
+
+);
 
 
 
@@ -79,8 +80,8 @@ $tipo_filter2=$_SESSION['state']['customers']['pending_orders']['f_field'];
 $smarty->assign('filter0',$tipo_filter2);
 $smarty->assign('filter_value0',($_SESSION['state']['customers']['pending_orders']['f_value']));
 $filter_menu2=array(
-                  'public_id'=>array('db_key'=>'public_id','menu_label'=>'Order Number starting with  <i>x</i>','label'=>'Order Number'),
-              );
+	'public_id'=>array('db_key'=>'public_id','menu_label'=>'Order Number starting with  <i>x</i>','label'=>'Order Number'),
+);
 $smarty->assign('filter_menu0',$filter_menu2);
 $smarty->assign('filter_name0',$filter_menu2[$tipo_filter2]['label']);
 $paginator_menu0=array(10,25,50,100,500);
@@ -92,13 +93,13 @@ $elements_number=array('InProcessbyCustomer'=>0,'InProcess'=>0,'SubmittedbyCusto
 $sql=sprintf("select count(*) as num,`Order Current Dispatch State` from  `Order Dimension` where  `Order Store Key`=%d  group by `Order Current Dispatch State` ",$store_id);
 $res=mysql_query($sql);
 while ($row=mysql_fetch_assoc($res)) {
-    $elements_number[preg_replace('/\s/','',$row['Order Current Dispatch State'])]=$row['num'];
+	$elements_number[preg_replace('/\s/','',$row['Order Current Dispatch State'])]=$row['num'];
 }
 
 $sql=sprintf("select count(*) as num  from  `Order Dimension` where  `Order Store Key`=%d  and `Order Current Dispatch State` in ('Ready to Pick','Picking & Packing','Ready to Ship') ",$store_id);
 $res=mysql_query($sql);
 while ($row=mysql_fetch_assoc($res)) {
-    $elements_number['InWarehouse']=$row['num'];
+	$elements_number['InWarehouse']=$row['num'];
 }
 
 
