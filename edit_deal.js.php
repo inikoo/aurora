c4efafb67c774fe60e52762c3be13e7514321c91
Raul Perusquia <rulovico@gmail.com>
2012-11-08 17:07:04 +0000

    xx

--- edit_deal.js.php
+++ edit_deal.js.php
@@ -165,42 +165,53 @@ YAHOO.util.Event.addListener(window, "load", function() {
 
 
 
-function init(){
+function validate_deal_code(query){
+alert(query)
+ validate_general('deal','code',unescape(query));
+}
 
+function validate_deal_name(query){
+ validate_general('deal','name',unescape(query));
+}
+function validate_deal_description(query){
+ validate_general('deal','description',unescape(query));
+}
+
+
+
+function init(){
 var validate_scope_data=
 {
     'deal':{
-	'description':{'changed':false,'validated':true,'required':true,'group':1,'type':'item','name':'Deal_Description','ar':false,'validation':[{'regexp':"[a-z\\d]+",'invalid_msg':'<?php echo _('Invalid Offer Description')?>'}]}
-	,'name':{'changed':false,'validated':true,'required':false,'group':1,'type':'item','name':'Customer_Main_Contact_Name','validation':[{'regexp':"[a-z\\d]+",'invalid_msg':'<?php echo _('Invalid Offer Name')?>'}]}
-	,'description':{'ar':'find','ar_request':'ar_assets.php?tipo=code_in_other_deal&deal_key='+Dom.get('deal_key').value+'&store_key='+Dom.get('store_key').value+'&query=','changed':false,'validated':true,'required':false,'group':1,'type':'item','name':'Deal_Code','validation':[{'regexp':regexp_valid_email,'invalid_msg':'<?php echo _('Invalid Offer Code')?>'}]}
+	'description':{'changed':false,'validated':true,'required':true,'group':1,'type':'item','name':'Deal_Description','ar':false,'validation':[{'regexp':"[a-z\\d]+",'invalid_msg':'<?php echo _('Invalid Offer Description')?>'}]},
+	'name':{'changed':false,'validated':true,'required':false,'group':1,'type':'item','name':'Customer_Main_Contact_Name','validation':[{'regexp':"[a-z\\d]+",'invalid_msg':'<?php echo _('Invalid Offer Name')?>'}]},
+	'code':{'ar':'find','ar_request':'ar_assets.php?tipo=code_in_other_deal&deal_key='+Dom.get('deal_key').value+'&store_key='+Dom.get('store_key').value+'&query=','changed':false,'validated':true,'required':false,'group':1,'type':'item','name':'Deal_Code','validation':false}
 }  
 };
-
 var validate_scope_metadata={
 'deal':{'type':'edit','ar_file':'ar_edit_assets.php','key_name':'deal_key','key':Dom.get('deal_key').value}
 
 };
-
-
- var customer_name_oACDS = new YAHOO.util.FunctionDataSource(validate_customer_name);
-    customer_name_oACDS.queryMatchContains = true;
-    var customer_name_oAutoComp = new YAHOO.widget.AutoComplete("Customer_Name","Customer_Name_Container", customer_name_oACDS);
-    customer_name_oAutoComp.minQueryLength = 0; 
-    customer_name_oAutoComp.queryDelay = 0.1;
+ var deal_code_oACDS = new YAHOO.util.FunctionDataSource(validate_deal_code);
+    deal_code_oACDS.queryMatchContains = true;
+    var deal_code_oAutoComp = new YAHOO.widget.AutoComplete("Deal_Code","Deal_Code_Container", deal_code_oACDS);
+    deal_code_oAutoComp.minQueryLength = 0; 
+    deal_code_oAutoComp.queryDelay = 0.1;
     
-     var customer_name_oACDS = new YAHOO.util.FunctionDataSource(validate_customer_name);
-    customer_name_oACDS.queryMatchContains = true;
-    var customer_name_oAutoComp = new YAHOO.widget.AutoComplete("Customer_Name","Customer_Name_Container", customer_name_oACDS);
-    customer_name_oAutoComp.minQueryLength = 0; 
-    customer_name_oAutoComp.queryDelay = 0.1;
+     var deal_name_oACDS = new YAHOO.util.FunctionDataSource(validate_deal_name);
+    deal_name_oACDS.queryMatchContains = true;
+    var deal_name_oAutoComp = new YAHOO.widget.AutoComplete("Deal_Name","Deal_Name_Container", deal_name_oACDS);
+    deal_name_oAutoComp.minQueryLength = 0; 
+    deal_name_oAutoComp.queryDelay = 0.1;
     
     
-     var customer_name_oACDS = new YAHOO.util.FunctionDataSource(validate_customer_name);
-    customer_name_oACDS.queryMatchContains = true;
-    var customer_name_oAutoComp = new YAHOO.widget.AutoComplete("Customer_Name","Customer_Name_Container", customer_name_oACDS);
-    customer_name_oAutoComp.minQueryLength = 0; 
-    customer_name_oAutoComp.queryDelay = 0.1;
+     var deal_description_oACDS = new YAHOO.util.FunctionDataSource(validate_deal_description);
+    deal_description_oACDS.queryMatchContains = true;
+    var deal_description_oAutoComp = new YAHOO.widget.AutoComplete("Deal_Description","Deal_Description_Container", deal_description_oACDS);
+    deal_description_oAutoComp.minQueryLength = 0; 
+    deal_description_oAutoComp.queryDelay = 0.1;
 
+alert("x")
 
 
 init_search('products_store');
