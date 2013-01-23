d368a2f0c5091034f0ddc95f316ffa8820d0236c
Raul Perusquia <rulovico@gmail.com>
2013-01-23 18:49:57 +0100

    xx

--- mantenence/scripts/ci_get_orders_db.php
+++ mantenence/scripts/ci_get_orders_db.php
@@ -1282,12 +1282,12 @@ while ($row2=mysql_fetch_array($res, MYSQL_ASSOC)) {
 			$data['Order Type']='Order';
 			$order=create_order($data);
 
-			if (strtotime('today -1 month')>strtotime($date_order)) {
-				$order->suspend(_('Order automatically suspended'),date("Y-m-d H:i:s",strtotime($date_order." +1 month")));
-			}
 			if (strtotime('today -6 month')>strtotime($date_order)) {
-				$order->cancel(_('Order automatically cancelled'),date("Y-m-d H:i:s",strtotime($date_order." +6 month")));
+				$order->suspend(_('Order automatically suspended'),date("Y-m-d H:i:s",strtotime($date_order." +6 month")));
 			}
+			//if (strtotime('today -6 month')>strtotime($date_order)) {
+			//	$order->cancel(_('Order automatically cancelled'),date("Y-m-d H:i:s",strtotime($date_order." +6 month")));
+			//}
 
 
 
