454f0fe84b94ae184dc44b1ebc798342fcee6114
Raul Perusquia <rulovico@gmail.com>
2013-06-09 21:18:48 +0100

    xx

--- js/export_common.js
+++ js/export_common.js
@@ -183,11 +183,11 @@ return;
 
 
     request = 'ar_export.php?tipo=export&table=' + data.table + '&parent=' + data.parent + '&parent_key=' + data.parent_key + '&output=' + data.output+'&fields='+list_fields
-  alert(request)
+ // alert(request)
     YAHOO.util.Connect.asyncRequest('POST', request, {
 
         success: function(o) {
-            alert(o.responseText)
+   //         alert(o.responseText)
             var r = YAHOO.lang.JSON.parse(o.responseText);
             if (r.state == '200') {
 
