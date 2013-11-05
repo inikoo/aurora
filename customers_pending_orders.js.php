9b53b28e9de3f920f30755aaa6cd525f0f582f8b
Raul Perusquia <rulovico@gmail.com>
2013-11-05 20:42:36 +0100

    xx

--- customers_pending_orders.js.php
+++ customers_pending_orders.js.php
@@ -32,7 +32,7 @@ YAHOO.util.Event.addListener(window, "load", function() {
 
 				];
 request="ar_edit_orders.php?tipo=store_pending_orders&sf=0&parent_key="+Dom.get('store_key').value
-alert(request)
+//alert(request)
 	    this.dataSource0 = new YAHOO.util.DataSource(request);
 		//alert("ar_edit_orders.php?tipo=ready_to_pick_orders");
 	    this.dataSource0.responseType = YAHOO.util.DataSource.TYPE_JSON;
