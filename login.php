ad03c492e6092e31b20705667193854057886ef9
Raul Perusquia <rulovico@gmail.com>
2013-11-19 15:49:15 +0000

    xx

--- login.php
+++ login.php
@@ -79,6 +79,10 @@ $css_files=array(
 		 'public_theme.css.php',
 		  'css/snow.css',
 		 'http://fonts.googleapis.com/css?family=Spirax',
+			 'http://fonts.googleapis.com/css?family=Mountains+of+Christmas'
+	 
+
+		 
 		 );
 $js_files=array(
 		$yui_path.'utilities/utilities.js',
@@ -146,6 +150,47 @@ if ($row=mysql_fetch_array($res)) {
 	$smarty->assign('account_name',$row['Account Name']);
 
 }
+
+
+$default_message="At Christmas, all roads lead home.";
+
+$messages=array(
+'2013-11-19'=>"What I don't like about office Christmas parties is looking for a job the next day.",
+
+'2013-11-20'=>'I hate Santa! He reminds me of the men who come for 10 minutes, do their thing and disappear for the remaining 364 days!',
+'2013-11-21'=>"Dearest Santa I promise I will never bitch about anyone please get me....ah nah, forget it, I'll get it for myself!",
+'2013-11-22'=>'Dearest God, this Christmas I planned on going green. So please get the point and send my lots of cash this Christmas. Thank you!',
+'2013-11-23'=>"Put on a Santa suit and open a mall kiosk that sells reindeer jerky and Easter Bunny filets.",
+'2013-11-24'=>'Christmas gifts are lovely, only when you are not the one paying for them though! ',
+'2013-11-25'=>'Christmas shopping is awesome, only when it is for you.',
+'2013-11-26'=>'Is it legal for a obese man to ride on reindeer’s? Is RSPCA listening?',
+'2013-11-27'=>"Decorate your yard to look like a sleigh and eight tiny reindeer crashed and burned. Walk back and forth along the street muttering, \"Oh, the humanity\".",
+'2013-11-28'=>"Hasn't Santa ever heard of Diet Coke and the treadmill?",
+'2013-11-29'=>"You are all that I want this Christmas! No actually, I wouldn't mind a car, diamonds and some cash too!",
+'2013-11-30'=>"Christmas isn't a season. It's a feeling.",
+'2013-12-01'=>"The perfect Christmas tree? All Christmas trees are perfect!",
+'2013-12-02'=>"At Christmas, all roads lead home.",
+'2013-12-03'=>"Christmas is weird. What other time of the year do you sit in front of a dead tree and eat candy out of your socks?",
+'2013-12-04'=>"This christmas i've decided to put the mistletoe in my back pocket, so all the people i don't like can kiss my ass.",
+'2013-12-05'=>"Sell jars of water, advertising them as Frosty the Snowman urns.",
+'2013-12-06'=>"Stand on a street corner selling dime bags of mistletoe.",
+'2013-12-07'=>"Randomly replace one bulb in your neighbor's lights so they no longer work. Repeat this every day until Christmas.",
+'2013-12-08'=>"What I don't like about office Christmas parties is looking for a job the next day."
+
+);
+
+
+$date=date('Y-m-d');
+
+if(array_key_exists($date,$messages)){
+
+$message=$messages[$date];
+}else{
+
+$message=$default_message;
+}
+
+	$smarty->assign('message',$message);
 
 
 $smarty->display("login.tpl");
