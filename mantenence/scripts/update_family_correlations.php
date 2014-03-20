39030844ec9f8337e4f1168cfd2e040a44d1bee1
Raul Perusquia <rulovico@gmail.com>
2014-03-20 22:39:08 +0100

    xx

--- mantenence/scripts/update_family_correlations.php
+++ mantenence/scripts/update_family_correlations.php
@@ -1,13 +1,13 @@
 <?php
 //@author Raul Perusquia <rulovico@gmail.com>
 //Copyright (c) 2009 LW
-include_once('../../app_files/db/dns.php');
-include_once('../../class.Department.php');
-include_once('../../class.Family.php');
-include_once('../../class.Product.php');
-include_once('../../class.Supplier.php');
-include_once('../../class.Part.php');
-include_once('../../class.Store.php');
+include_once '../../app_files/db/dns.php';
+include_once '../../class.Department.php';
+include_once '../../class.Family.php';
+include_once '../../class.Product.php';
+include_once '../../class.Supplier.php';
+include_once '../../class.Part.php';
+include_once '../../class.Store.php';
 error_reporting(E_ALL);
 
 date_default_timezone_set('UTC');
@@ -16,14 +16,14 @@ date_default_timezone_set('UTC');
 $con=@mysql_connect($dns_host,$dns_user,$dns_pwd );
 
 if (!$con) {
-    print "Error can not connect with database server\n";
-    exit;
+	print "Error can not connect with database server\n";
+	exit;
 }
 //$dns_db='dw_avant';
 $db=@mysql_select_db($dns_db, $con);
 if (!$db) {
-    print "Error can not access the database\n";
-    exit;
+	print "Error can not access the database\n";
+	exit;
 }
 
 
@@ -42,21 +42,38 @@ if ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 	$total=$row['total'];
 }
 $contador=0;
-
 $lap_time0=date('U');
-
 $sql="select `Product Family Key` from `Product Family Dimension`";
 $result=mysql_query($sql);
 while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
 
-    $family=new Family($row['Product Family Key']);
-    $family->update_correlated_sales_families();
-     $family->update_similar_families();
-   
-$contador++;
-	
-$lap_time1=date('U');
-print ' Time '.percentage($contador,$total,3)."  time  ".sprintf("%.2f",($lap_time1-$lap_time0))." lap  ".sprintf("%.2f",($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h \r";
+	$family=new Family($row['Product Family Key']);
+	$family->update_correlated_sales_families();
+
+	$contador++;
+
+	$lap_time1=date('U');
+	print 'Sim Time '.percentage($contador,$total,3)."  time  ".sprintf("%.2f",($lap_time1-$lap_time0))." lap  ".sprintf("%.2f",($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h \r";
+}
+
+$sql="select count(*) as total from `Product Family Dimension`  ";
+$result=mysql_query($sql);
+if ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+	$total=$row['total'];
+}
+$contador=0;
+$lap_time0=date('U');
+$sql="select `Product Family Key` from `Product Family Dimension`";
+$result=mysql_query($sql);
+while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+
+	$family=new Family($row['Product Family Key']);
+	$family->update_similar_families();
+
+	$contador++;
+
+	$lap_time1=date('U');
+	print 'Sales Corr Time '.percentage($contador,$total,3)."  time  ".sprintf("%.2f",($lap_time1-$lap_time0))." lap  ".sprintf("%.2f",($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h \r";
 }
 
 ?>
