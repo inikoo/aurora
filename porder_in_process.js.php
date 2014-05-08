a766f53a6b09bfb183cfe24d6a74355e2ac04533
Raul Perusquia <rulovico@gmail.com>
2014-05-08 17:02:36 +0100

    xx

--- porder_in_process.js.php
+++ porder_in_process.js.php
@@ -124,7 +124,7 @@ var myonCellClick = function(oArgs) {
 					'POST',
 					ar_file, {
 					    success:function(o) {
-						alert(o.responseText);
+					//	alert(o.responseText);
 						var r = YAHOO.lang.JSON.parse(o.responseText);
 						if (r.state == 200) {
 						    for(x in r.data){
