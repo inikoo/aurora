82a76bf42350a40913609f64ebecbab14becf45a
Raul Perusquia <rulovico@gmail.com>
2014-06-02 09:14:12 +0200

    xx

--- common_geography_functions.php
+++ common_geography_functions.php
@@ -24,4 +24,23 @@ return $countries_EC_Fiscal_VAT_area;
 }
 
 
+function gbr_postcode_first_part($mypostcode) {
+if (($posOfSpace = stripos($mypostcode," ")) !== false) return substr($mypostcode,0,$posOfSpace);
+    // Deal with the format BS000
+    if (strlen($mypostcode) < 5) return $mypostcode;
+
+    $shortened = substr($mypostcode,0,5);
+    if ((string)(int)substr($shortened,4,1) === (string)substr($shortened,4,1)) {
+       // BS000. Strip one and return
+       return substr($shortened,0,4);
+    }
+    else {
+      if ((string)(int)substr($shortened,3,1) === (string)substr($shortened,3,1)) {
+         return substr($shortened,0,3);
+      }
+      else return substr($shortened,0,2);
+    }
+}
+
+
 ?>
