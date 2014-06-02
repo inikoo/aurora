74d4cd6e79994fbb8a893f6d648db75b083ac350
Raul Perusquia <rulovico@gmail.com>
2014-06-02 13:28:49 +0200

    removing alerts

--- class.Order.php
+++ class.Order.php
@@ -3500,7 +3500,7 @@ class Order extends DB_Table {
 					switch ($row['Charge Terms Type']) {
 					
 					case 'Order Items Net Amount':
-					default:
+					
 						$sql=sprintf("select sum(`Order Transaction Net Amount`*(`Delivery Note Quantity`/`Order Quantity`)) as amount from `Order Transaction Fact` where `Order Key`=%d and `Delivery Note Key`=%d and `Order Quantity`!=0",
 							$this->id,
 							$dn_key
@@ -3512,7 +3512,7 @@ class Order extends DB_Table {
 							$order_amount=0;
 						}
 						break;
-					}
+					
 					
 					
 					case 'Order Items Gross Amount':
