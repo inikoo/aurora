1bd57d3b749d60e0940cd85eb41761421300ef92
Raul Perusquia <rulovico@gmail.com>
2014-06-05 17:15:21 +0100

    debug ie

--- sites/inikoo_files/profile_contact.js.php
+++ sites/inikoo_files/profile_contact.js.php
@@ -352,9 +352,13 @@ var request='ar_edit_contacts.php?tipo=edit_customer&values='+ jsonificated_valu
 
 function save_category(o) {
     
+      alert("xx")
+     alert(o)
+    alert(o.selectedIndex)
+    
     var current_category_key=o.getAttribute('ovalue');
     
-    alert(o.selectedIndex)
+  
     
     var category_key=o.options[o.selectedIndex].value;
     var subject='Customer';
