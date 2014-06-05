15f8abe78126ade265075746e052f7d58c2a7838
Raul Perusquia <rulovico@gmail.com>
2014-06-05 14:44:23 +0100

    debug ie orde rist

--- sites/inikoo_files/js/checkout.js
+++ sites/inikoo_files/js/checkout.js
@@ -99,7 +99,7 @@ function confirm_payment() {
 
         YAHOO.util.Connect.asyncRequest('POST', request, {
             success: function(o) {
-                //alert(o.responseText)
+                alert(o.responseText)
                 var r = YAHOO.lang.JSON.parse(o.responseText);
                 if (r.state == 200) {
 
