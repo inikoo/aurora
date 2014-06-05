5e301360bed9d01ba46093a9117de7497fc10b85
Raul Perusquia <rulovico@gmail.com>
2014-06-05 22:17:53 +0100

    confirmation emails w payment

--- sites/inikoo_files/thanks.php
+++ sites/inikoo_files/thanks.php
@@ -3,6 +3,7 @@ include_once 'class.DB_Table.php';
 
 include_once 'class.Payment.php';
 include_once 'class.Payment_Account.php';
+include_once 'class.Payment_Service_Provider.php';
 
 include_once 'common.php';
 $page_key=$site->get_page_key_from_section('Thanks');
