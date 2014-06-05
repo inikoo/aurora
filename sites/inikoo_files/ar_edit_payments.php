c2fb99e57467ef561525840bc57d97eb1427bcfd
Raul Perusquia <rulovico@gmail.com>
2014-06-05 21:28:29 +0100

    confirmation emails w payment

--- sites/inikoo_files/ar_edit_payments.php
+++ sites/inikoo_files/ar_edit_payments.php
@@ -92,7 +92,7 @@ function submit_order($data) {
 	}
 
 
-	//$order->checkout_submit_order();
+	$order->checkout_submit_order();
 
 
 	$order->update(
@@ -103,7 +103,7 @@ function submit_order($data) {
 		));
 
 
-
+include_once('send_confirmation_email_function.php');
 	send_confirmation_email($order);
 
 
@@ -375,183 +375,6 @@ function cancel_payment($data) {
 
 
 
-function send_confirmation_email($order) {
 
-	global $site,$user,$customer;
-
-	$email_mailing_list_key=0;
-
-
-	$credentials=$site->get_email_credentials();
-	if (!$credentials) {
-		return false;
-	}
-	$message_data['from_name']=$site->data['Site Name'];
-	$message_data['method']='smtp';
-	$message_data['type']='HTML';
-	$message_data['to']=$user->data['User Handle'];
-	$message_data['subject']=_('Order confirmation');
-	$message_data['html']='';
-	$message_data['email_credentials_key']=$credentials['Email Credentials Key'];
-	$message_data['email_matter']='Order Confirmation';
-	$message_data['email_matter_key']=$email_mailing_list_key;
-	$message_data['email_matter_parent_key']=$email_mailing_list_key;
-	$message_data['recipient_type']='User';
-	$message_data['recipient_key']=0;
-	$message_data['email_key']=0;
-	$message_data['plain']=null;
-
-
-
-	$payment_account=new Payment_Account($order->data['Order Payment Account Key']);
-	$payment_service_provider=new Payment_Service_Provider($payment_account->data['Payment Service Provider Key']);
-	//print_r($payment_account->data);
-	//print_r($payment_service_provider->data);
-
-	$payment_info=':)';
-	
-	if($order->data['Order Payment Key']){
-	
-		$payment = new Payment($order->data['Order Payment Key']);
-	
-	
-	}else{
-	
-	if($payment_service_provider->data['Payment Service Provider Type']=='Bank'){
-		
-		$payment_info='<p>'._('Here are our bank details').'</p><div>'.$payment_account->get_formated_bank_data().'</div><p>'._('Please always state the order number in the payment reference').'.</p>';
-	}
-}
-
-	$order_items_info=$order->get_items_info();
-	$order_info='<table  cellpadding="0">';
-	$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-	<td style="padding:5px;border-bottom:1px solid #777777">%s</td>
-	<td style="padding:5px;border-bottom:1px solid #777777">%s</td>
-	<td style="padding:5px;border-bottom:1px solid #777777">%s</td>
-	<td style="padding:5px;border-bottom:1px solid #777777">%s</td>
-	</tr>',
-		_('Code'),
-		_('Description'),
-		_('Quantity'),
-		_('Amount')
-
-	);
-
-	foreach ($order_items_info as $data) {
-		$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td style="padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-			$data['code_plain'],
-			$data['description'],
-			$data['quantity'],
-			$data['to_charge']
-
-		);
-	}
-
-
-	$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-		_('Items Net'),
-		$order->get('Items Net Amount')
-
-	);
-
-	if ($order->get('Order Net Credited Amount')!=0) {
-		$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-			_('Credits'),
-			$order->get('Net Credited Amount
-			')
-
-		);
-	}
-
-	if ($order->get('Order Charges Net Amount')!=0) {
-		$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-			_('Charges'),
-			$order->get('Charges Net Amount')
-
-		);
-	}
-
-	$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-		_('Shipping'),
-		$order->get('Shipping Net Amount')
-
-	);
-
-	$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-		_('Net'),
-		$order->get('Balance Net Amount')
-
-	);
-
-	$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-		_('Tax'),
-		$order->get('Balance Tax Amount')
-
-	);
-
-	$order_info.=sprintf('<tr style="border-bottom:1px solid #cccccc">
-		<td colspan=2></td>
-		<td style="text-align:right;padding:5px;vertical-align:top;border-bottom:1px solid #cccccc">%s</td>
-		<td style="padding:5px;vertical-align:top;text-align:right;border-bottom:1px solid #cccccc">%s</td></tr>',
-		_('Total'),
-		$order->get('Balance Total Amount')
-
-	);
-
-
-
-
-
-
-	$order_info.='</table>';
-
-	$message_data['email_placeholders']=array(
-		'CUSTOMERS_NAME' => $customer->get_name_for_grettings(),
-		'ORDER_NUMBER'=> $order->data['Order Public ID'],
-		'PAYMENT_EXTRA_INFO'=>$payment_info,
-		'ORDER_DATA'=>$order_info
-	);
-
-	$message_data['promotion_name']='thanks_cart_inikoo_AW';
-
-	if ($site->data['Site Direct Subscribe Madmimi']) {
-		$message_data['madmimi_auto_subscribe']=$site->data['Site Direct Subscribe Madmimi'];
-	}
-
-	$send_email=new SendEmail();
-
-	$send_email->track=false;
-	$send_email->secret_key=CKEY;
-
-//	print_r($message_data);
-
-	$send_email->set($message_data);
-	$result=$send_email->send();
-	//print_r($result);
-
-}
 
 ?>
--- sites/inikoo_files/thanks.php
+++ sites/inikoo_files/thanks.php
@@ -1,6 +1,227 @@
 <?php
 
-include_once('common.php');
+include_once 'common.php';
 $page_key=$site->get_page_key_from_section('Thanks');
-include_once('page.php');
+
+
+if (!isset($page_key)) {
+	header('Location: index.php?no_page_key');
+	exit;
+}
+
+$page=new Page($page_key);
+
+
+
+if (!$page->id) {
+	header('Location: index.php?no_page');
+	exit;
+}
+
+$template_suffix='';
+update_page_key_visit_log($page->id,$user_click_key);
+
+if ($logged_in) {
+	$page->customer=$customer;
+	$page->order=$order_in_process;
+}
+
+$smarty->assign('logged',$logged_in);
+$page->site=$site;
+$page->user=$user;
+$page->logged=$logged_in;
+$page->currency=$store->data['Store Currency Code'];
+$page->currency_symbol=currency_symbol($store->data['Store Currency Code']);
+$page->customer=$customer;
+
+
+$smarty->assign('title',$page->data['Page Title']);
+$smarty->assign('store',$store);
+$smarty->assign('page',$page);
+$smarty->assign('site',$site);
+
+$css_files=array();
+$js_files=array();
+// CSS JS FILES FOR ALL PAGES
+$base_css_files=array(
+	$yui_path.'reset-fonts-grids/reset-fonts-grids.css',
+	$yui_path.'menu/assets/skins/sam/menu.css',
+	$yui_path.'calendar/assets/skins/sam/calendar.css',
+	$yui_path.'button/assets/skins/sam/button.css',
+	$yui_path.'editor/assets/skins/sam/editor.css',
+	$yui_path.'assets/skins/sam/autocomplete.css'
+
+);
+$base_js_files=array(
+	$yui_path.'utilities/utilities.js',
+	$yui_path.'json/json-min.js',
+	$yui_path.'paginator/paginator-min.js',
+	$yui_path.'datasource/datasource-min.js',
+	$yui_path.'autocomplete/autocomplete-min.js',
+	$yui_path.'datatable/datatable-min.js',
+	$yui_path.'container/container-min.js',
+	$yui_path.'editor/editor-min.js',
+	$yui_path.'menu/menu-min.js',
+	$yui_path.'calendar/calendar-min.js',
+	$yui_path.'uploader/uploader-min.js',
+	'external_libs/ampie/ampie/swfobject.js',
+	'js/common.js',
+	'js/edit_common.js',
+);
+
+
+
+
+$sql=sprintf("select `External File Type`,`Page Store External File Key` as external_file_key from `Page Header External File Bridge` where `Page Header Key`=%d",$page->data['Page Header Key']);
+$res=mysql_query($sql);
+//print $sql;
+while ($row=mysql_fetch_assoc($res)) {
+	if ($row['External File Type']=='CSS')
+		$base_css_files[]='public_external_file.php?id='.$row['external_file_key'];
+	else
+		$base_js_files[]='public_external_file.php?id='.$row['external_file_key'];
+
+}
+
+$sql=sprintf("select `External File Type`,`Page Store External File Key` as external_file_key from `Page Footer External File Bridge` where `Page Footer Key`=%d",$page->data['Page Footer Key']);
+$res=mysql_query($sql);
+while ($row=mysql_fetch_assoc($res)) {
+	if ($row['External File Type']=='CSS')
+		$base_css_files[]='public_external_file.php?id='.$row['external_file_key'];
+	else
+		$base_js_files[]='public_external_file.php?id='.$row['external_file_key'];
+
+}
+
+$sql=sprintf("select `External File Type`,`Page Store External File Key` as external_file_key from `Site External File Bridge` where `Site Key`=%d",$site->id);
+$res=mysql_query($sql);
+while ($row=mysql_fetch_assoc($res)) {
+	if ($row['External File Type']=='CSS')
+		$base_css_files[]='public_external_file.php?id='.$row['external_file_key'];
+	else
+		$base_js_files[]='public_external_file.php?id='.$row['external_file_key'];
+
+}
+
+
+
+$sql=sprintf("select `External File Type`,`Page Store External File Key` as external_file_key from `Page Store External File Bridge` where `Page Key`=%d",$page->id);
+$res=mysql_query($sql);
+while ($row=mysql_fetch_assoc($res)) {
+	if ($row['External File Type']=='CSS')
+		$base_css_files[]='public_external_file.php?id='.$row['external_file_key'];
+	else
+		$base_js_files[]='public_external_file.php?id='.$row['external_file_key'];
+}
+
+
+
+
+
+$smarty->assign('type_content','file');
+
+$css_files[]='css/'.$page->data['Page Store Content Template Filename'].$template_suffix.'.css';
+
+if ($page->data['Page Code']=='login') {
+
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false) {
+		$smarty->assign('template_string','login.chrome.tpl');
+		$js_files[]='js/login.chrome.js';
+	}else {
+		$smarty->assign('template_string','login.tpl');
+		$js_files[]='js/login.js';
+	}
+}else {
+
+	$smarty->assign('template_string',$page->data['Page Store Content Template Filename'].$template_suffix.'.tpl');
+	$js_files[]='js/'.$page->data['Page Store Content Template Filename'].$template_suffix.'.js';
+}
+
+
+
+
+
+
+array_unshift($css_files,'css/order.css');
+
+
+if (!$logged_in) {
+	header('location: login.php');
+	exit;
+}
+
+
+if (!isset($_REQUEST['id'])) {
+	$smarty->assign('template_string','order_not_found.tpl');
+
+}else {
+
+	$order_key=$_REQUEST['id'];
+	$order=new Order($order_key);
+
+	if (!$order->id) {
+		$smarty->assign('template_string','order_not_found.tpl');
+
+	}else {
+
+
+
+
+
+		$page->order=$order;
+
+
+		$smarty->assign('referral','');
+		$smarty->assign('products_display_type','ordered');
+
+		$js_files[]='js/table_common.js';
+		$js_files[]='js/edit_common.js';
+
+		array_unshift($css_files,'css/table.css');
+		array_unshift($css_files,'css/edit.css');
+		array_unshift($css_files,'css/inikoo.css');
+
+
+
+
+
+
+		$smarty->assign('filter0','code');
+		$smarty->assign('filter_value0','');
+		$filter_menu=array(
+			'code'=>array('db_key'=>'code','menu_label'=>'Code starting with  <i>x</i>','label'=>'Code'),
+			'family'=>array('db_key'=>'family','menu_label'=>'Family starting with  <i>x</i>','label'=>'Code'),
+			'name'=>array('db_key'=>'name','menu_label'=>'Name starting with  <i>x</i>','label'=>'Code')
+
+		);
+		$smarty->assign('filter_menu0',$filter_menu);
+		$smarty->assign('filter_name0',$filter_menu['code']['label']);
+
+
+		$paginator_menu=array(10,25,50,100);
+		$smarty->assign('paginator_menu0',$paginator_menu);
+
+
+
+		$smarty->assign('order',$order);
+		$smarty->assign('customer',$customer);
+
+
+
+		$charges_deal_info=$order_in_process->get_no_product_deal_info('Charges');
+		if ($charges_deal_info!='') {
+			$charges_deal_info='<span style="color:red" title="'.$charges_deal_info.'">*</span> ';
+		}
+		$smarty->assign('charges_deal_info',$charges_deal_info);
+	}
+}
+
+
+
+
+$smarty->assign('css_files',array_merge( $base_css_files,$css_files));
+$smarty->assign('js_files',array_merge( $base_js_files,$js_files));
+
+$smarty->display('page.tpl');
+
 ?>
