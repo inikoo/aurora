044805a45a1166d6a3e24ada2841bddffaa3a02e
Raul Perusquia <rulovico@gmail.com>
2014-06-27 11:30:05 +0100

    xx

--- js/common_order_not_dispatched.js
+++ js/common_order_not_dispatched.js
@@ -103,10 +103,10 @@ function save_set_shipping() {
     Dom.setStyle('save_set_shipping', 'display', 'none');
 
     //return;
-    alert(request);
+    //alert(request);
     YAHOO.util.Connect.asyncRequest('POST', ar_file, {
         success: function(o) {
-            //alert(o.responseText);
+            alert(o.responseText);
             var r = YAHOO.lang.JSON.parse(o.responseText);
             if (r.state == 200) {
                 Dom.setStyle('save_set_shipping_wait', 'display', 'none');
