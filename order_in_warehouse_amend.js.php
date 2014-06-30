02fe253d0ac67ca92e4ce00fb4fe107d1aaf8b36
Raul Perusquia <rulovico@gmail.com>
2014-06-30 20:49:23 +0100

    xx

--- order_in_warehouse_amend.js.php
+++ order_in_warehouse_amend.js.php
@@ -142,7 +142,7 @@ YAHOO.util.Event.addListener(window, "load", function() {
 
 		//alert("ar_edit_orders.php?tipo=transactions_to_process&tid=0&sf=0&f_value=&display="+Dom.get('products_display_type').value);
 	  request="ar_edit_orders.php?tipo=transactions_to_process&tid=0&sf=0&f_value=&display=products&order_key="+Dom.get('order_key').value+"&store_key="+Dom.get('store_key').value+'&tableid='+tableid+'&lookup_family='+Dom.get('lookup_family').value
-	//alert(request)
+	alert(request)
 	  	    this.dataSource1 = new YAHOO.util.DataSource(request);
 	 // alert("ar_edit_orders.php?tipo=transactions_to_process&tid=0&sf=0&f_value=&display="+Dom.get('products_display_type').value)
 	    this.dataSource1.responseType = YAHOO.util.DataSource.TYPE_JSON;
