59bd7984d7e4a768ce0d4530262d80668d29890e
Raul Perusquia <rulovico@gmail.com>
2014-07-21 13:13:15 +0200

    xx

--- class.Order.php
+++ class.Order.php
@@ -4770,7 +4770,7 @@ class Order extends DB_Table {
 						$this->id,
 						prepare_mysql(date('Y-m-d',strtotime("now -".$deal_component_data['Deal Component Terms'])).' 00:00:00')
 					);
-				
+				print $sql;
 					$res2=mysql_query($sql);
 					if ($_row=mysql_fetch_assoc($res2)) {
 
