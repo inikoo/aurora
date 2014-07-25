3c304572e8c334b78057d66946cc1ca627a42f18
Raul Perusquia <rulovico@gmail.com>
2014-07-25 18:02:23 +0200

    xx

--- class.Invoice.php
+++ class.Invoice.php
@@ -445,8 +445,8 @@ if (array_key_exists('Invoice Tax Number',$invoice_data)) {
 			$orders_keys=join(',',$_orders_ids);
 			
 			var_dump(join(',',$_orders_ids));
-			
-				$sql=sprintf('select `Order No Product Transaction Fact Key`,`Transaction Net Amount`,`Transaction Tax Amount`,`Transaction Type`  from `Order No Product Transaction Fact` where `Order Key` in (%s) and ISNULL(`Invoice Key`) '
+			var_dump($orders_keys);
+				$sql=sprintf("select `Order No Product Transaction Fact Key`,`Transaction Net Amount`,`Transaction Tax Amount`,`Transaction Type`  from `Order No Product Transaction Fact` where `Order Key` in (%s) and ISNULL(`Invoice Key`) "
 				,$order_keys);
 			$res=mysql_query($sql);
 			
