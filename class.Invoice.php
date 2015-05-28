62d376eae48be24a15f9a107ca3da2ec7970b1e4
Raul Perusquia <raul@inikoo.com>
2015-05-28 12:09:42 +0200

    fix error in calculate the tax

--- class.Invoice.php
+++ class.Invoice.php
@@ -1242,7 +1242,7 @@ class Invoice extends DB_Table {
 					if (array_key_exists($row['Tax Category Code'],$tax_sum_by_code))
 						$tax_sum_by_code[$row['Tax Category Code']]['net']+=$net;
 					else
-						$tax_sum_by_code[$row['tax_code']]=array('net'=>$net,'rate'=>$tax_category->data['Transaction Tax Rate']);
+						$tax_sum_by_code[$row['Tax Category Code']]=array('net'=>$net,'rate'=>$tax_category->data['Tax Category Rate']);
 				}
 
 
