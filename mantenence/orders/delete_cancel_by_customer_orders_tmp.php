2fd966f1219335043065b0de6cdbfaa409c16b66
Raul Perusquia <raul@inikoo.com>
2015-05-30 12:58:47 +0100

    temporal script to remove all cancelled by customer orders

---
+++ mantenence/orders/delete_cancel_by_customer_orders_tmp.php
@@ -0,0 +1,93 @@
+<?php
+//@author Raul Perusquia <rulovico@gmail.com>
+//Copyright (c) 2009 LW
+include_once '../../conf/dns.php';
+include_once '../../class.Department.php';
+include_once '../../class.Family.php';
+include_once '../../class.Product.php';
+include_once '../../class.Supplier.php';
+include_once '../../class.Part.php';
+include_once '../../class.PartLocation.php';
+include_once '../../class.Order.php';
+
+include_once '../../class.SupplierProduct.php';
+error_reporting(E_ALL);
+
+date_default_timezone_set('UTC');
+include_once '../../set_locales.php';
+require '../../locale.php';
+$_SESSION['locale_info'] = localeconv();
+
+$con=@mysql_connect($dns_host,$dns_user,$dns_pwd );
+
+if (!$con) {print "Error can not connect with database server\n";exit;}
+
+$db=@mysql_select_db($dns_db, $con);
+if (!$db) {print "Error can not access the database\n";exit;}
+
+
+require_once '../../common_functions.php';
+
+mysql_set_charset('utf8');
+require_once '../../conf/conf.php';
+date_default_timezone_set('UTC');
+
+$inikoo_account=new Account(1);
+
+$sql="select `Order Key` from `Order Dimension` where  `Order Current Dispatch State` in  ('Cancelled by Customer')  order by `Order Key` desc";
+$result=mysql_query($sql);
+while ($row=mysql_fetch_array($result, MYSQL_ASSOC)   ) {
+	$order=new Order($row['Order Key']);
+
+	print $order->data['Order Date']." ".$order->data['Order Public ID']."  \n";
+
+	$sql=sprintf("select `History Key` from `History Dimension` where `Direct Object`='Order' and `Direct Object Key`=%d",$order->id);
+	$result2=mysql_query($sql);
+	while ($row2=mysql_fetch_array($result2, MYSQL_ASSOC)   ) {
+
+		$sql=sprintf('delete from `Customer History Bridge` where `History Key`=%d',$row2['History Key']);
+		mysql_query($sql);
+
+
+		$sql=sprintf('delete from `History Dimension` where `History Key`=%d',$row2['History Key']);
+		mysql_query($sql);
+	}
+
+
+
+	$sql=sprintf("select * from `Order History Bridge` where `Order Key`=%d",$order->id);
+	$result2=mysql_query($sql);
+	while ($row2=mysql_fetch_array($result2, MYSQL_ASSOC)   ) {
+
+		$sql=sprintf('delete from `History Dimension` where `History Key`=%d',$row2['History Key']);
+		mysql_query($sql);
+	}
+
+
+
+
+	$sql=sprintf('delete from `Order Deal Bridge` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order Basket History Dimension` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order Meta Transaction Deal Dimension` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order No Product Transaction Deal Bridge` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order No Product Transaction Fact` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order Transaction Deal Bridge` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order Transaction Fact` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Voucher Order Bridge` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+	$sql=sprintf('delete from `Order Dimension` where `Order Key`=%d',$order->id);
+	mysql_query($sql);
+
+
+
+}
+
+
+?>
