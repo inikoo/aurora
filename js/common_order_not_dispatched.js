6a51c6f4c15eb9200a8a88d8739f9131c5b4514d
Raul Perusquia <raul@inikoo.com>
2015-06-10 15:43:45 +0100

    debug

--- js/common_order_not_dispatched.js
+++ js/common_order_not_dispatched.js
@@ -1934,7 +1934,7 @@ function remove_voucher(voucher_key) {
     YAHOO.util.Connect.asyncRequest('POST', request, {
         success: function(o) {
 
-            //   alert(o.responseText)
+              alert(o.responseText)
             var r = YAHOO.lang.JSON.parse(o.responseText);
 
             if (r.state == '200') {
