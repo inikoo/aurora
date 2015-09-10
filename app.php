4e57403673c694fe2ed3598442357e3e427ad90d
Raul Perusquia <raul@inikoo.com>
2015-09-10 15:34:17 +0800

    missing files previous comit

---
+++ app.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ About:
+ Autor: Raul Perusquia <raul@inikoo.com>
+ Created:27 August 2015 12:49:03 GMT+8, Singapure
+ Copyright (c) 2015, Inikoo
+
+ Version 3
+
+*/
+
+require_once 'common.php';
+
+
+$smarty->assign('_request',$_SERVER['REQUEST_URI']);
+$smarty->display('app.tpl');
+
+?>
