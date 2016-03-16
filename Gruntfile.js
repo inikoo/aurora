65fc5481421d522fe45e768282688c843c218758
Raul Perusquia <raul@inikoo.com>
2016-03-16 16:20:59 +0800

    adding js uglify to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,26 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         concat: {
-            // 2. Configuration for concatinating files goes here.
+            dist: {
+                src: ['js/*.js', ],
+                dest: 'build/js/aurora.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: 'build/js/aurora.js',
+                dest: 'build/js/aurora.min.js',
+            }
         }
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat','uglify']);
 
 };
