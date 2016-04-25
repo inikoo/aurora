450bdc5b52185a039e718bb284536be21f2de0e4
Raul Perusquia <raul@inikoo.com>
2016-04-25 15:21:48 +0800

    profile edit little cleaning

--- tabs/profile.details.tab.php
+++ tabs/profile.details.tab.php
@@ -9,77 +9,14 @@
 
 */
 
+include_once 'conf/object_fields.php';
+
 include_once 'utils/invalid_messages.php';
-include 'utils/available_locales.php';
-include 'conf/user_groups.php';
 
 $system_user=new User($state['key']);
 
+$object_fields=get_object_fields($system_user, $db, $user, $smarty,array('type'=>'profile'));
 
-$options_yn=array(
-	'Yes'=>_('Yes'), 'No'=>_('No')
-);
-
-$options_locales=array();
-foreach ($available_locales as $locale) {
-
-	$options_locales[$locale['Locale']]=$locale['Language Name'].($locale['Language Name']!=$locale['Language Original Name']?' ('.$locale['Language Original Name'].')':'');
-}
-
-
-
-
-
-$object_fields=array(
-
-
-	array(
-		'label'=>_('Access'),
-		'show_title'=>true,
-		'class'=>'edit_fields',
-		'fields'=>array(
-	
-			array(
-				'render'=>($system_user->get('User Active')=='Yes'?true:false),
-				'id'=>'User_Password',
-				'edit'=>'password',
-				'value'=>$system_user->get('User Password'),
-				'formatted_value'=>$system_user->get('Password'),
-				'label'=>_('Password'),
-				'invalid_msg'=>get_invalid_message('password'),
-			),
-			array(
-				'render'=>($system_user->get('User Active')=='Yes'?true:false),
-				'id'=>'User_PIN',
-				'edit'=>'pin',
-				'value'=>$system_user->get('User PIN'),
-				'formatted_value'=>$system_user->get('PIN'),
-				'label'=>_('PIN'),
-				'invalid_msg'=>get_invalid_message('pin'),
-			),
-
-		)
-	),
-	
-	array(
-		'label'=>_('Preferences'),
-		'show_title'=>true,
-		'class'=>'edit_fields',
-		'fields'=>array(
-			array(
-				'id'=>'User_Preferred_Locale',
-				'edit'=>'option',
-				'value'=>$system_user->get('User Preferred Locale') ,
-				'formatted_value'=>$system_user->get('Preferred Locale') ,
-				'label'=>ucfirst($system_user->get_field_label('Preferred Locale')),
-				'options'=>$options_locales,
-
-
-			)
-
-		)
-	),
-);
 $smarty->assign('state', $state);
 $smarty->assign('object_fields', $object_fields);
 
