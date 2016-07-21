b10dc11de8e679e0eda2ce7e4c8366e83a19edd9
Raul Perusquia <raul@inikoo.com>
2016-07-21 21:21:31 +0800

    fix errors deleting templete operation

--- conf/fields/barcode.fld.php
+++ conf/fields/barcode.fld.php
@@ -47,7 +47,7 @@ $operations=array(
 			'id'=>'delete_barcode',
 			'class'=>'operation',
 			'value'=>'',
-			'label'=>'<i class="fa fa-fw fa-lock button" onClick="toggle_unlock_delete_object(this)" style="margin-right:20px"></i> <span data-data='{ "object": "{$object->get_object_name()}", "key":"{$object->id}"}' onClick="delete_object(this)" class="delete_object disabled">'._('Delete barcode').' <i class="fa fa-trash new_button link"></i></span>',
+			'label'=>'<i class="fa fa-fw fa-lock button" onClick="toggle_unlock_delete_object(this)" style="margin-right:20px"></i> <span data-data=\'{ "object": "'.$object->get_object_name().'", "key":"'.$object->id.'"}\' onClick="delete_object(this)" class="delete_object disabled">'._('Delete barcode').' <i class="fa fa-trash new_button link"></i></span>',
 			'reference'=>'',
 			'type'=>'operation'
 		),
