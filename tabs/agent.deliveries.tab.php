569515d8288a1ce518a0e07f67bc03c9cdb2269f
Raul Perusquia <raul@inikoo.com>
2016-07-21 22:10:32 +0800

    deliveries list in suppliers and agents

---
+++ tabs/agent.deliveries.tab.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ About:
+ Autor: Raul Perusquia <raul@inikoo.com>
+ Created: 21 July 2016 at 22:09:23 GMT+8, Kuala Lumpur, Malaysia
+ Copyright (c) 2016 Inikoo
+
+ Version 3
+
+*/
+
+$tab='supplier.deliveries';
+$ar_file='ar_suppliers_tables.php';
+$tipo='deliveries';
+
+$default=$user->get_tab_defaults($tab);
+
+$table_views=array(
+);
+
+$table_filters=array(
+	'number'=>array('label'=>_('Number')),
+);
+
+$parameters=array(
+	'parent'=>$state['object'],
+	'parent_key'=>$state['key'],
+
+);
+
+include 'utils/get_table_html.php';
+
+?>
