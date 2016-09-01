cdbf440ba075382770169860b66a5635f5595212
Raul Perusquia <raul@inikoo.com>
2016-09-01 12:52:52 +0800

    clean debug

--- class.TimeSeries.php
+++ class.TimeSeries.php
@@ -58,8 +58,7 @@ class Timeseries extends DB_Table {
 				$this->parent=new Store($this->data['Timeseries Parent Key']);
 
 			}else {
-			    print_r($this);
-			    print "getting parent\n";
+			 
 				$this->parent=new Account($this->db);
 			}
 		}
