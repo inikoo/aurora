f0c5e404903a71fad47d8d6137d89bf1b5a654a1
Raul Perusquia <raul@inikoo.com>
2016-09-14 16:59:01 +0800

    product eroor

--- utils/object_functions.php
+++ utils/object_functions.php
@@ -37,7 +37,7 @@ function get_object($object_name, $key, $load_other_data=false) {
 	case 'product':
 	case 'service':
 		include_once 'class.Product.php';
-		$object=new Product($key);
+		$object=new Product('id',$key);
 		$object->get_webpage();
 		break;
 	case 'order':
