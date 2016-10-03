9e1f9e7987420bfd9381ad47ef57c9527f50bacb
Raul Perusquia <raul@inikoo.com>
2016-10-04 04:32:12 +0800

    debugging

--- cron/update_parts_categories_sales.php
+++ cron/update_parts_categories_sales.php
@@ -51,19 +51,19 @@ function update_parts_sales($db, $print_est) {
 
 
 
-			//$category->update_part_category_sales('Total');
-			//$category->update_part_category_sales('Week To Day');
-			//$category->update_part_category_sales('Month To Day');
+			$category->update_part_category_sales('Total');
+			$category->update_part_category_sales('Week To Day');
+			$category->update_part_category_sales('Month To Day');
 			$category->update_part_category_sales('Quarter To Day');
-			//$category->update_part_category_sales('Year To Day');
-			//$category->update_part_category_sales('1 Year');
-			//$category->update_part_category_sales('1 Quarter');
+			$category->update_part_category_sales('Year To Day');
+			$category->update_part_category_sales('1 Year');
+			$category->update_part_category_sales('1 Quarter');
 
 			$contador++;
 			$lap_time1=date('U');
 
 			if ($print_est) {
-		//		print 'Pa '.percentage($contador, $total, 3)."  lap time ".sprintf("%.2f", ($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h  ($contador/$total) \r";
+			print 'Pa '.percentage($contador, $total, 3)."  lap time ".sprintf("%.2f", ($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h  ($contador/$total) \r";
 			}
 
 
--- cron/update_parts_sales.php
+++ cron/update_parts_sales.php
@@ -55,19 +55,19 @@ function update_parts_sales($db, $print_est) {
 
 			$part->load_acc_data();
 
-			//$part->update_sales_from_invoices('Total');
-			//$part->update_sales_from_invoices('Week To Day');
-			//$part->update_sales_from_invoices('Month To Day');
+			$part->update_sales_from_invoices('Total');
+			$part->update_sales_from_invoices('Week To Day');
+			$part->update_sales_from_invoices('Month To Day');
 			$part->update_sales_from_invoices('Quarter To Day');
-			//$part->update_sales_from_invoices('Year To Day');
-			//$part->update_sales_from_invoices('1 Year');
-			//$part->update_sales_from_invoices('1 Quarter');
+			$part->update_sales_from_invoices('Year To Day');
+			$part->update_sales_from_invoices('1 Year');
+			$part->update_sales_from_invoices('1 Quarter');
 
 			$contador++;
 			$lap_time1=date('U');
 
 			if ($print_est) {
-			//	print 'Pa '.percentage($contador, $total, 3)."  lap time ".sprintf("%.2f", ($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h  ($contador/$total) \r";
+				print 'Pa '.percentage($contador, $total, 3)."  lap time ".sprintf("%.2f", ($lap_time1-$lap_time0)/$contador)." EST  ".sprintf("%.1f", (($lap_time1-$lap_time0)/$contador)*($total-$contador)/3600)  ."h  ($contador/$total) \r";
 			}
 
 
