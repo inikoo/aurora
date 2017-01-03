e60501dfb898ba1bc4a884eb89d29170b6a5839d
Raul Perusquia <raul@inikoo.com>
2017-01-03 18:14:25 +0100

    debug

--- tabs/category.webpage.preview.tab.php
+++ tabs/category.webpage.preview.tab.php
@@ -328,7 +328,7 @@ switch ($webpage->get('Page Store Content Template Filename')) {
         // todo remove this when all descriptions are moved inside webpage content data
 
 
-        if ($webpage->id and $webpage->get('Content Data') == '') {
+        if ($webpage->id and $webpage->get('Content Data') == '' or true  ) {
 
 
             $sql = sprintf(
