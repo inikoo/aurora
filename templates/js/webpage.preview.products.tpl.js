41554934223ab63d3f1f12aa56c482742243577a
Raul Perusquia <raul@inikoo.com>
2017-01-18 16:26:37 +0000

    long size

--- templates/js/webpage.preview.products.tpl.js
+++ templates/js/webpage.preview.products.tpl.js
@@ -181,6 +181,28 @@
 
 
 
+    $( ".item_name" ).each(function( index ) {
+
+
+        var len_fit = 60; // According to your question, 10 letters can fit in.
+        var un = $(this)
+
+        // Get the lenght of user name.
+        var len_user_name = un.html().length;
+        if(len_fit < len_user_name ) {
+
+            // Calculate the new font size.
+            var size_now = parseInt(un.css("font-size"));
+            var size_new = size_now * len_fit / len_user_name;
+
+            // Set the new font size to the user name.
+            un.css("font-size", size_new);
+
+        }
+
+        });
+
+
 
     $('#sort_options i').click(function() {
 
