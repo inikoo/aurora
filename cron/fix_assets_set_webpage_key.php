8f8e80995a1fe0aa52887aedc37b15caf8521570
Raul Perusquia <raul@inikoo.com>
2017-06-18 14:05:53 +0800

    fix prev commit bugs

--- cron/fix_assets_set_webpage_key.php
+++ cron/fix_assets_set_webpage_key.php
@@ -19,27 +19,27 @@ include_once 'class.Public_Category.php';;
 
 
 $sql = sprintf(
-	"SELECT `Product ID` FROM `Product Dimension` "
+    "SELECT `Product ID` FROM `Product Dimension` "
 );
 if ($result = $db->query($sql)) {
-	foreach ($result as $row) {
+    foreach ($result as $row) {
 
-		$product = new Product($row['Product ID']);
+        $product = new Product($row['Product ID']);
 
-print $product->get('Code')."\n";
-		$subject_webpage = new Public_Webpage('scope', 'Product', $product->id);
-		$subject_webpage_key=$subject_webpage->id;
+        print $product->get('Code')."\n";
+        $subject_webpage     = new Public_Webpage('scope', 'Product', $product->id);
+        $subject_webpage_key = $subject_webpage->id;
 
 
-//print $subject_webpage_key." xx \n";
+        //print $subject_webpage_key." xx \n";
 
-	  $product->update(array('Product Webpage Key'=>$subject_webpage_key), 'no_history');
-	}                      
+        $product->update(array('Product Webpage Key' => $subject_webpage_key), 'no_history');
+    }
 
 
 } else {
-	print_r($error_info = $db->errorInfo());
-	exit;
+    print_r($error_info = $db->errorInfo());
+    exit;
 }
 
 
@@ -47,50 +47,43 @@ $where = "where true";
 //$where="where `Category Key`=15362";
 
 $sql = sprintf(
-	"select count(distinct `Category Key`) as num from `Category Dimension` $where and  `Category Scope`='Product' "
+    "select count(distinct `Category Key`) as num from `Category Dimension` $where and  `Category Scope`='Product' "
 );
 
 if ($result = $db->query($sql)) {
-	if ($row = $result->fetch()) {
-		$total = $row['num'];
-	} else {
-		$total = 0;
-	}
+    if ($row = $result->fetch()) {
+        $total = $row['num'];
+    } else {
+        $total = 0;
+    }
 } else {
-	print_r($error_info = $db->errorInfo());
-	exit;
+    print_r($error_info = $db->errorInfo());
+    exit;
 }
 
 $lap_time0 = date('U');
 $contador  = 0;
 
 $sql = sprintf(
-	"select `Category Key` from `Category Dimension` $where and  `Category Scope`='Product' "
+    "select `Category Key` from `Category Dimension` $where and  `Category Scope`='Product' "
 );
 
 if ($result = $db->query($sql)) {
-	foreach ($result as $row) {
-		$subject = new Category($row['Category Key']);
+    foreach ($result as $row) {
+        $subject = new Category($row['Category Key']);
 
-		$subject_webpage = new Public_Webpage('scope', ($subject->get('Category Subject') == 'Category' ? 'Category Categories' : 'Category Products'), $subject->id);
-		$subject_webpage_key=$subject_webpage->id;
+        $subject_webpage     = new Public_Webpage('scope', ($subject->get('Category Subject') == 'Category' ? 'Category Categories' : 'Category Products'), $subject->id);
+        $subject_webpage_key = $subject_webpage->id;
 
-		$subject->update(array('Product Category Webpage Key'=>$subject_webpage_key), 'no_history');
+        $subject->update(array('Product Category Webpage Key' => $subject_webpage_key), 'no_history');
 
 
-	}
+    }
 
 } else {
-	print_r($error_info = $db->errorInfo());
-	exit;
+    print_r($error_info = $db->errorInfo());
+    exit;
 }
-
-
-
-
-
-
-
 
 
 ?>
