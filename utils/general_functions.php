95d34dea5013c6eb074d04a49c65785aafaa21d2
Raul Perusquia <raul@inikoo.com>
2017-08-08 22:47:54 +0200

    fix bugs

--- utils/general_functions.php
+++ utils/general_functions.php
@@ -12,6 +12,29 @@
 //@author Raul Perusquia <rulovico@gmail.com>
 //Copyright (c) 2009 LW
 
+
+if(!function_exists('hash_equals'))
+{
+    function hash_equals($str1, $str2)
+    {
+        if(strlen($str1) != strlen($str2))
+        {
+            return false;
+        }
+        else
+        {
+            $res = $str1 ^ $str2;
+            $ret = 0;
+            for($i = strlen($res) - 1; $i >= 0; $i--)
+            {
+                $ret |= ord($res[$i]);
+            }
+            return !$ret;
+        }
+    }
+}
+
+
 function get_prev_next($pivot, $array) {
     $prev_key = current($array);
     $next_key = false;
