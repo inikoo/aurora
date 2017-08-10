d0c18b8758eb9c51a6751b345ffc0ed62dbab722
Raul Perusquia <raul@inikoo.com>
2017-08-10 08:20:37 +0200

    fix change images in department

--- templates/js/webpage.preview.categories.tpl.js
+++ templates/js/webpage.preview.categories.tpl.js
@@ -1,4 +1,4 @@
-$('#overview_container,#items_container').on('dblclick', '.category_image img', function () {
+$('#overview_container,#items_container').on('dblclick', '.category_image', function () {
 
 
 
@@ -7,7 +7,7 @@ $('#overview_container,#items_container').on('dblclick', '.category_image img', 
     var index_key = $(this).closest('.item_showcase').attr('index_key');
 
 
-    console.log(index_key)
+    //console.log(index_key)
 
     $('#item_image_uploader').attr('section_key',section_key).attr('item_key',item_key).attr('index_key',index_key).click()
 
