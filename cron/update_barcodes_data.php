d1214d37b671a1a1a435dca92bfe26897eea89a7
Raul Perusquia <raul@inikoo.com>
2017-11-02 11:51:51 +0800

    debug

---
+++ cron/update_barcodes_data.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 2 November 2017 at 11:47:48 GMT+8, Sanur, Bali, Indonesia
+ Copyright (c) 2016, Inikoo
+
+ Version 3
+
+*/
+
+require_once 'common.php';
+require_once 'utils/object_functions.php';
+
+
+$print_est = true;
+
+$sql = sprintf(
+    'SELECT `Barcode Key` FROM `Barcode Dimension` '
+);
+
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+        $barcode = get_object('Barcode', $row['Barcode Key']);
+        $barcode->update_status();
+    }
+} else {
+    print_r($error_info = $db->errorInfo());
+    print "$sql\n";
+    exit;
+}
+
+
+?>
