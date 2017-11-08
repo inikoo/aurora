d4651b47d8aa8afefde57e5adaeef3423470702f
Raul Perusquia <raul@inikoo.com>
2017-11-08 23:49:48 +0800

    fix bug

--- class.Product.php
+++ class.Product.php
@@ -1716,13 +1716,16 @@ class Product extends Asset {
 
 
                 $msg = new_fork(
-                    'housekeeping', array(
+                    'housekeeping',
+                    array(
                     'type'       => 'product_web_state',
                     'product_id' => $this->id,
                     'wa'         => $web_availability,
                     'wa_updated' => $web_availability_updated
 
-                ), $account->get('Account Code')
+                ),
+                    $account->get('Account Code'),
+                    $this->db
                 );
 
 
