adc9549c417188fcaca83969347908a5cb215e63
Raul Perusquia <raul@inikoo.com>
2017-12-19 16:42:06 +0000

    fix bugs

--- navigation/warehouses.nav.php
+++ navigation/warehouses.nav.php
@@ -268,6 +268,9 @@ function get_location_navigation($data, $smarty, $user, $db, $account) {
 
 
     if (isset($_SESSION['table_state'][$tab])) {
+
+
+
         $number_results  = $_SESSION['table_state'][$tab]['nr'];
         $start_from      = 0;
         $order           = $_SESSION['table_state'][$tab]['o'];
@@ -283,9 +286,12 @@ function get_location_navigation($data, $smarty, $user, $db, $account) {
         $order_direction          = ($default['sort_order'] == 1 ? 'desc' : '');
         $f_value                  = '';
         $parameters               = $default;
-        $parameters['parent']     = $data['parent'];
-        $parameters['parent_key'] = $data['parent_key'];
+
     }
+    $parameters['parent']     = $data['parent'];
+    $parameters['parent_key'] = $data['parent_key'];
+
+
 
 
     include_once 'prepare_table/'.$tab.'.ptble.php';
@@ -302,97 +308,107 @@ function get_location_navigation($data, $smarty, $user, $db, $account) {
     $prev_key   = 0;
     $next_key   = 0;
     $sql        = trim($sql_totals." $wheref");
+
+
+
     if ($result2 = $db->query($sql)) {
-        if ($row2 = $result2->fetch() and $row2['num'] > 1) {
+        if ($row2 = $result2->fetch() ) {
 
 
-            $sql = sprintf(
-                "select `Location Code` object_name,L.`Location Key` as object_key from %s
-	                and ($_order_field < %s OR ($_order_field = %s AND L.`Location Key` < %d))  order by $_order_field desc , L.`Location Key` desc limit 1", ' $table   $where $wheref ',
-                prepare_mysql($_order_field_value), prepare_mysql($_order_field_value), $object->id
-            );
-            if ($result = $db->query($sql)) {
-                if ($row = $result->fetch()) {
-                    $prev_key   = $row['object_key'];
-                    $prev_title = _("Product").' '.$row['object_name'].' ('.$row['object_key'].')';
-                }
-            }
 
 
-            $sql = sprintf(
-                "select `Location Code` object_name,L.`Location Key` as object_key from  %s
-	                and ($_order_field  > %s OR ($_order_field  = %s AND L.`Location Key` > %d))  order by $_order_field   , L.`Location Key`  limit 1", ' $table   $where $wheref ',
-                prepare_mysql($_order_field_value), prepare_mysql($_order_field_value), $object->id
-            );
-
-            if ($result = $db->query($sql)) {
-                if ($row = $result->fetch()) {
-                    $next_key   = $row['object_key'];
-                    $next_title = _("Product").' '.$row['object_name'].' ('.$row['object_key'].')';
-                }
-            }
+            if ($row2['num'] > 1) {
 
 
-            if ($order_direction == 'desc') {
-                $_tmp1      = $prev_key;
-                $_tmp2      = $prev_title;
-                $prev_key   = $next_key;
-                $prev_title = $next_title;
-                $next_key   = $_tmp1;
-                $next_title = $_tmp2;
-            }
 
 
-            switch ($data['parent']) {
-                case 'warehouse':
-
-                    $up_button = array(
-                        'icon'      => 'arrow-up',
-                        'title'     => _(
-                            'Warehouse'
-                        ),
-                        'reference' => 'warehouse/'.$data['parent_key'],
-                        'parent'    => ''
-                    );
-
-
-                    if ($prev_key) {
-                        $left_buttons[] = array(
-                            'icon'      => 'arrow-left',
-                            'title'     => $prev_title,
-                            'reference' => 'locations/'.$data['parent_key'].'/'.$prev_key
-                        );
-
-                    } else {
-                        $left_buttons[] = array(
-                            'icon'  => 'arrow-left disabled',
-                            'title' => ''
-                        );
-
+                $sql = sprintf(
+                    "select `Location Code` object_name,L.`Location Key` as object_key from %s %s %s
+	                and ($_order_field < %s OR ($_order_field = %s AND L.`Location Key` < %d))  order by $_order_field desc , L.`Location Key` desc limit 1",  $table  , $where ,$wheref , prepare_mysql($_order_field_value), prepare_mysql($_order_field_value), $object->id
+                );
+                if ($result = $db->query($sql)) {
+                    if ($row = $result->fetch()) {
+                        $prev_key   = $row['object_key'];
+                        $prev_title = _("Product").' '.$row['object_name'].' ('.$row['object_key'].')';
                     }
-                    $left_buttons[] = $up_button;
+                }
 
 
-                    if ($next_key) {
-                        $left_buttons[] = array(
-                            'icon'      => 'arrow-right',
-                            'title'     => $next_title,
-                            'reference' => 'locations/'.$data['parent_key'].'/'.$next_key
-                        );
 
-                    } else {
-                        $left_buttons[] = array(
-                            'icon'  => 'arrow-right disabled',
-                            'title' => '',
-                            'url'   => ''
-                        );
 
+                $sql = sprintf(
+                    "select `Location Code` object_name,L.`Location Key` as object_key from  %s %s %s
+	                and ($_order_field  > %s OR ($_order_field  = %s AND L.`Location Key` > %d))  order by $_order_field   , L.`Location Key`  limit 1", $table ,  $where, $wheref, prepare_mysql($_order_field_value), prepare_mysql($_order_field_value), $object->id
+                );
+
+                if ($result = $db->query($sql)) {
+                    if ($row = $result->fetch()) {
+                        $next_key   = $row['object_key'];
+                        $next_title = _("Product").' '.$row['object_name'].' ('.$row['object_key'].')';
                     }
+                }
 
-                    break;
+
+                if ($order_direction == 'desc') {
+                    $_tmp1      = $prev_key;
+                    $_tmp2      = $prev_title;
+                    $prev_key   = $next_key;
+                    $prev_title = $next_title;
+                    $next_key   = $_tmp1;
+                    $next_title = $_tmp2;
+                }
+
+
+                switch ($data['parent']) {
+                    case 'warehouse':
+
+                        $up_button = array(
+                            'icon'      => 'arrow-up',
+                            'title'     => _(
+                                'Warehouse'
+                            ),
+                            'reference' => 'warehouse/'.$data['parent_key'],
+                            'parent'    => ''
+                        );
+
+
+                        if ($prev_key) {
+                            $left_buttons[] = array(
+                                'icon'      => 'arrow-left',
+                                'title'     => $prev_title,
+                                'reference' => 'locations/'.$data['parent_key'].'/'.$prev_key
+                            );
+
+                        } else {
+                            $left_buttons[] = array(
+                                'icon'  => 'arrow-left disabled',
+                                'title' => ''
+                            );
+
+                        }
+                        $left_buttons[] = $up_button;
+
+
+                        if ($next_key) {
+                            $left_buttons[] = array(
+                                'icon'      => 'arrow-right',
+                                'title'     => $next_title,
+                                'reference' => 'locations/'.$data['parent_key'].'/'.$next_key
+                            );
+
+                        } else {
+                            $left_buttons[] = array(
+                                'icon'  => 'arrow-right disabled',
+                                'title' => '',
+                                'url'   => ''
+                            );
+
+                        }
+
+                        break;
+                }
             }
         }
-
 
     }
 
