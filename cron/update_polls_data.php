8d5c1fd9e18572eb462f0ef7eaf60599fb5c4c83
Raul Perusquia <raul@inikoo.com>
2018-02-26 14:25:32 +0800

    update polls data script

---
+++ cron/update_polls_data.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 26 February 2018 at 14:21:19 GMT+8, Kuala Lumpur, Malaysia
+ Copyright (c) 2016, Inikoo
+
+ Version 3
+
+*/
+require_once 'common.php';
+
+
+$sql = sprintf('SELECT `Customer Poll Query Option Key` FROM `Customer Poll Query Option Dimension` ');
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+        $poll_option = get_object('Customer_Poll_Query_Option', $row['Customer Poll Query Option Key']);
+        $poll_option->update_customers();
+    }
+} else {
+    print_r($error_info = $db->errorInfo());
+    print "$sql\n";
+    exit;
+}
+
+
+$sql = sprintf('SELECT `Customer Poll Query Key` FROM `Customer Poll Query Dimension` ');
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+        $poll_query = get_object('Customer_Poll_Query', $row['Customer Poll Query Key']);
+        $poll_query->update_answers();
+    }
+} else {
+    print_r($error_info = $db->errorInfo());
+    print "$sql\n";
+    exit;
+}
+
+
+?>
