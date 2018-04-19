ca43e0080aa33d44a7c5b0c312f193130f9a39d7
Raul Perusquia <raul@inikoo.com>
2018-04-19 16:11:25 +0100

    test

--- api.php
+++ api.php
@@ -9,9 +9,12 @@
 
  Version 2.0
 */
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+header("Access-Control-Allow-Origin", "*");
+header("Access-Control-Allow-Credentials", "true");
+header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
 
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
