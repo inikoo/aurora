84fb6fc9ebc06368ce4b09accda35e737d4f72b7
Raul Perusquia <raul@inikoo.com>
2018-04-19 16:09:08 +0100

    seeds

--- api.php
+++ api.php
@@ -11,6 +11,8 @@
 */
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
 include_once 'utils/general_functions.php';
