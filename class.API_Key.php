becb8682266a90183c6aa4156b5a58da5db2a2c9
Raul Perusquia <raul@inikoo.com>
2018-04-19 15:13:49 +0100

    t4est

--- class.API_Key.php
+++ class.API_Key.php
@@ -83,18 +83,21 @@ class API_Key extends DB_Table {
 
 
         include_once 'utils/password_functions.php';
-        $this->secret_key = 'P'.generatePassword(39, 10);
+        $this->secret_key = 'P'.generatePassword(39, 1);
 
         $data['API Key Code'] = hash('crc32', generatePassword(32, 10), false);
 
 
         $data['API Key Hash'] = password_hash($this->secret_key, PASSWORD_DEFAULT);
 
-        //print $this->secret_key;
+        print $this->secret_key;
 
-        //print '>>>>>';
-        //print base64_encode($this->secret_key);
-        //print '<<<<<';
+        print '>>>>>';
+        print base64_encode($this->secret_key);
+        print '<<<<<';
+
+        print_r($data);
+
         $this->secret_key     = base64_encode($this->secret_key);
         $this->data           = $data;
 
