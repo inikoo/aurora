7dc98b6d52dac605f7dba2f0c0f0999d0f2a97e0
Raul Perusquia <raul@inikoo.com>
2018-04-19 16:13:01 +0100

    test

--- api.php
+++ api.php
@@ -9,11 +9,15 @@
 
  Version 2.0
 */
+//header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+//header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 header("Access-Control-Allow-Origin", "*");
 header("Access-Control-Allow-Credentials", "true");
 header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 
 error_reporting(E_ALL);
