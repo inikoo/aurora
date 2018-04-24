c3cbfb27013633b273cf666e3d3dda92ef0eb5be
Raul Perusquia <raul@inikoo.com>
2018-04-24 19:33:32 +0100

    test redis

---
+++ test_redis.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 24 April 2018 at 13:50:55 BST, Sheffield UK
+ Copyright (c) 2016, Inikoo
+
+ Version 3
+
+*/
+
+
+ $redis = new Redis(); 
+ if(  $redis->connect('127.0.0.1', 6379)){
+
+     if($redis->exists('tutorial-name')){
+
+         print $redis->get('tutorial-name').'xddddd';
+     }
+ }
+
+
+
+
+?>
