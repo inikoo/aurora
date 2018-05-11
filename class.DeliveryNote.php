47a6a6ec639914d7683a6320529c18c06b6b208f
Raul Perusquia <raul@inikoo.com>
2018-05-11 11:38:09 +0200

    test

--- class.DeliveryNote.php
+++ class.DeliveryNote.php
@@ -66,6 +66,7 @@ class DeliveryNote extends DB_Table {
             $sql = sprintf(
                 "SELECT * FROM `Delivery Note Dimension` WHERE  `Delivery Note Public ID`=%s", prepare_mysql($tag)
             );
+            print $sql;
         } else {
             return;
         }
