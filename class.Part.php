c3ea7de0b5acc2912ffa2dd858ab9c5943b90dd3
Raul Perusquia <raul@inikoo.com>
2018-06-05 15:57:47 +0800

    test

--- class.Part.php
+++ class.Part.php
@@ -2919,9 +2919,12 @@ class Part extends Asset {
 
                 $sql=sprintf('select `Date`,(`Inventory Transaction Amount`/`Inventory Transaction Quantity`) as value_per_sko from  `Inventory Transaction Fact` ITF  where  `Inventory Transaction Amount`>0 and `Inventory Transaction Quantity`>0 and  ( `Inventory Transaction Section`=\'In\' or ( `Inventory Transaction Type`=\'Adjust\' and `Inventory Transaction Quantity`>0 and `Location Key`>1 )  )  and ITF.`Part SKU`=%d  order by `Date`, FIELD(`Inventory Transaction Type`, \'In\',\'Adjust\') desc limit 1 ',$this->id);
 
-
+print $sql;
                 if ($result=$this->db->query($sql)) {
                 		foreach ($result as $row) {
+
+                		    print_r($row);
+
                             $this->update_field_switcher('Part Cost in Warehouse',$row['value_per_sko'],'no_history');
                 		}
                 }else {
