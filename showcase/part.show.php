18cf35cc2d4da16873e427f4b4a21045ebda925c
Raul Perusquia <raul@inikoo.com>
2018-08-06 16:09:13 +0800

    bug

--- showcase/part.show.php
+++ showcase/part.show.php
@@ -27,7 +27,7 @@ function get_part_showcase($data, $smarty, $user, $db) {
    // $part->update_next_deliveries_data();
    // $part->update_next_deliveries_data();
 
-    $update_stock_run();
+    $part->update_stock_run();
 
     $part->update_stock();
   //  $part->validate_barcode();
