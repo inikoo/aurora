e171ea0c29366c4923081681a5ef1ae7103cca96
Raul Perusquia <raul@inikoo.com>
2018-08-14 17:13:20 +0800

    fix screp

--- cron/set_sales_representatives.oneoff.php
+++ cron/set_sales_representatives.oneoff.php
@@ -17,6 +17,8 @@ $sql = sprintf('update `Invoice Dimension` set `Invoice Sales Representative Key
 print "$sql\n";
 $db->exec($sql);
 
+
+
 $sql = sprintf('select `Customer Sales Representative Key`,`Customer Key` from `Customer Dimension` where `Customer Sales Representative Key`>0 ');
 if ($result = $db->query($sql)) {
     foreach ($result as $row) {
@@ -24,6 +26,11 @@ if ($result = $db->query($sql)) {
         $sql = sprintf('update `Invoice Dimension` set `Invoice Sales Representative Key`=%d where `Invoice Customer Key`=%d and `Invoice Date`>"2017-01-01 00:00:00" ', $row['Customer Sales Representative Key'], $row['Customer Key']);
         print "$sql\n";
         $db->exec($sql);
+
+        $sql = sprintf('update `Order Dimension` set `Order Sales Representative Key`=%d where `Order Customer Key`=%d and `Order Date`>"2017-01-01 00:00:00" ', $row['Customer Sales Representative Key'], $row['Customer Key']);
+        print "$sql\n";
+        $db->exec($sql);
+
     }
 } else {
     print_r($error_info = $db->errorInfo());
