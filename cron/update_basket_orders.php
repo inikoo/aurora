8745296bbbfe454183a9ca1e19a872127e76a7a6
Raul Perusquia <raul@inikoo.com>
2018-11-07 14:03:03 +0800

    fix scripy

--- cron/update_basket_orders.php
+++ cron/update_basket_orders.php
@@ -15,38 +15,34 @@ require_once 'utils/natural_language.php';
 require_once 'utils/order_functions.php';
 
 
+$sql = sprintf("SELECT `Order Key` FROM `Order Dimension`  left join `Store Dimension` on (`Store Key`=`Order Store Key`) where `Order State`='InBasket'and `Store Version`=2 ");
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+
+        $dn_key = false;
+
+        $order = get_object('order', $row['Order Key']);
+
+        $order->update_totals();
+
+        $order->update_discounts_items();
 
 
-
-  $sql = sprintf("SELECT `Order Key` FROM `Order Dimension`  left join `Store Dimension` on (`Store Key`=`Order Store Key`) where `Order State`='InBasket'and `Store Version`=2 ");
-    if ($result = $db->query($sql)) {
-        foreach ($result as $row) {
-        
-        $dn_key=false;
-        
-            $order = get_object('order', $row['Order Key']);
-
-            $order->update_totals();
-
-            $order->update_discounts_items();
+        $order->update_shipping($dn_key, false);
+        $order->update_charges($dn_key, false);
+        $order->update_discounts_no_items();
+        $order->update_deal_bridge();
 
 
-            $order->update_shipping($dn_key, false);
-            $order->update_charges($dn_key, false);
-
-            $order->update_deal_bridge();
+        $order->update_totals();
 
 
-            $order->update_totals();
-
-
-
-        }
-
-    } else {
-        print_r($error_info = $db->errorInfo());
-        exit;
     }
+
+} else {
+    print_r($error_info = $db->errorInfo());
+    exit;
+}
 
 
 ?>
