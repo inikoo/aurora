26f95c4b4be3a1068ffd8c99d4ac8f8739c7bbb9
Raul Perusquia <raul@inikoo.com>
2018-11-10 03:13:31 +0800

    testing sentry

--- common.php
+++ common.php
@@ -5,6 +5,10 @@ define("_DEVEL", isset($_SERVER['devel']));
 
 
 require_once 'vendor/autoload.php';
+$sentry_client = new Raven_Client('https://7c38fecb8a274f7e93cb0488ad22ca10@sentry.io/1319834');
+$sentry_client->install();
+
+
 
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
