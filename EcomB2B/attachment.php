5a4b4fce1138cb693fba6e0319e64b989bcab508
Raul Perusquia <raul@inikoo.com>
2018-11-10 11:42:05 +0800

    ErrorExceptionattachment.php

--- EcomB2B/attachment.php
+++ EcomB2B/attachment.php
@@ -14,7 +14,7 @@ require_once 'utils/sentry.php';
 
 
 
-require_once 'keyring/key.php';
+require_once 'keyring/dns.php';
 
 $db = new PDO(
     "mysql:host=$dns_host;dbname=$dns_db;charset=utf8", $dns_user, $dns_pwd, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '+0:00';")
