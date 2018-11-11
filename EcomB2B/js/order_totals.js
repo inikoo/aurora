92bda9653f1f54c3bb18e55c9d4a2e8312a984b1
Raul Perusquia <raul@inikoo.com>
2018-11-12 01:14:14 +0800

    improve basket /checkout js and fix WEB-C

---
+++ EcomB2B/js/order_totals.js
@@ -0,0 +1,20 @@
+/*Author: Raul Perusquia <raul@inikoo.com>
+ Created: 11 November 2018 at 21:46:16 GMT+8, Kuala Lumpur, Malaysia
+ Copyright (c) 2018, Inikoo
+ Version 3.0*/
+
+
+ function show_charges_info(){
+
+        var request = '/ar_web_basket.php?tipo=get_charges_info'
+        $.getJSON(request, function (data) {
+
+            if (data.state == 200) {
+                swal({
+                    html:true,
+                    title: '',
+                    text:data.text,
+                })
+            }
+        })
+    }
