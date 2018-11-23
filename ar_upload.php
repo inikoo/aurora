898506884684eeeb0441b3720236f323629d42fc
Raul Perusquia <raul@inikoo.com>
2018-11-24 03:00:35 +0800

    fix AU-6N

--- ar_upload.php
+++ ar_upload.php
@@ -59,10 +59,13 @@ switch ($tipo) {
 
         $data = prepare_values(
             $_REQUEST, array(
-                         'parent'     => array('type' => 'string'),
-                         'parent_key' => array('type' => 'numeric'),
-                         'objects'    => array('type' => 'string'),
-                         'allow_duplicate_part_reference'    => array('type' => 'string','optional'=>true),
+                         'parent'                         => array('type' => 'string'),
+                         'parent_key'                     => array('type' => 'numeric'),
+                         'objects'                        => array('type' => 'string'),
+                         'allow_duplicate_part_reference' => array(
+                             'type'     => 'string',
+                             'optional' => true
+                         ),
 
                      )
         );
@@ -307,6 +310,8 @@ function upload_images($account, $db, $user, $editor, $data, $smarty) {
 
     include_once 'class.Image.php';
 
+    $images = array();
+
 
     if (isset($data['parent_object_scope'])) {
         $parent_object_scope = $data['parent_object_scope'];
@@ -516,14 +521,14 @@ function upload_images($account, $db, $user, $editor, $data, $smarty) {
 
         if (!empty($_options['fit_to_canvas'])) {
 
-            list($canvas_w,$canvas_h)=preg_split('/x/',$_options['fit_to_canvas']);
+            list($canvas_w, $canvas_h) = preg_split('/x/', $_options['fit_to_canvas']);
 
-            $size  = getimagesize($tmp_name);
+            $size = getimagesize($tmp_name);
 
             $w = $size[0];
-            $h     = $size[1];
+            $h = $size[1];
 
-                      $format     = guess_file_format($tmp_name);
+            $format = guess_file_format($tmp_name);
 
 
             //  print "$format $new_width $new_height";
@@ -568,40 +573,34 @@ function upload_images($account, $db, $user, $editor, $data, $smarty) {
             }
 
 
-
-
             $r = $w / $h;
 
-            $r_canvas=$canvas_w/$canvas_h;
+            $r_canvas = $canvas_w / $canvas_h;
 
-            if($r < $r_canvas) {
-                $fit_h = $canvas_h;
-                $fit_w = $w * ($fit_h / $h);
+            if ($r < $r_canvas) {
+                $fit_h    = $canvas_h;
+                $fit_w    = $w * ($fit_h / $h);
                 $canvas_y = 0;
                 $canvas_x = ($canvas_w - $fit_w) / 2;
-            }elseif($r > $r_canvas) {
+            } elseif ($r > $r_canvas) {
                 $fit_w = $canvas_w;
                 $fit_h = $h * ($fit_w / $w);
 
                 $canvas_x = 0;
                 $canvas_y = ($canvas_h - $fit_h) / 2;
-            }else{
-                $fit_h = $canvas_h;
-                $fit_w = $canvas_w;
+            } else {
+                $fit_h    = $canvas_h;
+                $fit_w    = $canvas_w;
                 $canvas_x = 0;
                 $canvas_y = 0;
 
             }
 
             $canvas = imagecreatetruecolor($canvas_w, $canvas_h);
-            $white = imagecolorallocate($canvas, 255, 255, 255);
+            $white  = imagecolorallocate($canvas, 255, 255, 255);
             imagefill($canvas, 0, 0, $white);
 
             imagecopyresampled($canvas, $im, $canvas_x, $canvas_y, 0, 0, $fit_w, $fit_h, $w, $h);
-
-
-
-
 
 
             ob_start();
@@ -724,18 +723,22 @@ function upload_images($account, $db, $user, $editor, $data, $smarty) {
         $image = $parent->add_image($image_data, $options);
 
 
-
-
-
         if ($parent->error) {
 
 
             $errors++;
-
             $error_msg[] = $parent->msg;
 
         } else {
-            $uploads++;
+
+
+            if (is_object($image) and $image->id) {
+                $uploads++;
+                $images[$image->id] = $image;
+            } else {
+                $errors++;
+                $error_msg[] = $image->msg;
+            }
 
 
         }
@@ -744,378 +747,81 @@ function upload_images($account, $db, $user, $editor, $data, $smarty) {
     }
 
 
+    if (count($images) > 0) {
 
+        $image = array_pop($images);
 
-
-    if (isset($data['response_type']) and $data['response_type'] == 'froala') {
-        echo json_encode(array('link' => sprintf('/image_root.php?id=%d', $image->id)));
-    }  else {
-        if ($uploads > 0) {
-            $msg = '<i class="fa fa-check"></i> '._('Success');
+        if (isset($data['response_type']) and $data['response_type'] == 'froala') {
+            echo json_encode(array('link' => sprintf('/image_root.php?id=%d', $image->id)));
         } else {
-            $msg = '<i class="fa fa-exclamation-circle"></i>';
+
+            if ($uploads > 0) {
+                $msg = '<i class="fa fa-check"></i> '._('Success');
+            } else {
+                $msg = '<i class="fa fa-exclamation-circle"></i>';
+            }
+
+
+            //   print "xx $errors  $uploads";
+
+
+            $response = array(
+                'state'          => 200,
+                'tipo'           => 'upload_images',
+                'msg'            => $msg,
+                'errors'         => $errors,
+                'error_msg'      => $error_msg,
+                'uploads'        => $uploads,
+                'number_images'  => $parent->get_number_images(),
+                'main_image_key' => $parent->get_main_image_key(),
+                'image_src'      => sprintf('/image_root.php?id=%d', $image->id),
+                'thumbnail'      => sprintf('<img src="/image_root.php?id=%d&size=thumbnail">', $image->id),
+                'small_image'    => sprintf('<img src="/image_root.php?id=%d&size=small">', $image->id),
+                'img_key'        => $image->id,
+                'height'         => $image->get('Image Height'),
+                'width'          => $image->get('Image Width'),
+                'ratio'          => $image->get('Image Width') / $image->get('Image Height'),
+            );
+
+
+            if (isset($data['response_type']) and $data['response_type'] == 'upload_item_image') {
+                $response['images'] = $parent->get_images_slidesshow();
+
+            }
+
+            // todo remove parent->get_object_name()=='Page' when new class is used
+            if ($parent->get_object_name() == 'Page' or $parent->get_object_name() == 'Webpage') {
+                $response['publish'] = $parent->get('Publish');
+            }
+
+
+            //  print_r($response);
+
+            echo json_encode($response);
+
         }
-
-
-        //   print "xx $errors  $uploads";
-
-
-        $response = array(
-            'state'          => 200,
-            'tipo'           => 'upload_images',
-            'msg'            => $msg,
-            'errors'         => $errors,
-            'error_msg'      => $error_msg,
-            'uploads'        => $uploads,
-            'number_images'  => $parent->get_number_images(),
-            'main_image_key' => $parent->get_main_image_key(),
-            'image_src'      => sprintf('/image_root.php?id=%d', $image->id),
-            'thumbnail'      => sprintf('<img src="/image_root.php?id=%d&size=thumbnail">', $image->id),
-            'small_image'    => sprintf('<img src="/image_root.php?id=%d&size=small">', $image->id),
-            'img_key'        => $image->id,
-            'height'         => $image->get('Image Height'),
-            'width'          => $image->get('Image Width'),
-            'ratio'          => $image->get('Image Width') / $image->get('Image Height'),
-        );
-
-
-        if (isset($data['response_type']) and $data['response_type'] == 'upload_item_image') {
-            $response['images'] = $parent->get_images_slidesshow();
-
-        }
-
-        // todo remove parent->get_object_name()=='Page' when new class is used
-        if ($parent->get_object_name() == 'Page' or $parent->get_object_name() == 'Webpage') {
-            $response['publish'] = $parent->get('Publish');
-        }
-
-
-        //  print_r($response);
-
-        echo json_encode($response);
-
-    }
-
-
-}
-
-
-function upload_objects_to_deete($account, $db, $user, $editor, $data, $smarty) {
-    require_once 'class.Upload.php';
-
-    require_once 'external_libs/PHPExcel/Classes/PHPExcel.php';
-    require_once 'external_libs/PHPExcel/Classes/PHPExcel/IOFactory.php';
-
-    $valid_extensions = array(
-        'xls',
-        'xlt',
-        'xlm',
-        'xlsx',
-        'xlsm',
-        'xltx',
-        'xltm',
-        'xlsb',
-        'ods',
-        'slk',
-        'gnumeric',
-        'tsv',
-        'tab',
-        'csv'
-    );
-
-    $parent         = get_object($data['parent'], $data['parent_key']);
-    $parent->editor = $editor;
-
-
-    if (!$parent->id) {
-        $msg      = 'parent key not found';
+    } else {
+        $msg      = _("There is a problem uploading your image");
         $response = array(
             'state' => 400,
-            'msg'   => $msg
-        );
-        echo json_encode($response);
-        exit;
-    }
-
-
-    if (empty($_FILES) && empty($_POST) && isset($_SERVER['REQUEST_METHOD'])
-        && strtolower($_SERVER['REQUEST_METHOD']) == 'post') { //catch file overload error...
-        $postMax  = ini_get('post_max_size'); //grab the size limits...
-        $msg      = sprintf(
-            _(
-                "File can not be attached, please note files larger than %s will result in this error!, let's us know, an we will increase the size limits"
-            ), $postMax
-        );
-        $response = array(
-            'state' => 400,
+            'title' => _('Error'),
             'msg'   => $msg,
             'key'   => 'attach'
         );
         echo json_encode($response);
         exit;
-
     }
-
-    if (empty($_FILES)) {
-        $msg      = '_FILES array empty';
-        $response = array(
-            'state' => 400,
-            'msg'   => _("File can't be uploaded").", ".$msg
-        );
-        echo json_encode($response);
-        exit;
-
-    }
-
-    $upload_files_data = array();
-    $files_with_errors = array();
-
-    foreach ($_FILES['files']['name'] as $file_key => $name) {
-
-
-        $error             = $_FILES['files']['error'][$file_key];
-        $size              = $_FILES['files']['size'][$file_key];
-        $original_tmp_name = $_FILES['files']['tmp_name'][$file_key];
-        $type              = $_FILES['files']['type'][$file_key];
-        $extension         = strtolower(pathinfo($name, PATHINFO_EXTENSION));
-
-
-        if ($error) {
-            $msg = parse_upload_file_error_msg($error);
-
-            $files_with_errors[] = array(
-                'msg'      => $msg,
-                'filename' => $name
-            );
-            continue;
-
-        }
-
-        if ($size == 0) {
-            $msg                 = _(
-                    "This file seems that is empty, have a look and try again"
-                ).'.';
-            $files_with_errors[] = array(
-                'msg'      => $msg,
-                'filename' => $name
-            );
-            continue;
-
-
-        }
-
-        if (!in_array($extension, $valid_extensions)) {
-            $msg = _('Invalid file type').' <b>'.$extension.'</b> <i>('.$type.')</i>';
-
-            $files_with_errors[] = array(
-                'msg'      => $msg,
-                'filename' => $name
-            );
-            continue;
-
-        }
-
-        $tmp_name = 'up_'.microtime(true).'_'.$user->id.'_'.md5_file(
-                $original_tmp_name
-            ).'.'.pathinfo($name, PATHINFO_EXTENSION);
-        $tmp_path = 'server_files/uploads/';
-
-        // rename($original_tmp_name, $tmp_path.$tmp_name);
-
-
-        $upload_files_data[] = array(
-            'editor'               => $editor,
-            'Upload File Checksum' => md5_file($original_tmp_name),
-            'Upload File Name'     => $name,
-            'Upload File Size'     => filesize($original_tmp_name),
-            'Upload File Filename' => $original_tmp_name,
-            'Upload File Type'     => $type,
-            'Upload File Metadata' => json_encode(
-                array(
-                    'extension' => $extension,
-                    'type'      => $type,
-                    'tmp_name'  => $tmp_name
-                )
-            )
-
-        );
-
-
-    }
-
-    $number_files_uploaded = count($upload_files_data);
-    $fork_key              = false;
-    $upload_key            = false;
-
-    if ($number_files_uploaded) {
-        $upload_data = array(
-            'editor'            => $editor,
-            'Upload Type'       => 'EditObjects',
-            'Upload Object'     => $data['object'],
-            'Upload Parent'     => $data['parent'],
-            'Upload Parent Key' => $data['parent_key'],
-            'Upload User Key'   => $user->id,
-            'Upload Metadata'   => json_encode(
-                array(
-                    'uploaded_files'    => $number_files_uploaded,
-                    'files_with_errors' => count($files_with_errors),
-                    'files_data'        => $upload_files_data
-                )
-            )
-
-        );
-
-        $upload = new Upload('create', $upload_data);
-
-        $file_index     = 0;
-        $number_records = 0;
-        foreach ($upload_files_data as $upload_file_data) {
-
-            if ($file_index == 0) {
-
-                $upload_file_key = create_upload_file(
-                    $db, $upload->id, $upload_file_data
-                );
-
-                //print_r($upload_file_data);
-
-                $inputFileType = PHPExcel_IOFactory::identify(
-                    $upload_file_data['Upload File Filename']
-                );
-                $objReader     = PHPExcel_IOFactory::createReader(
-                    $inputFileType
-                );
-                $objReader->setReadDataOnly(true);
-
-                $objPHPExcel = @$objReader->load(
-                    $upload_file_data['Upload File Filename']
-                );
-
-
-                $objWorksheet = $objPHPExcel->getActiveSheet();
-
-                $highestRow         = $objWorksheet->getHighestRow();
-                $highestColumn      = $objWorksheet->getHighestColumn();
-                $highestColumnIndex = PHPExcel_Cell::columnIndexFromString(
-                    $highestColumn
-                );
-
-
-                for ($row = 2; $row <= $highestRow; ++$row) {
-                    $row_data = array();
-                    $empty    = true;
-                    for ($col = 0; $col <= $highestColumnIndex; ++$col) {
-                        $value          = $objWorksheet->getCellByColumnAndRow(
-                            $col, $row
-                        )->getCalculatedValue();
-                        $row_data[$col] = $value;
-                        if ($value != '') {
-                            $empty = false;
-                        }
-                    }
-
-
-                    if (!$empty) {
-                        $sql = sprintf(
-                            'INSERT INTO `Upload Record Dimension` (`Upload Record Upload Key`,`Upload Record Data`,`Upload Record Upload File Key`,`Upload Record Row Index`) VALUE (%d,COMPRESS(%s),%d,%d)', $upload->id, prepare_mysql(json_encode($row_data)),
-                            $upload_file_key, ($row)
-                        );
-
-                        $db->exec($sql);
-                        $number_records++;
-                    }
-
-                }
-
-
-                $file_index++;
-            }
-            unlink($upload_file_data['Upload File Filename']);
-
-        }
-
-        $upload_data = array(
-            'upload_key' => $upload->id,
-            'user_key'   => $user->id
-        );
-
-        $upload->update(
-            array('Upload Records' => $number_records), 'no_history'
-        );
-
-        $upload_key = $upload->id;
-        list($fork_key, $msg) = new_fork(
-            'au_upload', $upload_data, $account->get('Account Code'), $db
-        );
-
-        $sql = sprintf(
-            'UPDATE `Fork Dimension` SET `Fork Operations Total Operations`=%d WHERE `Fork Key`=%d ', $number_records, $fork_key
-        );
-        $db->exec($sql);
-
-
-    }
-
-
-    if ($number_files_uploaded == 1) {
-        $msg   = '<i class="fa fa-spinner fa-spin"></i> '._('Processing');
-        $state = 200;
-    } elseif ($number_files_uploaded > 1) {
-
-        $msg   = '<i class="fa fa-spinner fa-spin"></i> '.sprintf(
-                _('Processing %s files'), $number_files_uploaded
-            );
-        $state = 200;
-    } else {
-        if (count($files_with_errors) == 1) {
-
-            foreach ($files_with_errors as $file_with_errors) {
-                $error_msg = $file_with_errors['msg'];
-            }
-
-            $msg   = '<i class="fa fa-exclamation-circle"></i> '.$error_msg;
-            $state = 400;
-        } else {
-            if (count($files_with_errors) > 0) {
-                $error_msg = '';
-                foreach ($files_with_errors as $file_with_errors) {
-                    $error_msg .= $file_with_errors['filename'].': '.$file_with_errors['msg'].', ';
-                }
-                $error_msg = preg_replace('/,$/', '', $error_msg);
-
-                $msg   = '<i class="fa fa-exclamation-circle"></i> '.$error_msg;
-                $state = 400;
-            } else {
-                $msg   = '<i class="fa fa-exclamation-circle"></i> '._(
-                        'No files uploaded'
-                    );
-                $state = 400;
-            }
-        }
-    }
-
-    $response = array(
-        'state'             => $state,
-        'msg'               => $msg,
-        'tipo'              => 'upload_objects',
-        'files_with_errors' => $files_with_errors,
-        'fork_key'          => $fork_key,
-        'upload_key'        => $upload_key
-
-
-    );
-
-    echo json_encode($response);
 
 
 }
+
 
 
 function parse_upload_file_error_msg($file_data_error) {
 
     if ($file_data_error === UPLOAD_ERR_INI_SIZE) {
         $msg = sprintf(
-            _('file exceeds the upload max filesize (%s)'), ini_get('upload_max_filesize')
+            _('file exceeds the upload max file size (%s)'), ini_get('upload_max_file size')
         );
 
     } elseif ($file_data_error === UPLOAD_ERR_FORM_SIZE) {
@@ -1474,8 +1180,8 @@ function edit_objects($account, $db, $user, $editor, $data, $smarty) {
         );
 
 
-        if(isset($data['allow_duplicate_part_reference'])){
-            $upload_data['allow_duplicate_part_reference']=$data['allow_duplicate_part_reference'];
+        if (isset($data['allow_duplicate_part_reference'])) {
+            $upload_data['allow_duplicate_part_reference'] = $data['allow_duplicate_part_reference'];
         }
 
 
