d9600a0dee3cef5955f638fb19ca46e04ecedd97
Raul Perusquia <raul@inikoo.com>
2018-12-19 17:34:32 +0800

    fix AU-BT

--- prepare_table/invoice.payments.ptble.php
+++ prepare_table/invoice.payments.ptble.php
@@ -79,7 +79,7 @@ $table = '`Payment Dimension` P left join `Payment Account Dimension` PA on (PA.
 
 $sql_totals = "select count(P.`Payment Key`) as num from $table  $where  ";
 $fields
-            = "`Payment Order Key`,`Store Key`,`Store Name`,`Store Code`,PA.`Payment Account Code`,PA.`Payment Account Name`,`Payment Account Block`,`Payment Transaction Amount Refunded`,`Payment Transaction Amount Credited`,`Payment Submit Type`,`Payment Key`,`Payment Transaction ID`,`Payment Currency Code`,`Payment Transaction Amount`,P.`Payment Type`,`Payment Last Updated Date`,`Payment Transaction Status`,`Payment Transaction Status Info`";
+            = "`Payment Method`,`Payment Order Key`,`Store Key`,`Store Name`,`Store Code`,PA.`Payment Account Code`,PA.`Payment Account Name`,`Payment Account Block`,`Payment Transaction Amount Refunded`,`Payment Transaction Amount Credited`,`Payment Submit Type`,`Payment Key`,`Payment Transaction ID`,`Payment Currency Code`,`Payment Transaction Amount`,P.`Payment Type`,`Payment Last Updated Date`,`Payment Transaction Status`,`Payment Transaction Status Info`";
 
 
 ?>
