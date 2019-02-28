40714ef2d5cca72d6f413e4dc873de6f2c802865
Raul Perusquia <raul@inikoo.com>
2019-02-28 19:33:40 +0800

    fix webpages sys

--- conf/webpage_blocks.php
+++ conf/webpage_blocks.php
@@ -138,38 +138,8 @@ function get_webpage_blocks($theme = '') {
             'show_title'        => true
         ),
 
-        'not_found'   => array(
-            'type'          => 'not_found',
-            'label'         => _('Not found'),
-            'icon'          => 'fa-times-octagon',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'labels'        => array(
-                '_strong_title' => '404',
-                '_title'        => _('Oops... Page Not Found!'),
-                '_text'         => _('Sorry the page could not be found here.'),
-                '_home_guide'   => _('Try using the button below to go to main page of the site'),
-                '_home_label'   => _('Go to homepage'),
 
-            )
-        ),
-        'offline'     => array(
-            'type'          => 'offline',
-            'label'         => _('Offline page'),
-            'icon'          => 'fa-ban',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'labels'        => array(
-                '_strong_title' => '410',
-                '_title'        => _('Oops... This page is gone!'),
-                '_text'         => _('Sorry this page has been removed.'),
-                '_home_guide'   => _('Try using the button below to go to main page of the site'),
-                '_home_label'   => _('Go to homepage'),
 
-            )
-        ),
         'in_process'     => array(
             'type'          => 'in_process',
             'label'         => _('Under construction'),
@@ -184,111 +154,10 @@ function get_webpage_blocks($theme = '') {
 
             )
         ),
-        'profile'     => array(
-            'type'          => 'profile',
-            'label'         => _('Profile'),
-            'icon'          => 'fa-user',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'labels'        => array(
-
-                '_customer_orders_title'  => _("Customer <i>Orders</i>"),
-                '_customer_profile_title' => _("Customer <i>Profile</i>"),
-
-
-                '_current_order_title' => _('Current order'),
-                '_last_order_title'    => _('Last order'),
-                '_orders_title'        => _('Orders'),
-
-
-                '_contact_details_title' => _('Contact details'),
-                '_email_placeholder'     => _('Email address'),
-                '_email_label'           => _('Email address (this is also your login name)'),
-                '_email_tooltip'         => _('Needed to login to your account'),
-
-
-                '_login_details_title' => _('Login details'),
-
-                '_invoice_address_title'      => _('Invoice address'),
-                '_invoice_address_save_label' => _('Save'),
-
-                '_delivery_addresses_title'                 => _('Delivery address'),
-                '_delivery_addresses_same_as_invoice_label' => _('Deliver to invoice address'),
-
-                '_delivery_addresses_save_label' => _('Save'),
-
-
-                '_password_placeholder' => _('New password'),
-                '_password_label'       => _('Password'),
-
-                '_password_tooltip' => _("Write new password"),
-
-                '_password_confirm_placeholder' => _('Confirm new password'),
-                '_password_confirm_label'       => _('Confirm new password'),
-
-                '_password_conform_tooltip' => _("Don't forget your password"),
-
-                '_mobile_placeholder' => _('Mobile'),
-                '_mobile_label'       => _('Mobile'),
-
-                '_mobile_tooltip' => _('Needed to enter your mobile/telephone'),
-
-                '_contact_name_placeholder' => _('Contact name'),
-                '_contact_name_label'       => _('Contact name'),
-
-                '_contact_name_tooltip' => _('Needed to enter your name'),
-
-                '_company_placeholder' => _('Company'),
-                '_company_label'       => _('Company'),
-
-                '_company_tooltip' => _('Enter your account company name (optional)'),
-
-                '_tax_number_placeholder' => _('VAT number'),
-                '_tax_number_label'       => _('VAT number'),
-
-                '_tax_number_tooltip' => _('Enter your VAT number (optional)'),
-
-                '_registration_number_placeholder' => _('Registration number'),
-                '_registration_number_label'       => _('Registration number'),
-
-                '_registration_number_tooltip' => _('Enter your company registration number (optional)'),
-
-
-                '_save_contact_details_label'          => _('Save'),
-                '_save_login_details_label'            => _('Save'),
-                '_save_invoice_address_details_label'  => _('Save'),
-                '_save_delivery_address_details_label' => _('Save'),
-
-
-                '_username_info' => _('Your username is your email address')
-
-
-            )
-        ),
-        'unsubscribe' => array(
-            'type'          => 'unsubscribe',
-            'label'         => _('Unsubscribe'),
-            'icon'          => 'fa-comment-slash',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'labels'        => array(
-                '_unsubscribe_title'      => _('Email subscriptions'),
-                '_unsubscribe_text'       => _('Select which kind of emails you want to receive from us'),
-                '_save_unsubscribe_label' => _('Save'),
-                '_newsletter'             => _('Newsletter'),
-                '_marketing_emails'       => _('Marketing emails and special offers'),
-                '_unsubscribe_error_msg'=>_('Sorry, we could not access your record, please login to you account and unsubscribe in your profile section or contact our customer services'),
-                '_unsubscribe_error_login_link'=>_('Login'),
-
-                  '_unsubscribe_error_logged_in_msg'=>_('Oops..., that link is not working properly, please click link below to unsubscribe'),
-                '_unsubscribe_error_profile_link'=>_('Profile')
 
 
 
-            )
-        ),
+
         'favourites'  => array(
             'type'          => 'favourites',
             'label'         => _('Favourites'),
@@ -302,76 +171,10 @@ function get_webpage_blocks($theme = '') {
             )
         ),
 
-        'checkout' => array(
-            'type'          => 'checkout',
-            'label'         => _('Checkout'),
-            'icon'          => 'fa-credit-card',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'labels'        => array(
 
 
-                '_credit_card_label' => _('Credit card'),
-                '_bank_label'        => _('Bank transfer'),
-
-                '_credit_card_number'                      => _('Card number'),
-                '_credit_card_ccv'                         => _('CVV'),
-                '_credit_card_expiration_date'             => _('Expiration date'),
-                '_credit_card_expiration_date_month_label' => _('Month'),
-                '_credit_card_expiration_date_year_label'  => _('Year'),
-                '_credit_card_save'                        => _('Save card'),
 
 
-                '_form_title_credit_card'          => _('Checkout form'),
-                '_form_title_paypal'               => _('Checkout form'),
-                '_form_title_cond'                 => _('Checkout form'),
-                '_form_title_sofort'               => _('Checkout form'),
-                '_form_title_bank'                 => _('Checkout form'),
-                '_form_title_other'                => _('Checkout form'),
-                '_form_title_online_bank_transfer' => _('Checkout form'),
-                '_form_title_cash_on_delivery'     => _('Checkout form'),
-
-
-                '_bank_header' => _('Please go to your bank and make a payment of <b>[Order Amount]</b>  to our bank account, details below'),
-                '_bank_footer' => _('Remember to state the order number in the payment reference').' [Order Number] . '._(
-                        'Please note, we cannot process your order until payment arrives in our account'
-                    ),
-
-
-                '_back_to_basket' => _('Go back to basket'),
-
-                '_place_order'                           => _('Place order'),
-                '_place_order_from_bank'                 => _('Place order'),
-                '_place_order_from_credit_card'          => _('Place order'),
-                '_place_order_from_paypal'               => _('Place order'),
-                '_place_order_from_cash_on_delivery'     => _('Place order'),
-                '_place_order_from_online_bank_transfer' => _('Place order'),
-
-
-            )
-        ),
-
-        'thanks' => array(
-            'type'          => 'thanks',
-            'label'         => _('Thanks'),
-            'icon'          => 'fa-thumbs-up',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'text'          => '<h1 >'._('Thank you for your order').'</h1><p>'._('Thank you!  We are delighted to receive your order').'</p><p>[Pay Info]</p><p>'._('Your order details are listed below, if you have any questions please email our team')
-                .'</p><p>[Order]</p>'
-        ),
-        'search' => array(
-            'type'          => 'search',
-            'label'         => _('Search'),
-            'icon'          => 'fa-search',
-            'show'          => 1,
-            'top_margin'    => 40,
-            'bottom_margin' => 60,
-            'labels'        => array()
-
-        ),
         'reviews' => array(
             'type'          => 'reviews',
             'label'         => _('Reviews'),
@@ -382,6 +185,13 @@ function get_webpage_blocks($theme = '') {
             'labels'        => array()
 
         ),
+
+
+
+
+
+
+
 
     );
 
