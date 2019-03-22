4110b3715d61697a7c2f3262eda8047c518e959c
Raul Perusquia <raul@inikoo.com>
2019-03-22 22:40:36 +0800

    shipping zones

--- app_files/db_changelog/change_190322.sql
+++ app_files/db_changelog/change_190322.sql
@@ -8,3 +8,5 @@ CREATE TABLE `Credit Transaction History Bridge` (
 
 /* 12:19:51 localhost sk */ ALTER TABLE `Customer Dimension` MODIFY COLUMN `Customer Account Balance` DECIMAL(14,2) NOT NULL DEFAULT '0.00' AFTER `Customer Email State`;
 /* 12:29:12 localhost sk */ ALTER TABLE `Customer Dimension` ADD `Customer Number Credit Transactions` SMALLINT  UNSIGNED  NOT NULL  DEFAULT '0'  AFTER `Customer Account Balance`;
+ALTER TABLE `User Dimension` ADD `User Settings` JSON  ;
+UPDATE `User Dimension` SET `User Settings`='{}' ;
