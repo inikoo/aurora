530146a6a866c9bfed99bd11bdea5063e9414fcc
Raul Perusquia <raul@inikoo.com>
2019-03-26 12:43:43 +0800

    Part HTSUS Code

---
+++ app_files/db_changelog/change_190326.sql
@@ -0,0 +1,3 @@
+/* 12:08:55 localhost sk */ ALTER TABLE `Part Dimension` ADD `Part HTSUS Code` VARCHAR(10)  NULL  DEFAULT NULL  AFTER `Part Duty Rate`;
+/* 12:20:56 localhost sk */ ALTER TABLE `Product Dimension` ADD `Product HTSUS Code` VARCHAR(10)  NULL  DEFAULT NULL  AFTER `Product Duty Rate`;
+
