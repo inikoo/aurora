a1eb1d46be7316f3440b2ddb793a6b29c0560453
Raul Perusquia <raul@inikoo.com>
2019-04-03 13:17:24 +0800

    redirect legacy page.php Ecom

---
+++ EcomB2B/page.php
@@ -0,0 +1,14 @@
+<?php
+/*
+
+  About:
+  Author: Raul Perusquia <raul@inikoo.com>
+  created: 3 April 2019 at 13:12:16 GMT+8, Kuala Lumpur, Malaysia
+
+  Copyright (c) 2019, Inikoo
+
+  Version 2.0
+*/
+
+
+header("Location: https://".$_SERVER['HTTP_HOST']);
