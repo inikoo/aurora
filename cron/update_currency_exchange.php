ac87e7ee14113160accb76c9c72c3a1f28701d70
Raul Perusquia <raul@inikoo.com>
2019-04-03 16:06:59 +0800

    test currency echange at fork

---
+++ cron/update_currency_exchange.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 3 April 2019 at 15:58:51 GMT+8, Kuala Lumpur, Malaysia
+ Copyright (c) 2019, Inikoo
+
+ Version 3
+
+*/
+
+require_once 'common.php';
+include_once 'utils/new_fork.php';
+
+new_housekeeping_fork(
+    'au_housekeeping', array(
+    'type'        => 'update_currency_exchange',
+    'currency_from' => 'EUR',
+    'currency_to'   => 'GBP',
+), $account->get('Account Code'), $db
+);
+
