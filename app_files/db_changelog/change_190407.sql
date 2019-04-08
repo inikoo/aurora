0e8bbeacd4183bbd7d4937afb0db5ca0674c427c
Raul Perusquia <raul@inikoo.com>
2019-04-08 11:43:02 +0800

    DB changes and a missing file prev commit

---
+++ app_files/db_changelog/change_190407.sql
@@ -0,0 +1,5 @@
+alter table `Website Data` add `Website Number Online Webpages` mediumint unsigned default 0 null;
+alter table `Website Data` add `Website Number Offline Webpages` mediumint unsigned default 0 null;
+alter table `Website Data` add `Website Number In Process  Webpages` mediumint unsigned default 0 null;
+alter table `Website Data` add `Website Number Registered Users` mediumint unsigned default 0 null;
+alter table `Website Data` add `Website Number Active Users` mediumint unsigned default 0 null;
