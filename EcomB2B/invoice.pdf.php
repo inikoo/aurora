3cd986545ec143d1fc7da6b4d6f7158fa75ecf7f
Raul Perusquia <raul@inikoo.com>
2019-04-11 19:14:26 +0800

    test

--- EcomB2B/invoice.pdf.php
+++ EcomB2B/invoice.pdf.php
@@ -245,13 +245,13 @@ if ($result = $db->query($sql)) {
             $_country = new Country('code', $row['Product Origin Country Code']);
 
             if($_country->get('Country 2 Alpha Code')!='XX'){
-
-               try {
-                   $country = $countryRepository->get($_country->get('Country 2 Alpha Code'));
-                   $description .= ' <br>'._('Origin').': '.$country->getName().' ('.$country->getThreeLetterCode().')';
-               }catch (Exception $e) {
-                   $description .= ' <br>'._('Origin').': '.$_country->get('Country 2 Alpha Code');
-               }
+                $description .= ' <br>'._('Origin').': '.$_country->get('Country 2 Alpha Code');
+              // try {
+              //     $country = $countryRepository->get($_country->get('Country 2 Alpha Code'));
+              //     $description .= ' <br>'._('Origin').': '.$country->getName().' ('.$country->getThreeLetterCode().')';
+              // }catch (Exception $e) {
+              //     $description .= ' <br>'._('Origin').': '.$_country->get('Country 2 Alpha Code');
+              // }
 
 
             }
