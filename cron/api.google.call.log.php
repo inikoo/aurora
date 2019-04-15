bd7877492d4d8a308cc733e0f8c13e02aa2f0d0e
SasiAW <sasi@ancientwisdom.biz>
2019-04-16 00:35:48 +0800

    Adding Google API Last Call Log php file

---
+++ cron/api.google.call.log.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sasi
+ * Date: 14/04/2019
+ * Time: 11:43
+ */
+
+require_once 'common.php';
+
+$sql = "SELECT `Google API Call Details` FROM kbase.`Google API Call Dimension` ORDER BY `Google API Call Key` DESC LIMIT 1";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$row = $stmt->fetchAll();
+
+$Mask = "|%-30s |%-30s |%-30s |%-30s |\n";
+printf($Mask,"Date Range","Code","Rows","Token");
+
+$someArray = json_decode($row[0]['Google API Call Details'], true);
+
+while($element = current($someArray)) {
+    while($arrayRows = current($element)) {
+        printf($Mask,key($someArray),key($arrayRows),$arrayRows[key($arrayRows)]['rows'],$arrayRows[key($arrayRows)]['token']);
+        next($element);
+         }
+    next($someArray);
+}
