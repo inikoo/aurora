8e4fadd2b47b9fb36a7fe6ddf402e239f3b82e7a
Raul Perusquia <raul@inikoo.com>
2019-04-25 16:35:23 +0800

    cleaning Auth

--- app_files/db_changelog/post_migration.sql
+++ app_files/db_changelog/post_migration.sql
@@ -2,3 +2,10 @@ ALTER TABLE `Page Store Dimension` DROP `Page Header Key`,  DROP `Page Header Ty
 
 update `Order Dimension` set `Order Website Key`=`Order Site Key` where `Order Website Key` is null;
 
+ALTER TABLE `User Log Dimension` CHANGE `Site Key` `Site Key` SMALLINT(5) UNSIGNED NULL DEFAULT '0', CHANGE `Remember Cookie` `Remember Cookie` ENUM('Yes','No','Unknown') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'Unknown';
+
+// back up `User Log Dimension`
+// run legacy/cron/fix_user_log_dimension.php
+
+
+ALTER TABLE `User Log Dimension` DROP `Site Key`, DROP `Remember Cookie`;
