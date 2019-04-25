01a0690eb3af8fc1d637d81fe7c5df97f5d96e03
Raul Perusquia <raul@inikoo.com>
2019-04-25 15:52:54 +0800

    cleaning up node class

---
+++ app_files/db_changelog/post_migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `Page Store Dimension` DROP `Page Header Key`,  DROP `Page Header Type`,  DROP `Page Footer Key`,   DROP `Page Footer Type`;
+
+update `Order Dimension` set `Order Website Key`=`Order Site Key` where `Order Website Key` is null;
+
