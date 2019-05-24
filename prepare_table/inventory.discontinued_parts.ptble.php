9fd98ac17b8cb4c16d2fd42c2c6b0c6999207881
Raul Perusquia <raul@inikoo.com>
2019-05-24 17:16:35 +0200

    ssymbols

--- prepare_table/inventory.discontinued_parts.ptble.php
+++ prepare_table/inventory.discontinued_parts.ptble.php
@@ -165,7 +165,7 @@ $sql_totals
     = "select count(Distinct P.`Part SKU`) as num from $table  $where  ";
 
 $fields
-    .= "P.`Part SKU`,`Part Reference`,`Part Package Description`,`Part Current Stock`,`Part Stock Status`,`Part Days Available Forecast`,`Part Current On Hand Stock`,`Part Next Deliveries Data`,
+    .= "P.`Part SKU`,`Part Reference`,`Part Package Description`,`Part Current Stock`,`Part Stock Status`,`Part Days Available Forecast`,`Part Current On Hand Stock`,`Part Next Deliveries Data`,`Part Symbol`,
 `Part $db_period Acc Dispatched` as dispatched,`Part Number Active Products`,`Part Margin`,
 `Part $db_period Acc Invoiced Amount` as sales,
 `Part Days Available Forecast`,$yb_fields
