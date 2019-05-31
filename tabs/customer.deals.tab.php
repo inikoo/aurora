25ecdd7a85ba6547d530d94bc803aa1c2af6cf84
Raul Perusquia <raul@inikoo.com>
2019-05-31 15:23:16 +0100

    customer discounts 3

---
+++ tabs/customer.deals.tab.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 31-05-2019 14:39:51 BST Sheffield, UK
+ Copyright (c) 2016, Inikoo
+
+ Version 3
+
+*/
+
+
+$tab     = 'customer.deals';
+$ar_file = 'ar_marketing_tables.php';
+$tipo    = 'customer_deals';
+
+$default = $user->get_tab_defaults($tab);
+
+
+$table_views = array(
+    'overview' => array(
+        'label' => _('Overview'),
+        'title' => _('Overview')
+    ),
+
+);
+
+$table_filters = array(
+
+    'name' => array('label' => _('Name')),
+
+);
+
+$parameters = array(
+    'parent'     => $state['object'],
+    'parent_key' => $state['key'],
+
+);
+
+
+$table_buttons   = array();
+/*
+$table_buttons[] = array(
+    'icon'      => 'plus',
+    'title'     => _('New offer'),
+    'reference' => "products/".$state['parent_key']."/category/".$state['key']."/deal_component/new"
+);
+*/
+$smarty->assign('table_buttons', $table_buttons);
+
+
+include 'utils/get_table_html.php';
+
