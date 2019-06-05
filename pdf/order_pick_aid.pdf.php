b32854fc72e2c331af5faa38ff0f03de8557503e
Raul Perusquia <raul@inikoo.com>
2019-06-05 11:25:34 +0100

    test

--- pdf/order_pick_aid.pdf.php
+++ pdf/order_pick_aid.pdf.php
@@ -26,18 +26,36 @@ $css = '<style>'.file_get_contents(getcwd().'/css/fontawesome-all.css').'</style
 $smarty->assign('css', $css);
 
 
-$mpdf = new \Mpdf\Mpdf(
-    [
-        'tempDir'       => __DIR__.'/../server_files/pdf_tmp',
-        'mode'          => 'utf-8',
-        'margin_left'   => 15,
-        'margin_right'  => 15,
-        'margin_top'    => 12,
-        'margin_bottom' => 25,
-        'margin_header' => 5,
-        'margin_footer' => 10
-    ]
-);
+if(isset($_REQUEST['with_labels'])) {
+
+
+    $mpdf = new \Mpdf\Mpdf(
+        [
+            'tempDir'       => __DIR__.'/../server_files/pdf_tmp',
+            'mode'          => 'utf-8',
+            'margin_left'   => 15,
+            'margin_right'  => 15,
+            'margin_top'    => 15,
+            'margin_bottom' => 25,
+            'margin_header' => 5,
+            'margin_footer' => 10
+        ]
+    );
+
+}else{
+    $mpdf = new \Mpdf\Mpdf(
+        [
+            'tempDir'       => __DIR__.'/../server_files/pdf_tmp',
+            'mode'          => 'utf-8',
+            'margin_left'   => 15,
+            'margin_right'  => 15,
+            'margin_top'    => 22,
+            'margin_bottom' => 25,
+            'margin_header' => 10,
+            'margin_footer' => 10
+        ]
+    );
+}
 
 
 //$mpdf->useOnlyCoreFonts = true;    // false is default
