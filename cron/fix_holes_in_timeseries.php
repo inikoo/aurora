2274a66ec60223df521e83a5c1dab9a8c4471fe7
Raul Perusquia <raul@inikoo.com>
2019-06-13 23:15:16 +0800

    save bis

--- cron/fix_holes_in_timeseries.php
+++ cron/fix_holes_in_timeseries.php
@@ -34,35 +34,8 @@ $date1 = '2018-03-01';
 $date2 = '2018-09-01';
 
 
-$sql = sprintf(
-    'SELECT `Supplier Key` FROM `Supplier Dimension`   '
-);
-
-if ($result = $db->query($sql)) {
-    foreach ($result as $row) {
-
-        $supplier=get_object('Supplier',$row['Supplier Key']);
-
-        print $supplier->get('Code')."\n";
-
-        $timeseries_data = $timeseries['Supplier'];
-        foreach ($timeseries_data as $time_series_data) {
 
 
-            $time_series_data['Timeseries Parent']     = 'Supplier';
-            $time_series_data['Timeseries Parent Key'] = $supplier->id;
-            $time_series_data['editor']                = $editor;
-
-
-            $object_timeseries = new Timeseries('find', $time_series_data, 'create');
-            $supplier->update_timeseries_record($object_timeseries, $date1, $date2);
-
-
-        }
-    }
-}
-
-exit;
 
 $sql = sprintf(
     'SELECT `Store Key` FROM `Store Dimension` WHERE `Store State`="Normal"   '
@@ -98,6 +71,38 @@ if ($result = $db->query($sql)) {
     print_r($error_info = $db->errorInfo());
     exit($sql);
 }
+
+
+exit;
+
+$sql = sprintf(
+    'SELECT `Supplier Key` FROM `Supplier Dimension`   '
+);
+
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+
+        $supplier=get_object('Supplier',$row['Supplier Key']);
+
+        print $supplier->get('Code')."\n";
+
+        $timeseries_data = $timeseries['Supplier'];
+        foreach ($timeseries_data as $time_series_data) {
+
+
+            $time_series_data['Timeseries Parent']     = 'Supplier';
+            $time_series_data['Timeseries Parent Key'] = $supplier->id;
+            $time_series_data['editor']                = $editor;
+
+
+            $object_timeseries = new Timeseries('find', $time_series_data, 'create');
+            $supplier->update_timeseries_record($object_timeseries, $date1, $date2);
+
+
+        }
+    }
+}
+
 
 
 
