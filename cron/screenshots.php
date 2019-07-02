d921c4e5d4e05ae73290cf654cac5efacfe9963c
Raul Perusquia <raul@inikoo.com>
2019-07-02 19:14:35 +0800

    screenshots

--- cron/screenshots.php
+++ cron/screenshots.php
@@ -5,22 +5,34 @@ require_once 'common.php';
 $optimizerChain = OptimizerChainFactory::create();
 
 
-$sql=sprintf('SELECT `Page Key`,`Webpage URL` FROM `Page Store Dimension` where  `Webpage State`="Online" ');
+$sql=sprintf('SELECT `Page Key`,`Webpage URL` FROM `Page Store Dimension` where  `Webpage State`="Online" and `Page Key`= 2562  ');
 if ($result=$db->query($sql)) {
 		foreach ($result as $row) {
 
 
-            $tmp_file_root=sprintf('server_files/tmp/%d_%d',gmdate('U'),$row['Page Key']);
+            $tmp_file_root=sprintf('server_files/tmp/original_%d_%d',gmdate('U'),$row['Page Key']);
 
             $cmd=sprintf('node cron/screenshots.js --file_root="%s" --url="%s" &',$tmp_file_root,addslashes($row['Webpage URL']));
 
             exec($cmd, $output);
 
 
-            $optimizerChain->optimize($tmp_file_root.'_desktop_full_screenshot.jpeg',$tmp_file_root.'x_desktop_full_screenshot.jpeg');
-            $optimizerChain->optimize($tmp_file_root.'_desktop_screenshot.jpeg',$tmp_file_root.'x_desktop_screenshot.jpeg');
-            $optimizerChain->optimize($tmp_file_root.'_mobile_screenshot.jpeg',$tmp_file_root.'x_mobile_screenshot.jpeg');
-            $optimizerChain->optimize($tmp_file_root.'_tablet_screenshot.jpeg',$tmp_file_root.'x_tablet_screenshot.jpeg');
+            process_screenshoot($tmp_file_root.'_desktop_full_screenshot.jpeg','Fullscreen Thumbnail');
+
+            process_screenshoot($tmp_file_root.'_desktop_full_screenshot.jpeg','Fullscreen');
+
+
+
+            process_screenshoot($tmp_file_root.'_desktop_screenshot.jpeg','Desktop');
+            process_screenshoot($tmp_file_root.'_mobile_screenshot.jpeg','Mobile');
+            process_screenshoot($tmp_file_root.'_tablet_screenshot.jpeg','Tablet');
+
+
+
+            unlink($tmp_file_root.'_desktop_full_screenshot.jpeg');
+            unlink($tmp_file_root.'_desktop_screenshot.jpeg');
+            unlink($tmp_file_root.'_mobile_screenshot.jpeg');
+            unlink($tmp_file_root.'_tablet_screenshot.jpeg');
 
 
 
@@ -34,36 +46,96 @@ if ($result=$db->query($sql)) {
 }
 
 
-exit;
+function process_screenshoot($filename,$type){
+    $size_data = getimagesize($filename);
+    $width  = $size_data[0];
+    $height = $size_data[1];
 
-try {
+    $resized_image_filename=preg_replace('/original/','resize',$filename);
 
-    $stmt = $db->prepare("SELECT `Page Key`,`Webpage URL` FROM sk.`Page Store Dimension`");
-    $stmt->execute();
+    switch ($type){
+        case 'Fullscreen':
+            $width_resize  = $width*.55;
+            $height_resize =  $height*.55;
+            break;
+        case 'Fullscreen Thumbnail':
 
+            $resized_image_filename=preg_replace('/original/','resize_thumbnail',$filename);
+            $ratio=$width/$height;
 
-    //$stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach($stmt->fetchAll() as $k=>$v) {
-       // echo "Start Time :".date("Y-m-d  H:i:s", time())."\n";
-        /*echo $v['Page Key']."|".$v['Webpage URL']."\n";*/
-        exec("node cron/screenshots.js --pageKey=".$v['Page Key']." --url=\"".$v['Webpage URL']."\" &", $output);
-        //echo "End Time :".date("Y-m-d  H:i:s", time())."\n";
+            $width_resize  = 270;
+            $height_resize =  270*$ratio;
+            break;
 
+        case 'Desktop':
 
+            $ratio=$width/$height;
 
+            $width_resize  = 270;
+            $height_resize =  270*$ratio;
+            break;
+        case 'Tablet':
 
-        $optimizerChain->optimize('server_files/tmp/'.$v['Page Key'].'_desktop_full_screenshot.jpeg');
-        $optimizerChain->optimize('server_files/tmp/'.$v['Page Key'].'_desktop_screenshot.jpeg');
-        $optimizerChain->optimize('server_files/tmp/'.$v['Page Key'].'_mobile_screenshot.jpeg');
-        $optimizerChain->optimize('server_files/tmp/'.$v['Page Key'].'_tablet_screenshot.jpeg');
+            $ratio=$width/$height;
 
+            $width_resize  = 270;
+            $height_resize =  270*$ratio;
+            break;
+        case 'Mobile':
 
+            $ratio=$width/$height;
+            $height_resize =  270;
+            $width_resize  = 270/$ratio;
 
+            break;
     }
-//    foreach($output as $k=>$v) {
-//        echo $k."|".$v."\n";
 
-//    }
-} catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
+    $image              = new ImageResize($filename);
+    $image->quality_jpg = 100;
+    $image->quality_png = 9;
+
+    $image->resizeToBestFit($width_resize,$height_resize);
+
+
+    $image->save($resized_image_filename);
+
+
+
+    if (file_exists($resized_image_filename)) {
+        usleep(  1000 );
+    }
+    if (file_exists($resized_image_filename)) {
+        usleep(  2000 );
+    }
+    if (file_exists($resized_image_filename)) {
+        usleep(  3000 );
+    }
+    if (file_exists($resized_image_filename)) {
+        usleep(  100000 );
+    }
+
+
+
+    $optimizerChain = OptimizerChainFactory::create();
+    $optimizerChain->optimize($resized_image_filename);
+
+    if (file_exists($resized_image_filename)) {
+        usleep(  1000 );
+    }
+    if (file_exists($resized_image_filename)) {
+        usleep(  2000 );
+    }
+    if (file_exists($resized_image_filename)) {
+        usleep(  3000 );
+    }
+    if (file_exists($resized_image_filename)) {
+        usleep(  100000 );
+    }
+
+
+
+
+
 }
+
+
