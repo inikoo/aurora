baaf6ba92dfe4ced504d0ba58f554ef2b2d91f19
Raul Perusquia <raul@inikoo.com>
2019-07-03 14:27:24 +0800

    stock vaalue

--- cron/update_parts_warehouse_sko_value.php
+++ cron/update_parts_warehouse_sko_value.php
@@ -53,7 +53,7 @@ if ($result = $db->query($sql)) {
 
 
 
-$lap_time0 = date('U');
+$lap_time0 = date('U');update_parts_warehouse_sko_value.php
 $contador  = 0;
 
 $sql = sprintf(
@@ -69,8 +69,8 @@ if ($result = $db->query($sql)) {
 
 
         $sql = sprintf(
-            'select  *, POTF.`Supplier Key`,`Date`,(`Inventory Transaction Amount`/`Inventory Transaction Quantity`) as value_per_sko ,`ITF POTF POTF Key` from    `ITF POTF Bridge` B  left join     `Inventory Transaction Fact` ITF   on  (B.`ITF POTF ITF Key`=`Inventory Transaction Key`)  
-    left join `Purchase Order Transaction Fact` POTF on  (`Purchase Order Transaction Fact Key`=`ITF POTF POTF Key`) where  `Inventory Transaction Amount`>0 and `Inventory Transaction Quantity`>0 and   B.`ITF POTF ITF State`="InvoiceChecked"  and  `Inventory Transaction Section`="In"    and ITF.`Part SKU`=%d    and POTF.`Supplier Key`!=%d order by `Date` desc  limit 1 ',
+            'select  *, POTF.`Supplier Key`,`Date`,(`Inventory Transaction Amount`/`Inventory Transaction Quantity`) as value_per_sko ,`ITF POTF Costing Done POTF Key` from    `ITF POTF Costing Done Bridge` B  left join     `Inventory Transaction Fact` ITF   on  (B.`ITF POTF Costing Done ITF Key`=`Inventory Transaction Key`)  
+    left join `Purchase Order Transaction Fact` POTF on  (`Purchase Order Transaction Fact Key`=`ITF POTF Costing Done POTF Key`) where  `Inventory Transaction Amount`>0 and `Inventory Transaction Quantity`>0 and   B.`ITF POTF Costing Done ITF State`="InvoiceChecked"  and  `Inventory Transaction Section`="In"    and ITF.`Part SKU`=%d    and POTF.`Supplier Key`!=%d order by `Date` desc  limit 1 ',
             $part->id,$manufacturer_key
         );
 
