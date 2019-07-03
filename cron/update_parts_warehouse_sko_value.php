44607a8b6c9b3b779fe3879bd443eb5b3b808489
Raul Perusquia <raul@inikoo.com>
2019-07-03 14:42:03 +0800

    fix bug in script

--- cron/update_parts_warehouse_sko_value.php
+++ cron/update_parts_warehouse_sko_value.php
@@ -69,8 +69,10 @@ if ($result = $db->query($sql)) {
 
 
         $sql = sprintf(
-            'select  *, POTF.`Supplier Key`,`Date`,(`Inventory Transaction Amount`/`Inventory Transaction Quantity`) as value_per_sko ,`ITF POTF Costing Done POTF Key` from    `ITF POTF Costing Done Bridge` B  left join     `Inventory Transaction Fact` ITF   on  (B.`ITF POTF Costing Done ITF Key`=`Inventory Transaction Key`)  
-    left join `Purchase Order Transaction Fact` POTF on  (`Purchase Order Transaction Fact Key`=`ITF POTF Costing Done POTF Key`) where  `Inventory Transaction Amount`>0 and `Inventory Transaction Quantity`>0 and   B.`ITF POTF Costing Done ITF State`="InvoiceChecked"  and  `Inventory Transaction Section`="In"    and ITF.`Part SKU`=%d    and POTF.`Supplier Key`!=%d order by `Date` desc  limit 1 ',
+            'select  POTF.`Supplier Key`,`Date`,(`Inventory Transaction Amount`/`Inventory Transaction Quantity`) as value_per_sko ,`ITF POTF Costing Done POTF Key` 
+from    `ITF POTF Costing Done Bridge` B  left join     `Inventory Transaction Fact` ITF   on  (B.`ITF POTF Costing Done ITF Key`=`Inventory Transaction Key`)  
+    left join `Purchase Order Transaction Fact` POTF on  (`Purchase Order Transaction Fact Key`=`ITF POTF Costing Done POTF Key`) 
+where  `Inventory Transaction Amount`>0 and `Inventory Transaction Quantity`>0   and  `Inventory Transaction Section`="In"    and ITF.`Part SKU`=%d    and POTF.`Supplier Key`!=%d order by `Date` desc  limit 1 ',
             $part->id,$manufacturer_key
         );
 
@@ -79,10 +81,13 @@ if ($result = $db->query($sql)) {
         if ($result=$db->query($sql)) {
             if ($row = $result->fetch()) {
 
+                if($row['ITF POTF Costing Done POTF Key']>0){
+                    $value_per_sko=$row['value_per_sko'];
 
-            //    print_r($row);
-
-                $value_per_sko=$row['value_per_sko'];
+                }else{
+                    $part->update_cost();
+                    $value_per_sko=$part->get('Part Cost');
+                }
 
             }else{
                 $part->update_cost();
@@ -94,6 +99,7 @@ if ($result = $db->query($sql)) {
              $part->update_field_switcher('Part Cost in Warehouse',$value_per_sko , 'no_history');
         }else{
             print $sql;
+            exit;
         }
 
 
