f417034954fe4de549444c7acd4d5d723c80f18f
Raul Perusquia <raul@inikoo.com>
2019-08-13 11:59:17 +0800

    fix bug in discounts

--- api_picking.php
+++ api_picking.php
@@ -557,7 +557,7 @@ switch ($_REQUEST['action']) {
 
 
 
-        $sql = 'select `Delivery Note Key`,`Delivery Note Customer Key`,`Delivery Note Type`,`Delivery Note Date Created`,`Delivery Note Estimated Weight`,`Delivery Note Store Key`,`Delivery Note ID`,`Delivery Note Customer Name`,`Store Code`,`Store Name`,`Delivery Note Number Ordered Parts` 
+        $sql = 'select `Delivery Note Key`,`Delivery Note Customer Key`,`Delivery Note Type`,`Delivery Note Date Created`,`Delivery Note Estimated Weight`,`Delivery Note Store Key`,`Delivery Note ID`,`Delivery Note Customer Name`,`Store Code`,`Store Name`,`Delivery Note Number Ordered Parts` ,`Delivery Note State`
         from `Delivery Note Dimension` D left join `Store Dimension` on (`Store Key`=`Delivery Note Store Key`) where `Delivery Note State`=? or `Delivery Note State`=?
         ';
 
