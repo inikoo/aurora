695166ba94e890739ebadbbeca67aeb970e11ae4
Raul Perusquia <raul@inikoo.com>
2019-09-12 16:18:40 +0800

    testing GDC test

---
+++ cron/update_current_gsc_data.php
@@ -0,0 +1,117 @@
+<?php
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 11-09-2019 15:04:07 MYT, Kuala Lumpur Malaysia
+ Copyright (c) 2019, Inikoo
+
+ Version 3
+
+*/
+
+require_once 'common.php';
+require_once 'utils/google_api_functions.php';
+require_once 'keyring/google_dns.php';
+require_once 'utils/date_functions.php';
+
+$webmasters = initialize_webmasters();
+
+
+$sql  = 'select `Website Key`,`Website URL` from `Website Dimension` where `Website Status`="Active"';
+$stmt = $db->prepare($sql);
+$stmt->execute(
+    array()
+);
+while ($row = $stmt->fetch()) {
+
+    $domain     = preg_replace('/^www\./', '', $row['Website URL']);
+    get_gsc_website_dates($db, $webmasters, $domain, $row['Website Key']);
+
+
+}
+sleep(20);
+
+$sql       = 'SELECT `Date` FROM kbase.`Date Dimension` WHERE `Date`>=? AND `Date`<=? order by `Date`';
+$stmt_date = $db->prepare($sql);
+$stmt_date->execute(
+    array(
+        date('Y-m-d', strtotime('now -10 days')),
+        date('Y-m-d')
+    )
+);
+while ($row_date = $stmt_date->fetch()) {
+
+    $date_interval = array(
+        'From' => $row_date['Date'],
+        'To'   => $row_date['Date'],
+    );
+
+
+
+    $sql  = 'select `Website Key`,`Website URL` from `Website Dimension` where `Website Status`="Active"';
+    $stmt = $db->prepare($sql);
+    $stmt->execute(
+        array()
+    );
+    while ($row = $stmt->fetch()) {
+
+        $domain     = preg_replace('/^www\./', '', $row['Website URL']);
+
+
+
+        get_gsc_website($db, $webmasters, $domain, $date_interval, $row['Website Key']);
+
+        get_gsc_webpage($db, $webmasters, $domain, $date_interval, $row['Website Key']);
+
+        get_gsc_website_queries($db, $webmasters, $domain, $date_interval, $row['Website Key']);
+
+        get_gsc_webpage_queries($db, $webmasters, $domain, $date_interval, $row['Website Key']);
+        sleep(20);
+
+    }
+
+
+}
+
+foreach(array('Last Month','Last Week','Yesterday','Week To Day','Today','Quarter To Day','Month To Day','Year To Day
+','1 Year','1 Quarter','1 Month') as $interval){
+
+    $interval_dates=calculate_interval_dates($db,$interval);
+
+
+
+    $date_interval = array(
+        'From' => preg_replace('/\s\d{2}\:\d{2}\:\d{2}$/','',$interval_dates[1]),
+        'To'   =>preg_replace('/\s\d{2}\:\d{2}\:\d{2}$/','',$interval_dates[2]),
+    );
+
+
+    $sql  = 'select `Website Key`,`Website URL` from `Website Dimension` where `Website Status`="Active"';
+    $stmt = $db->prepare($sql);
+    $stmt->execute(
+        array()
+    );
+    while ($row = $stmt->fetch()) {
+
+        $domain     = preg_replace('/^www\./', '', $row['Website URL']);
+
+
+
+        get_gsc_website($db, $webmasters, $domain, $date_interval, $row['Website Key'],$interval);
+
+
+        get_gsc_webpage($db, $webmasters, $domain, $date_interval, $row['Website Key'],$interval);
+
+        get_gsc_website_queries($db, $webmasters, $domain, $date_interval, $row['Website Key'],$interval);
+
+        get_gsc_webpage_queries($db, $webmasters, $domain, $date_interval, $row['Website Key'],$interval);
+
+        sleep(5);
+
+    }
+
+
+
+}
+
+
