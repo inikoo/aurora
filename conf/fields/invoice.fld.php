87ae33e9b318ead197ecca850531ff16f7b96b9a
Raul Perusquia <raul@inikoo.com>
2019-09-24 22:38:35 +0800

    secure edit part and supplier part

--- conf/fields/invoice.fld.php
+++ conf/fields/invoice.fld.php
@@ -198,9 +198,9 @@ if ($invoice->get('Invoice Type') == 'Invoice') {
                 'id'        => 'delete_invoice',
                 'class'     => 'operation',
                 'value'     => '',
-                'label'     => '<i class="fa fa-fw fa-'.($can_supervisor_accounting ? 'lock-alt' : 'lock').' button"  data-labels=\'{ "text":"'._('Please ask an authorised user to delete this invoice').'","title":"'._('Restricted operation').'","footer":"'._(
-                        'Authorised users'
-                    ).': "}\'  onClick="'.($can_supervisor_accounting ? 'toggle_unlock_delete_object(this)' : 'not_authorised_toggle_unlock_delete_object(this,\'IS\')').'"  style="margin-right:20px"></i> <span data-labels=\'{ "no_message":"'._(
+                'label'     => '<i class="fa fa-fw fa-'.($can_supervisor_accounting ? 'lock-alt' : 'lock').' button"  
+                data-labels=\'{ "text":"'._('Please ask an authorised user to delete this invoice').'","title":"'._('Restricted operation').'","footer":"'._('Authorised users').': "}\'  
+                onClick="'.($can_supervisor_accounting ? 'toggle_unlock_delete_object(this)' : 'not_authorised_toggle_unlock_delete_object(this,\'IS\')').'"  style="margin-right:20px"></i> <span data-labels=\'{ "no_message":"'._(
                         'A reason should be provided'
                     ).'", "button_text":"'._('Delete').'",  "title":"'._(
                         'Deleting invoice'
@@ -234,8 +234,8 @@ if ($invoice->get('Invoice Type') == 'Invoice') {
             'id'        => 'delete_refund',
             'class'     => 'operation',
             'value'     => '',
-            'label'     => '<i class="fa fa-fw fa-'.($can_supervisor_accounting ? 'lock-alt' : 'lock').' button" onClick="'.($can_supervisor_accounting ? 'toggle_unlock_delete_object(this)' : 'not_authorised_toggle_unlock_delete_object(\'IS\')')
-                .'" style="margin-right:20px"></i> <span data-labels=\'{ "no_message":"'._('A reason should be provided').'", "button_text":"'._('Delete').'",  "title":"'._(
+            'label'     => '<i class="fa fa-fw fa-'.($can_supervisor_accounting ? 'lock-alt' : 'lock').' button" onClick="'.($can_supervisor_accounting ? 'toggle_unlock_delete_object(this)' : 'not_authorised_toggle_unlock_delete_object(\'IS\')').'" 
+                            style="margin-right:20px"></i> <span data-labels=\'{ "no_message":"'._('A reason should be provided').'", "button_text":"'._('Delete').'",  "title":"'._(
                     'Deleting refund'
                 ).'", "text":"'._("This operation cannot be undone").'",  "placeholder":"'._('Write the reason for deleting this refund').'" }\' data-data=\'{ "object": "'.$object->get_object_name().'", "key":"'.$object->id
                 .'"}\' onClick="delete_invoice(this)" class="delete_object disabled">'._('Delete refund').' <i class="far fa-trash-alt new_button link "></i></span>',
