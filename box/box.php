c3989c6f9d61b288eba2136b37e833e6cbfaad77
Raul Perusquia <raul@inikoo.com>
2019-10-01 17:24:36 +0800

    the box

--- box/box.php
+++ box/box.php
@@ -1,11 +1,13 @@
-<?
+<?php
 /*
 
  About:
  Author: Raul Perusquia <rulovico@gmail.com>
  Created: 01-10-2019 14:52:31 MYT, Kuala Lumpur, Malaysia
 
- Copyright (c) 2019, Inikoo
+ Copyright (c) 2019, Inikoo Ltd
 
  Version 2.0
 */
+
+print "The box :)";
