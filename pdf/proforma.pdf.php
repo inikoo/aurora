796af990eef9ccad7a75fa55fa68ab0ac9daacf3
Raul Perusquia <raul@inikoo.com>
2019-10-02 19:07:28 +0800

    catch exeption wrong country

--- pdf/proforma.pdf.php
+++ pdf/proforma.pdf.php
@@ -189,9 +189,15 @@ if ($result = $db->query($sql)) {
             $_country=new Country('code',$row['Product Origin Country Code']);
 
 
-            $country=$countryRepository->get($_country->get('Country 2 Alpha Code'));
+            try {
+                $country     = $countryRepository->get($_country->get('Country 2 Alpha Code'));
+                $description .= ' <br>'._('Origin').': '.$country->getName().' ('.$country->getThreeLetterCode().')';
+            }catch (Exception $e) {
+                $description .= ' <br>'._('Origin').': '.$row['Product Origin Country Code'];
 
-            $description .= ' <br>'._('Origin').': '.$country->getName().' ('.$country->getThreeLetterCode().')';
+            }
+
+
         }
 
 
