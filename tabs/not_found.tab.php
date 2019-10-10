38a07658c937b23c1fffc6d7075158cece0d824c
Raul Perusquia <raul@inikoo.com>
2019-10-11 02:02:02 +0800

    not found page + fix bug

---
+++ tabs/not_found.tab.php
@@ -0,0 +1,15 @@
+<?php
+/*
+
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: Fri 11 Oct 2019 01:55:09 +0800 MYT, Kuala Lumpur, Malaysia
+
+ Copyright (c) 2019, Inikoo
+
+ Version 3.0
+*/
+
+$html = '<div style="padding:20px;;font-size:30px;opacity:.5;font-family: "Courier New", Courier, monospace"><i class="fal fa-bug"></i> 404</div>';
+
+
