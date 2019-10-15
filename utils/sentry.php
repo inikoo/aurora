c5a5eec0dd0da0b9b6ec558daba9368d0d3a911b
Raul Perusquia <raul@inikoo.com>
2019-10-15 19:07:46 +0800

    sentry releases

--- utils/sentry.php
+++ utils/sentry.php
@@ -9,10 +9,10 @@
 
  Version 2.0
 */
-use \Rollbar\Rollbar;
-use \Rollbar\Payload\Level;
 
-if(defined('ROLLBACK_ACCESS_TOKEN')) {
+use Rollbar\Rollbar;
+
+if (defined('ROLLBACK_ACCESS_TOKEN')) {
     Rollbar::init(
         array(
             'access_token' => ROLLBACK_ACCESS_TOKEN,
@@ -21,7 +21,31 @@ if(defined('ROLLBACK_ACCESS_TOKEN')) {
     );
 }
 
-if(defined('SENTRY_DNS_AU')){
-    Sentry\init(['dsn' => SENTRY_DNS_AU ]);
+
+
+
+if (defined('SENTRY_DNS_AU')) {
+    $sentry_config=array(
+        'dsn' => SENTRY_DNS_AU,
+    );
+    if($release=get_current_git_commit()){
+        $sentry_config['release']=$release;
+    }
+    Sentry\init($sentry_config);
+}
+
+
+/**
+ * Get the hash of the current git HEAD
+ * @param string $branch The git branch to check
+ *
+ * @return bool|false|string Either the hash or a boolean false
+ */
+function get_current_git_commit( $branch='master' ) {
+    if ( $hash = file_get_contents( sprintf( '.git/refs/heads/%s', $branch ) ) ) {
+        return $hash;
+    } else {
+        return false;
+    }
 }
 
