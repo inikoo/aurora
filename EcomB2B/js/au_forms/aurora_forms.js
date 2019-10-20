b7e3b50185e8a06291bd0b44e8bcb117afc7e6f0
Raul Perusquia <raul@inikoo.com>
2019-10-21 04:16:31 +0800

    fix WEBJS-QN error log in in mobile

--- EcomB2B/js/au_forms/aurora_forms.js
+++ EcomB2B/js/au_forms/aurora_forms.js
@@ -1,7 +1,10 @@
 /*Author: Raul Perusquia <raul@inikoo.com>
  Created: 8 April 2018 at 20:55:34 GMT+8, Kuala Lumpur, Malaysia
  Copyright (c) 2017, Inikoo
- Version 3.0*/
+ Version 3.0
+
+ Revision to fix bug Mon 21 Oct 2019 04:15:30 +0800 MYT
+ */
 
 
 $(function() {
