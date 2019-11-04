deb912170076e8e6f47cbf40908fa7ca12048ffd
Raul Perusquia <raul@inikoo.com>
2019-11-04 08:48:08 +0100

    clean up store DB

---
+++ app_files/db_changelog/V1.8__clean_up_store.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `Store Dimension`
+    DROP `Store Fax`,
+    DROP `Short Marketing Description`,
+    DROP `Store Collection Address Key`,
+    DROP `Store No Products Department Key`,
+    DROP `Store No Products Family Key`,
+    DROP `Cancel Orders In Basket Older Than`;
