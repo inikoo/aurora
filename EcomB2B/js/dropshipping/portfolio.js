467c565384e441dab7a64859ff01078be83ab252
Raul Perusquia <raul@inikoo.com>
2019-11-11 16:49:02 +0100

    add/remove product from porfolio (Desktop)

--- EcomB2B/js/dropshipping/portfolio.js
+++ EcomB2B/js/dropshipping/portfolio.js
@@ -6,14 +6,17 @@
 
 $(function () {
 
-    $(document).on('click', '.portfolio_row .edit_portfolio_item', function (evt) {
+    $(document).on('click', '.portfolio_row .edit_portfolio_item_trigger', function (evt) {
 
 
         var action;
 
 
+
         if ($(this).find('i').hasClass('fa-spinner')) return;
 
+
+        var product_id=$(this).closest('.product_container').data('product_id')
 
         if ($(this).hasClass('add_to_portfolio')) {
             action = 'add_product_to_portfolio';
@@ -25,7 +28,7 @@ $(function () {
         var ajaxData = new FormData();
 
         ajaxData.append("tipo", action)
-        ajaxData.append("product_id", $(this).closest('.product_container').data('product_id'))
+        ajaxData.append("product_id", product_id)
         ajaxData.append("webpage_key", $('#webpage_data').data('webpage_key'))
 
         $.ajax({
@@ -34,6 +37,15 @@ $(function () {
             complete: function () {
 
             }, success: function (data) {
+                var portfolio_row=$('.portfolio_row_' + product_id);
+
+                if(data.result=='add'){
+                    portfolio_row.find('.add_to_portfolio').addClass('hide')
+                    portfolio_row.find('.remove_from_portfolio').removeClass('hide')
+                }else{
+                    portfolio_row.find('.add_to_portfolio').removeClass('hide')
+                    portfolio_row.find('.remove_from_portfolio').addClass('hide')
+                }
 
 
             }, error: function () {
