4e8b0d5009671f48a6cf181913d5959fa340b181
Raul Perusquia <raul@inikoo.com>
2019-12-06 10:55:18 +0100

    DB change

---
+++ app_files/db_changelog/V1.16.4__add_field_Prosoect.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `Prospect Dimension` ADD `Prospect Last Contacted Date` DATETIME NULL DEFAULT NULL AFTER `Prospect First Contacted Date`;
--- app_files/db_changelog/V1.16.4__add_field_Prospaect.php
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `Prospect Dimension` ADD `Prospect Last Contacted Date` DATETIME NULL DEFAULT NULL AFTER `Prospect First Contacted Date`;
-
