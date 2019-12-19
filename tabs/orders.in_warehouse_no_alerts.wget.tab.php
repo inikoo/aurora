b82424cb1e26ac1a99e576bde62980d4f5e65fb1
Raul Perusquia <raul@inikoo.com>
2019-12-19 13:21:39 +0800

    small code formatting

--- tabs/orders.in_warehouse_no_alerts.wget.tab.php
+++ tabs/orders.in_warehouse_no_alerts.wget.tab.php
@@ -18,9 +18,8 @@ $default = $user->get_tab_defaults($tab);
 $table_views = array();
 
 $table_filters = array(
-    'customer' => array('label' => _('Customer')),
     'number'   => array('label' => _('Number')),
-
+    'customer' => array('label' => _('Customer')),
 );
 
 $parameters = array(
@@ -28,11 +27,4 @@ $parameters = array(
     'parent_key' => $state['parent_key'],
 );
 
-
-
-
-
 include('utils/get_table_html.php');
-
-
-
