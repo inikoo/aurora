eadd9928643458c43a99e0ebf7e20e6ddeae5448
Raul Perusquia <raul@inikoo.com>
2020-06-12 13:07:17 +0800

    DB repair SD PO type

--- app_files/db_changelog/V1.23.6__SD_PO_type.sql
+++ app_files/db_changelog/V1.23.6__SD_PO_type.sql
@@ -1,5 +1,6 @@
-ALTER TABLE `Supplier Dimension` CHANGE `Supplier Purchase Order Type` `Supplier Purchase Order Type` ENUM('Production','Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Parcel';
-ALTER TABLE `Agent Dimension` CHANGE `Agent Purchase Order Type` `Agent Purchase Order Type` ENUM('Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Parcel';
-ALTER TABLE `Purchase Order Dimension` CHANGE `Purchase Order Type` `Purchase Order Type` ENUM('Production','Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Parcel';
-ALTER TABLE `Supplier Delivery Dimension` CHANGE `Supplier Delivery Type` `Supplier Delivery Type` ENUM('Production','Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Parcel';
+ALTER TABLE `Supplier Dimension` CHANGE `Supplier Purchase Order Type` `Supplier Purchase Order Type` ENUM('Production','Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NULL DEFAULT 'Parcel';
+ALTER TABLE `Agent Dimension` CHANGE `Agent Purchase Order Type` `Agent Purchase Order Type` ENUM('Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NULL DEFAULT 'Parcel';
+ALTER TABLE `Purchase Order Dimension` CHANGE `Purchase Order Type` `Purchase Order Type` ENUM('Production','Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NULL DEFAULT 'Parcel';
+ALTER TABLE `Supplier Delivery Dimension` CHANGE `Supplier Delivery Type` `Supplier Delivery Type` ENUM('Production','Parcel','Container') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NULL DEFAULT 'Parcel';
+
 
