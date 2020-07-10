399f7b254f1dba44c6d32759f0e1bc0a385f30f7
Raul Perusquia <raul@inikoo.com>
2020-07-10 22:31:23 +0800

    ficing OIP in dispatched orers

---
+++ cron/fix_zombio_OIP_dispatched_orders.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 21:35:46 Friday, 10 July 2020
+ Copyright (c) 2020, Inikoo
+
+ Version 3
+
+*/
+
+
+require_once 'common.php';
+
+
+$editor = array(
+    'Author Name'  => '',
+    'Author Alias' => '',
+    'Author Type'  => '',
+    'Author Key'   => '',
+    'User Key'     => 0,
+    'Date'         => gmdate('Y-m-d H:i:s'),
+    'Subject'      => 'System',
+    'Subject Key'  => 0,
+    'Author Name'  => 'Script (Fix IOP dispatched orders)'
+);
+
+$counter=1;
+
+$sql  = "select  `Delivery Note ID`,DN.`Delivery Note Key`,`Inventory Transaction Key`,`Required`,`Picked`,`Packed`,`Out of Stock`,`Inventory Transaction Quantity`,`Inventory Transaction Amount`,`Inventory Transaction Weight` ,`Part SKU`   from  `Inventory Transaction Fact` ITF  left join `Delivery Note Dimension` DN on (DN.`Delivery Note Key`=ITF.`Delivery Note Key`) 
+    where `Delivery Note State`='Dispatched' and `Inventory Transaction Section`='OIP' 
+";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+while ($row = $stmt->fetch()) {
+
+   // print_r($row);
+
+    $part         = get_object('Part', $row['Part SKU']);
+    $part->editor = $editor;
+    $part->update_stock();
+
+    print $counter.' '.$row['Delivery Note ID'].' '.$row['Required'].' '.$part->get('Reference')."\n";
+
+
+    $out_of_stock = $row['Required'];
+
+    $transaction_record_type = 'Info';
+    $transaction_type        = 'No Dispatched';
+    $transaction_section     = 'NoDispatched';
+
+
+    $sql = 'UPDATE  `Inventory Transaction Fact`  SET  `Inventory Transaction Record Type`=?,`Date Picked`=null ,`Date Packed`=null ,`Inventory Transaction Type`=? ,`Inventory Transaction Section`=? ,
+                                      `Inventory Transaction Quantity`=0, `Inventory Transaction Amount`=0,`Inventory Transaction Weight`=0,
+                                      `Picked`=0,`Packed`=0,`Out of Stock`=? WHERE `Inventory Transaction Key`=? ';
+
+
+    $db->prepare($sql)->execute(
+        [
+            $transaction_record_type,
+            $transaction_type,
+            $transaction_section,
+            $out_of_stock,
+            $row['Inventory Transaction Key']
+
+        ]
+    );
+
+
+    /**
+     * @var $part \Part
+     */
+
+
+    $part->update_stock();
+    $counter++;
+
+
+
+    //print_r($part);
+
+
+    // exit;
+
+
+}
