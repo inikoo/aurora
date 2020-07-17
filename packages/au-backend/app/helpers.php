beaf98fd4019901e0c32d7dec3f62739260cb8fe
Raul Perusquia <raul@inikoo.com>
2020-07-17 16:19:42 +0800

    add support for autoload

---
+++ packages/au-backend/app/helpers.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Global helper functions
+Author: Raul Perusquia (raul@inikoo.com)
+Created:  Fri Jul 17 2020 16:17:50 GMT+0800 (Malaysia Time)
+Copyright (c) 2020, Inikoo Ltd
+
+Version 4
+*/
+
