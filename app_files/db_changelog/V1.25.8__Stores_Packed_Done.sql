b9f336633bcf2dfc3228a5a8030c87397d5158a6
Raul Perusquia <raul@inikoo.com>
2020-10-28 14:36:13 +0800

    typo

--- app_files/db_changelog/V1.25.8__Stores_Packed_Done.sql
+++ app_files/db_changelog/V1.25.8__Stores_Packed_Done.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `Store Data` ADD `Store Orders Packed Done Number` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0' AFTER `Store Orders Packed Amount`, ADD `Store Orders Packed Done Amount` DECIMAL(12,2) NOT NULL DEFAULT '0' AFTER `Orders Packed Done Number`;
+ALTER TABLE `Store Data` ADD `Store Orders Packed Done Number` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0' AFTER `Store Orders Packed Amount`, ADD `Store Orders Packed Done Amount` DECIMAL(12,2) NOT NULL DEFAULT '0' AFTER `Store Orders Packed Done Number`;
 
 ALTER TABLE `Store DC Data` ADD `Store DC Orders Packed Done Amount` DECIMAL(12,2) NOT NULL DEFAULT '0.00' AFTER `Store DC Orders Packed Amount`;
 ALTER TABLE `Account Data` ADD `Account Orders Packed Done Number` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0' AFTER `Account Orders Packed Amount`, ADD `Account Orders Packed Done Amount` DECIMAL(12,2) NOT NULL DEFAULT '0.00' AFTER `Account Orders Packed Done Number`;
