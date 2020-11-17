d126be941bf93d0f4269083ab614867d46e08090
Raul Perusquia <raul@inikoo.com>
2020-11-17 13:32:55 +0800

    scrip clean IOP

--- cron/fix_zombio_OIP_dispatched_orders.php
+++ cron/fix_zombio_OIP_dispatched_orders.php
@@ -28,44 +28,55 @@ $editor = array(
 
 $counter=1;
 
-$sql  = "select  `Delivery Note ID`,DN.`Delivery Note Key`,`Inventory Transaction Key`,`Required`,`Picked`,`Packed`,`Out of Stock`,`Inventory Transaction Quantity`,`Inventory Transaction Amount`,`Inventory Transaction Weight` ,`Part SKU`   from  `Inventory Transaction Fact` ITF  left join `Delivery Note Dimension` DN on (DN.`Delivery Note Key`=ITF.`Delivery Note Key`) 
-    where `Delivery Note State`='Dispatched' and `Inventory Transaction Section`='OIP' 
+$sql  = "select  `Delivery Note State`,`Delivery Note ID`,DN.`Delivery Note Key`,`Inventory Transaction Key`,`Required`,`Picked`,`Packed`,`Out of Stock`,`Inventory Transaction Quantity`,`Inventory Transaction Amount`,`Inventory Transaction Weight` ,`Part SKU`   from  `Inventory Transaction Fact` ITF  left join `Delivery Note Dimension` DN on (DN.`Delivery Note Key`=ITF.`Delivery Note Key`) 
+    where `Delivery Note State` in ('Dispatched','Cancelled') and `Inventory Transaction Section`='OIP' 
 ";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 while ($row = $stmt->fetch()) {
 
-   // print_r($row);
+
+    //print_r($row);
+
 
     $part         = get_object('Part', $row['Part SKU']);
     $part->editor = $editor;
     $part->update_stock();
 
-    print $counter.' '.$row['Delivery Note ID'].' '.$row['Required'].' '.$part->get('Reference')."\n";
+    print $counter.' '.$row['Delivery Note State'].'  '.$row['Delivery Note ID'].' '.$row['Required'].' '.$part->get('Reference')."\n";
 
 
-    $out_of_stock = $row['Required'];
 
-    $transaction_record_type = 'Info';
-    $transaction_type        = 'No Dispatched';
-    $transaction_section     = 'NoDispatched';
+    if($row['Delivery Note State']=='Dispatched') {
+
+        $out_of_stock = $row['Required'];
+
+        $transaction_record_type = 'Info';
+        $transaction_type        = 'No Dispatched';
+        $transaction_section     = 'NoDispatched';
 
 
-    $sql = 'UPDATE  `Inventory Transaction Fact`  SET  `Inventory Transaction Record Type`=?,`Date Picked`=null ,`Date Packed`=null ,`Inventory Transaction Type`=? ,`Inventory Transaction Section`=? ,
+        $sql = 'UPDATE  `Inventory Transaction Fact`  SET  `Inventory Transaction Record Type`=?,`Date Picked`=null ,`Date Packed`=null ,`Inventory Transaction Type`=? ,`Inventory Transaction Section`=? ,
                                       `Inventory Transaction Quantity`=0, `Inventory Transaction Amount`=0,`Inventory Transaction Weight`=0,
                                       `Picked`=0,`Packed`=0,`Out of Stock`=? WHERE `Inventory Transaction Key`=? ';
 
 
-    $db->prepare($sql)->execute(
-        [
-            $transaction_record_type,
-            $transaction_type,
-            $transaction_section,
-            $out_of_stock,
-            $row['Inventory Transaction Key']
+        $db->prepare($sql)->execute(
+            [
+                $transaction_record_type,
+                $transaction_type,
+                $transaction_section,
+                $out_of_stock,
+                $row['Inventory Transaction Key']
 
-        ]
-    );
+            ]
+        );
+    }elseif($row['Delivery Note State']=='Cancelled') {
+
+        $sql = sprintf("UPDATE `Inventory Transaction Fact`  SET `Date Picked`=null ,`Date Packed`=null ,  `Picked`=0,`Packed`=0, `Inventory Transaction Type`='FailSale' , `Inventory Transaction Section`='NoDispatched'  WHERE `Inventory Transaction Key`=%d  ", $row['Inventory Transaction Key']);
+        $db->exec($sql);
+
+    }
 
 
     /**
@@ -81,7 +92,7 @@ while ($row = $stmt->fetch()) {
     //print_r($part);
 
 
-    // exit;
+     //exit;
 
 
 }
