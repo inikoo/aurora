551e102ac70c89e45f95da78eb1982854c02fc6a
Raul Perusquia <raul@inikoo.com>
2021-01-26 01:12:50 +0800

    small changes

--- cron/update_basket_orders.php
+++ cron/update_basket_orders.php
@@ -32,7 +32,7 @@ if ($result = $db->query($sql)) {
 $lap_time0 = date('U');
 $contador  = 0;
 
-$sql = sprintf("SELECT `Order Key` FROM `Order Dimension`  left join `Store Dimension` on (`Store Key`=`Order Store Key`) where `Order State`='InBasket'and `Store Type`!='External'  order by `Order Date` desc");
+$sql = sprintf("SELECT `Order Key` FROM `Order Dimension`  left join `Store Dimension` on (`Store Key`=`Order Store Key`) where `Order State`='InBasket'and `Store Type`!='External'    order by `Order Date` desc");
 if ($result = $db->query($sql)) {
     foreach ($result as $row) {
 
@@ -41,6 +41,7 @@ if ($result = $db->query($sql)) {
 
       //  print $order->id.' '.$order->get('Order Date')."\n";
 
+        $order->update_tax();
 
         $order->fast_update(
             array(
