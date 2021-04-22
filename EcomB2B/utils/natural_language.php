b3a99412b2fd1e96362683dff83bdb211e99319a
Raul Perusquia <raul@inikoo.com>
2021-04-22 17:47:23 +0800

    fix bug

--- EcomB2B/utils/natural_language.php
+++ EcomB2B/utils/natural_language.php
@@ -736,5 +736,26 @@ function translate_written_number($string) {
     return $string;
 }
 
+function substrwords($text, $maxchar, $end='&hellip;') {
+    if (strlen($text) > $maxchar || $text == '') {
+        $words = preg_split('/\s/', $text);
+        $output = '';
+        $i      = 0;
+        while (1) {
+            $length = strlen($output)+strlen($words[$i]);
+            if ($length > $maxchar) {
+                break;
+            }
+            else {
+                $output .= " " . $words[$i];
+                ++$i;
+            }
+        }
+        $output .= $end;
+    }
+    else {
+        $output = $text;
+    }
+    return $output;
+}
 
-?>
