c013783346ba3a798cdea158b4219a097972ec36
Raul A Perusquia <raul@inikoo.com>
2021-07-25 13:32:13 +0800

    AU-55 dropping table Order Post Transaction Dimension (Feedback legacy)

---
+++ app_files/db_changelog/V1.28.30__Drop_OPTD.sql
@@ -0,0 +1,8 @@
+/*
+ *  Author: Raul Perusquia <raul@inikoo.com>
+ *  Created: Sun, 25 Jul 2021 13:30:04 Malaysia Time, Kuala Lumpur, Malaysia
+ *  Copyright (c) 2021, Inikoo
+ *  Version 3.0
+ */
+
+DROP TABLE IF EXISTS `Order Post Transaction Dimension`;
