bb1789d0711c449e9f62d3af8ff60c3aa090a33e
Raul A Perusquia <raul@inikoo.com>
2021-08-07 00:54:32 +0800

    AU-55 clean up scripts in cron directory

--- cron/cron_common.php
+++ cron/cron_common.php
@@ -11,23 +11,21 @@
 */
 
 chdir('../');
+/** @var string $dns_host */
+/** @var string $dns_port */
+/** @var string $dns_db */
+/** @var string $dns_user */
+/** @var string $dns_pwd */
 
 require_once 'vendor/autoload.php';
 require_once 'utils/sentry.php';
-
 require_once 'keyring/dns.php';
 require_once 'keyring/au_deploy_conf.php';
-
 require_once 'keyring/key.php';
 require_once 'utils/i18n.php';
 require_once 'utils/general_functions.php';
 require_once 'utils/object_functions.php';
-
-
 require_once "class.Account.php";
-
-
-
 
 
 $redis = new Redis();
@@ -39,22 +37,16 @@ $db = new PDO(
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 
-
 $warehouse_key = '';
-$sql           = sprintf('SELECT `Warehouse Key` FROM `Warehouse Dimension` WHERE `Warehouse State`="Active" limit 1');
+
+$sql = "SELECT `Warehouse Key` FROM `Warehouse Dimension` WHERE `Warehouse State`='Active' limit 1";
 
 if ($result2 = $db->query($sql)) {
     if ($row2 = $result2->fetch()) {
         $warehouse_key = $row2['Warehouse Key'];
     }
-} else {
-    print_r($error_info = $db->errorInfo());
-    print "$sql\n";
-    exit;
 }
-$_SESSION['current_warehouse']= $warehouse_key;
-
-
+$_SESSION['current_warehouse'] = $warehouse_key;
 
 
 $account = new Account($db);
