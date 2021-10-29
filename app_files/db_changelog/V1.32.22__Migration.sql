dd664f92afc095887a285aae60b79c39d335bea8
Raul A Perusquia <raul@inikoo.com>
2021-10-29 14:59:48 +0800

    Migrations

---
+++ app_files/db_changelog/V1.32.22__Migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Part Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+ALTER TABLE `Customer Portfolio Timeline` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
---
+++ app_files/db_changelog/V1.32.23__Migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `Part Dimension` ADD `aiku_unit_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_unit_id`);
