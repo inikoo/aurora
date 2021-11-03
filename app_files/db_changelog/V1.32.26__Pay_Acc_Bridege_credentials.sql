f3e5b8d4ac8c489a107a5171814b6a4b12ae91c2
Raul A Perusquia <raul@inikoo.com>
2021-11-03 21:34:13 +0800

    db

---
+++ app_files/db_changelog/V1.32.26__Pay_Acc_Bridege_credentials.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `Payment Account Store Bridge` ADD COLUMN `login` varchar(255) NULL COMMENT '';
+
+ALTER TABLE `Payment Account Store Bridge` ADD COLUMN `password` varchar(255) NULL COMMENT '';
