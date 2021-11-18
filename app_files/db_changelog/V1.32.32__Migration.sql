56d615cbd9096265c296df7a4dfb96209eecc7ee
Raul A Perusquia <raul@inikoo.com>
2021-11-18 16:41:36 +0800

    migration

---
+++ app_files/db_changelog/V1.32.32__Migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `Shipping Zone Schema Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+ALTER TABLE `Shipping Zone Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+ALTER TABLE `Charge Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+
+
