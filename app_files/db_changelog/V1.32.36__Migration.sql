3292a4ac94f3d5881e617a73618e3d4bc1be4f04
Raul A Perusquia <raul@inikoo.com>
2021-12-04 13:24:37 +0800

    migration

---
+++ app_files/db_changelog/V1.32.36__Migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `Order Transaction Fact` ADD `aiku_invoice_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_invoice_id`);
---
+++ app_files/db_changelog/V1.32.38__Migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Invoice Deleted Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+
