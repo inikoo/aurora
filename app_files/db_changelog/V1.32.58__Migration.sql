10d0d6d2e8e676f4a0057be333ef5d72fb8d66b1
Raul A Perusquia <raul@inikoo.com>
2022-01-15 15:05:32 +0800

    migrartion

---
+++ app_files/db_changelog/V1.32.58__Migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Supplier Dimension` ADD `aiku_workshop_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_workshop_id`);
+ALTER TABLE `Supplier Deleted Dimension` ADD `aiku_workshop_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_workshop_id`);
