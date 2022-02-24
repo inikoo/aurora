90a3042f88d67ea476e070efe1fb5952a2d08e25
Raul A Perusquia <raul@inikoo.com>
2022-02-24 21:15:41 +0800

    intastat

---
+++ app_files/db_changelog/V1.32.64__Migrations.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Supplier Part Historic Dimension` ADD COLUMN `aiku_agent_unit_id` int unsigned NULL COMMENT '';
+ALTER TABLE `Supplier Part Dimension` ADD COLUMN `aiku_agent_unit_id` int unsigned NULL COMMENT '';
