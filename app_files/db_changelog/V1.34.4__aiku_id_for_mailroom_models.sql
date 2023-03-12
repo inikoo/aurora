6891dcefbbc915953b3b10c1d642f056b4e94e9a
Raul Perusquia <raul@inikoo.com>
2023-03-12 20:09:12 +0800

    Mailroom migration

---
+++ app_files/db_changelog/V1.34.4__aiku_id_for_mailroom_models.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Email Campaign Type Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+ALTER TABLE `Email Campaign Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
