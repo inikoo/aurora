e1c7ea757fe0e36b8d827238342b1b4851470f08
Raul Perusquia <raul@inikoo.com>
2023-08-14 20:18:12 +0800

    past pay DB 3

--- app_files/db_changelog/V1.34.11__pastpay_charges.sql
+++ app_files/db_changelog/V1.34.11__pastpay_charges.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `Charge Dimension`add_charge
+ALTER TABLE `Charge Dimension`
     CHANGE `Charge Scope` `Charge Scope` enum('Hanging','Premium','Insurance','Tracking','Pastpay','CoD') NULL COMMENT '',
     CHANGE `Charge Trigger` `Charge Trigger` enum('Product','Order','Selected by Customer','Payment Type') NULL COMMENT '';
