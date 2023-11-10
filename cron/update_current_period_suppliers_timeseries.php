b8721289ad304db6ce015f350989be17c2e400a0
Raul Perusquia <raul@inikoo.com>
2023-11-10 21:04:32 +0800

    agent stats

--- cron/update_current_period_suppliers_timeseries.php
+++ cron/update_current_period_suppliers_timeseries.php
@@ -15,6 +15,7 @@ require_once __DIR__.'/cron_common.php';
 require_once 'class.Timeserie.php';
 
 require_once 'class.Supplier.php';
+require_once 'class.Agent.php';
 
 require_once 'utils/date_functions.php';
 require_once 'conf/timeseries.php';
@@ -29,6 +30,103 @@ $editor = array(
 );
 
 $timeseries = get_time_series_config();
+
+
+
+$sql = sprintf(
+    'SELECT `Agent Key`,`Agent Code` FROM `Agent Dimension`  ',
+);
+
+
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+
+        $agent = new Agent($row['Agent Key']);
+
+
+        $timeseries_data = $timeseries['Agent'];
+
+
+        foreach ($timeseries_data as $time_series_data) {
+
+
+
+            $to   = gmdate('Y-m-d');
+
+            if ($time_series_data['Timeseries Frequency'] == 'Daily') {
+                $from = gmdate('Y-m-d', strtotime('yesterday'));
+
+            }elseif ($time_series_data['Timeseries Frequency'] == 'Monthly') {
+                $from = gmdate('Y-m-01');
+
+
+            }elseif ($time_series_data['Timeseries Frequency'] == 'Yearly') {
+                $from = gmdate('Y-01-01');
+
+            }elseif ($time_series_data['Timeseries Frequency'] == 'Quarterly') {
+
+
+
+                $sql = sprintf(
+                    "SELECT  MAKEDATE(YEAR(`Date`), 1) + INTERVAL QUARTER(`Date`) QUARTER - INTERVAL 1 QUARTER  AS start FROM kbase.`Date Dimension` WHERE `Date`>=date(%s) AND `Date`<=DATE(%s)  GROUP BY Yearweek(`Date`,5) ",
+                    prepare_mysql(gmdate('Y-m-d')), prepare_mysql(gmdate('Y-m-d'))
+                );
+
+                if ($result2=$db->query($sql)) {
+                    if ($row2 = $result2->fetch()) {
+                        $from=$row2['start'];
+                    }
+                }else {
+                    print_r($error_info=$db->errorInfo());
+                    print "$sql\n";
+                    exit;
+                }
+
+            }elseif ($time_series_data['Timeseries Frequency'] == 'Weekly') {
+                $sql = sprintf(
+                    "SELECT  DATE_ADD(`Date`, INTERVAL(-WEEKDAY(`Date`)) DAY) AS  start FROM kbase.`Date Dimension` WHERE `Date`>=date(%s) AND `Date`<=DATE(%s)  GROUP BY Yearweek(`Date`,5) ",
+                    prepare_mysql(gmdate('Y-m-d')), prepare_mysql(gmdate('Y-m-d'))
+                );
+
+                if ($result2=$db->query($sql)) {
+                    if ($row2 = $result2->fetch()) {
+                        $from=$row2['start'];
+                    }
+                }else {
+                    print_r($error_info=$db->errorInfo());
+                    print "$sql\n";
+                    exit;
+                }
+
+
+            }
+
+
+
+            $time_series_data['Timeseries Parent']     = 'Agent';
+            $time_series_data['Timeseries Parent Key'] = $agent->id;
+
+
+            $editor['Date']             = gmdate('Y-m-d H:i:s');
+            $time_series_data['editor'] = $editor;
+
+            $object_timeseries = new Timeseries('find', $time_series_data, 'create');
+
+
+            $agent->update_timeseries_record($object_timeseries, $from, $to);
+
+
+        }
+        exit;
+    }
+
+} else {
+    print_r($error_info = $db->errorInfo());
+    exit($sql);
+}
+
+
+
 
 
 $date = gmdate('Y-m-d', strtotime('yesterday'));
@@ -128,4 +226,3 @@ if ($result = $db->query($sql)) {
 }
 
 
-?>
