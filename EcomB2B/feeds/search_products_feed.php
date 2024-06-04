dc2dcdda8adf6c3dd1c69ca9290fca9e21f33156
Raul Perusquia <raul@inikoo.com>
2024-06-04 11:57:48 +0200

    search feed

--- EcomB2B/feeds/search_products_feed.php
+++ EcomB2B/feeds/search_products_feed.php
@@ -73,7 +73,7 @@ function array_to_xml( $data, &$xml_data ) {
     foreach( $data as $key => $value ) {
         if( is_array($value) ) {
             if( is_numeric($key) ){
-                $key = 'item'.$key; //dealing with <0/>..<n/> issues
+                $key = 'item';
             }
             $subnode = $xml_data->addChild($key);
             array_to_xml($value, $subnode);
