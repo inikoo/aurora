0d74a85ddf89250d43df59fc9a81278c03d15a73
aqordeon <aqordivika@yahoo.co.id>
2024-06-21 15:29:02 +0800

    fix: Luigi: search in Indonesia

--- EcomB2B/js/vika_luigi.js
+++ EcomB2B/js/vika_luigi.js
@@ -1,58 +1,60 @@
-if (window.location.hostname === 'www.aw-indonesia.com') {
-  console.log('Hello world!!!')
-} else if (window.location.hostname === 'ancientwisdom.biz') {
-  console.log('Hello world!!')
+if (window.location.hostname === "www.aw-indonesia.com") {
+  console.log("Hello world!1!")
+  import("https://cdn.luigisbox.com/autocomplete.js") // For autocomplete
+
+  const luigiTrackerId = "483878-588294"
+
+  const LBInitAutocomplete = () => {
+    AutoComplete(
+      {
+        Layout: "heromobile",
+        TrackerId: luigiTrackerId,
+        Locale: "en",
+        Types: [
+          {
+            name: "Product",
+            type: "product",
+            size: 7,
+          },
+          {
+            name: "Query",
+            type: "query",
+          },
+          {
+            name: "Category",
+            type: "category",
+          },
+        ],
+      },
+      "#inputLuigi"
+    )
+  }
+
+  const loadCSS = (href) => {
+    const link = document.createElement("link")
+    link.rel = "stylesheet"
+    link.href = href
+    document.head.appendChild(link)
+  }
+
+  const loadInputLuigi = () => {
+    // const input = document.createElement("input")
+    // input.id = "inputSearchLuigi"
+
+    // Hide the original input Aurora
+    const originalInput = document.getElementById("header_search_input")
+    originalInput.classList.add("hide");
+
+    // Show input for Luigi
+    const inputLuigi = document.getElementById("inputLuigi")
+    inputLuigi.classList.remove("hide");
+  }
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    loadCSS("https://cdn.luigisbox.com/autocomplete.css")
+    loadInputLuigi()
+    LBInitAutocomplete()
+  })
+} else {
+  console.log("Hello world!!")
 }
-
-// import "https://cdn.luigisbox.com/autocomplete.js" // For autocomplete
-
-// console.log("Hello world 123!!")
-
-// const luigiTrackerId = "483878-588294"
-
-// const LBInitAutocomplete = () => {
-//   AutoComplete(
-//     {
-//       Layout: "heromobile",
-//       TrackerId: luigiTrackerId,
-//       Locale: "en",
-//       Types: [
-//         {
-//           name: "Product",
-//           type: "product",
-//           size: 7,
-//         },
-//         {
-//           name: "Query",
-//           type: "query",
-//         },
-//         {
-//           name: "Category",
-//           type: "category",
-//         },
-//       ],
-//     },
-//     "#inputSearchLuigi"
-//   )
-// }
-
-// const loadCSS = (href) => {
-//   const link = document.createElement("link")
-//   link.rel = "stylesheet"
-//   link.href = href
-//   document.head.appendChild(link)
-// }
-
-// const loadInputLuigi = () => {
-//   const input = document.createElement("input")
-//   input.id = "inputSearchLuigi"
-
-//   const divInputLuigi = document.getElementById("inputLuigi")
-//   divInputLuigi.appendChild(input)
-// }
-
-// document.addEventListener("DOMContentLoaded", async () => {
-//   loadCSS("https://cdn.luigisbox.com/autocomplete.css")
-//   loadInputLuigi()
-//   LBInitAutocomplete()
-// })
