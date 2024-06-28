f9beb14b1649009a1e55c42c673fc8ea0dcce38e
aqordeon <aqordivika@yahoo.co.id>
2024-06-28 16:38:38 +0800

    fix: Luigi: comment all

--- EcomB2B/js/vika_luigi.js
+++ EcomB2B/js/vika_luigi.js
@@ -1,171 +1,171 @@
-function stringToBoolean(str) {
-    if (str.toLowerCase() === 'true') {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function stringToBoolean(str) {
+//     if (str.toLowerCase() === 'true') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-// Init: Auto Complete
-const LBInitAutocomplete = async (luigiTrackerId, fieldsRemoved, attributesList) => {
-    await import("https://cdn.luigisbox.com/autocomplete.js")
-    await AutoComplete(
-        {
-            Layout: "heromobile",
-            TrackerId: luigiTrackerId,
-            Locale: "en",
-            Types: [
-                {
-                    name: "Item",
-                    type: "item",
-                    size: 7,
-                    attributes: attributesList,
-                },
-                {
-                    name: "Query",
-                    type: "query",
-                },
-                {
-                    name: "Category",
-                    type: "category",
-                },
-            ],
-            RemoveFields: fieldsRemoved,
-            ShowAllCallback: () => {  // Called when 'Show All Product' clicked
-                onSearchQuery(document.querySelector('#inputLuigi')?.value)
-            }
-        },
-        "#inputLuigi"
-    )
+// // Init: Auto Complete
+// const LBInitAutocomplete = async (luigiTrackerId, fieldsRemoved, attributesList) => {
+//     await import("https://cdn.luigisbox.com/autocomplete.js")
+//     await AutoComplete(
+//         {
+//             Layout: "heromobile",
+//             TrackerId: luigiTrackerId,
+//             Locale: "en",
+//             Types: [
+//                 {
+//                     name: "Item",
+//                     type: "item",
+//                     size: 7,
+//                     attributes: attributesList,
+//                 },
+//                 {
+//                     name: "Query",
+//                     type: "query",
+//                 },
+//                 {
+//                     name: "Category",
+//                     type: "category",
+//                 },
+//             ],
+//             RemoveFields: fieldsRemoved,
+//             ShowAllCallback: () => {  // Called when 'Show All Product' clicked
+//                 onSearchQuery(document.querySelector('#inputLuigi')?.value)
+//             }
+//         },
+//         "#inputLuigi"
+//     )
 
-    console.log("Init autocomplete")
-}
+//     console.log("Init autocomplete")
+// }
 
-// Init: Search result
-const LBInitSearchResult = async (luigiTrackerId, fieldsRemoved) => {
-    await import("https://cdn.luigisbox.com/search.js")
-    await Luigis.Search(
-        {
-            TrackerId: luigiTrackerId,
-            Locale: "en",
-            Theme: "boo",
-            Size: 10,
-            Facets: ['brand', 'category', 'color'],
-            DefaultFilters: {
-                type: 'item'
-            },
-            UrlParamName: {
-                QUERY: "q",
-            },
-            RemoveFields: fieldsRemoved
-        },
-        "#inputLuigi",
-        "#luigi_result_search"
-    )
+// // Init: Search result
+// const LBInitSearchResult = async (luigiTrackerId, fieldsRemoved) => {
+//     await import("https://cdn.luigisbox.com/search.js")
+//     await Luigis.Search(
+//         {
+//             TrackerId: luigiTrackerId,
+//             Locale: "en",
+//             Theme: "boo",
+//             Size: 10,
+//             Facets: ['brand', 'category', 'color'],
+//             DefaultFilters: {
+//                 type: 'item'
+//             },
+//             UrlParamName: {
+//                 QUERY: "q",
+//             },
+//             RemoveFields: fieldsRemoved
+//         },
+//         "#inputLuigi",
+//         "#luigi_result_search"
+//     )
 
-    console.log("Init Search")
-}
+//     console.log("Init Search")
+// }
 
 
-// Import Luigi CSS style
-const importStyleCSS = () => {
-    const link = document.createElement("link")
-    link.rel = "stylesheet"
-    link.href = "https://cdn.luigisbox.com/autocomplete.css"
-    document.head.appendChild(link)
-}
+// // Import Luigi CSS style
+// const importStyleCSS = () => {
+//     const link = document.createElement("link")
+//     link.rel = "stylesheet"
+//     link.href = "https://cdn.luigisbox.com/autocomplete.css"
+//     document.head.appendChild(link)
+// }
 
-// Method: add 'hide' class to component
-const setComponentHide = (selector) => {
-    const selectedComponent = document.querySelector(selector)
-    if (selectedComponent) {
-        selectedComponent.classList.add("hide")
-    }
-}
+// // Method: add 'hide' class to component
+// const setComponentHide = (selector) => {
+//     const selectedComponent = document.querySelector(selector)
+//     if (selectedComponent) {
+//         selectedComponent.classList.add("hide")
+//     }
+// }
 
-// Method: visit Page: Search with the query
-const onSearchQuery = (stringQuery) => {
-    if (stringQuery) {
-        console.log('query:', stringQuery)
-        window.location.href = `/search.sys?q=${encodeURIComponent(stringQuery)}`;
-    } else {
-        console.log('The query must be filled.',)
-    }
-}
+// // Method: visit Page: Search with the query
+// const onSearchQuery = (stringQuery) => {
+//     if (stringQuery) {
+//         console.log('query:', stringQuery)
+//         window.location.href = `/search.sys?q=${encodeURIComponent(stringQuery)}`;
+//     } else {
+//         console.log('The query must be filled.',)
+//     }
+// }
 
 
 document.addEventListener("DOMContentLoaded", () => {
     (async () => {
         if (window.location.hostname === "www.aw-indonesia.com") {
-            console.log("Hello Indonesia!")
+            console.log("Hello Indonesia! 1111111")
 
-            const luigiTrackerId = "483878-588294"
-            let listFieldsRemoved  // To remove data
-            let attributesList  // To show attribute shown in product list
-            let deviceType
+            // const luigiTrackerId = "483878-588294"
+            // let listFieldsRemoved  // To remove data
+            // let attributesList  // To show attribute shown in product list
+            // let deviceType
 
 
-            // Get the props
-            console.log('import meta url: ', import.meta?.url)
-            const scriptSrc = new URL(import.meta.url);
+            // // Get the props
+            // console.log('import meta url: ', import.meta?.url)
+            // const scriptSrc = new URL(import.meta.url);
 
-            // Read the parameter
-            if (scriptSrc?.searchParams) {
-                const color1 = scriptSrc.searchParams.get('color1');
-                const color2 = scriptSrc.searchParams.get('color2');
-                const color3 = scriptSrc.searchParams.get('color3');
-                const isLogin = scriptSrc.searchParams.get('logged_in')
-                deviceType = scriptSrc.searchParams.get('device_type')
+            // // Read the parameter
+            // if (scriptSrc?.searchParams) {
+            //     const color1 = scriptSrc.searchParams.get('color1');
+            //     const color2 = scriptSrc.searchParams.get('color2');
+            //     const color3 = scriptSrc.searchParams.get('color3');
+            //     const isLogin = scriptSrc.searchParams.get('logged_in')
+            //     deviceType = scriptSrc.searchParams.get('device_type')
 
-                listFieldsRemoved = stringToBoolean(isLogin) ? null : ['price', 'formatted_price', 'price_amount']                
-                attributesList = stringToBoolean(isLogin) ? ['product_code', 'formatted_price'] : ['product_code']                
+            //     listFieldsRemoved = stringToBoolean(isLogin) ? null : ['price', 'formatted_price', 'price_amount']                
+            //     attributesList = stringToBoolean(isLogin) ? ['product_code', 'formatted_price'] : ['product_code']                
         
-                console.log('Script paramter:', scriptSrc.searchParams);
-                console.log('list fields removed:', listFieldsRemoved)
+            //     console.log('Script paramter:', scriptSrc.searchParams);
+            //     console.log('list fields removed:', listFieldsRemoved)
         
-                // Apply these colors to some elements or use them in your logic
-                document.documentElement.style.setProperty('--luigiColor1', '#' + color1);
-                document.documentElement.style.setProperty('--luigiColor2', '#' + color2);
-                document.documentElement.style.setProperty('--luigiColor3', '#' + color3);
-            }
+            //     // Apply these colors to some elements or use them in your logic
+            //     document.documentElement.style.setProperty('--luigiColor1', '#' + color1);
+            //     document.documentElement.style.setProperty('--luigiColor2', '#' + color2);
+            //     document.documentElement.style.setProperty('--luigiColor3', '#' + color3);
+            // }
 
-            // Show: Luigi input autocomplete
-            const showInputAutocomplete = async () => {
-                setComponentHide('#header_search_input')
+            // // Show: Luigi input autocomplete
+            // const showInputAutocomplete = async () => {
+            //     setComponentHide('#header_search_input')
 
-                // Show: Input for autocomplete
-                const inputLuigi = document.getElementById("inputLuigi")
-                if (inputLuigi) {
-                    inputLuigi.classList.remove("hide")
-                } else {
-                    console.log('Element #inputLuigi is not exist.')
-                }
-            }
+            //     // Show: Input for autocomplete
+            //     const inputLuigi = document.getElementById("inputLuigi")
+            //     if (inputLuigi) {
+            //         inputLuigi.classList.remove("hide")
+            //     } else {
+            //         console.log('Element #inputLuigi is not exist.')
+            //     }
+            // }
 
 
-            if(deviceType === 'desktop') {
-                importStyleCSS()
-                showInputAutocomplete()
-                setComponentHide('#legacy_search')  // Hide original input in header
-                LBInitAutocomplete(luigiTrackerId, listFieldsRemoved, attributesList)
-                LBInitSearchResult(luigiTrackerId, listFieldsRemoved)
-            }
-            else if (deviceType === 'mobile') {
-                importStyleCSS()
-                LBInitAutocomplete(luigiTrackerId, listFieldsRemoved, attributesList)
+            // if(deviceType === 'desktop') {
+            //     importStyleCSS()
+            //     showInputAutocomplete()
+            //     setComponentHide('#legacy_search')  // Hide original input in header
+            //     LBInitAutocomplete(luigiTrackerId, listFieldsRemoved, attributesList)
+            //     LBInitSearchResult(luigiTrackerId, listFieldsRemoved)
+            // }
+            // else if (deviceType === 'mobile') {
+            //     importStyleCSS()
+            //     LBInitAutocomplete(luigiTrackerId, listFieldsRemoved, attributesList)
 
-                const groupInputLuigi = document.getElementById('groupInputLuigi')
-                if (groupInputLuigi) groupInputLuigi.style.display = 'flex'
+            //     const groupInputLuigi = document.getElementById('groupInputLuigi')
+            //     if (groupInputLuigi) groupInputLuigi.style.display = 'flex'
 
-                setComponentHide('.sidebar-left .menu-search')  // Hide original input in left sidebar
+            //     setComponentHide('.sidebar-left .menu-search')  // Hide original input in left sidebar
 
-                LBInitSearchResult(luigiTrackerId, listFieldsRemoved)
-                setComponentHide('.content .input-icon')  // Hide original input in Page: Result
-                setComponentHide('.page-content .content')
-            }
+            //     LBInitSearchResult(luigiTrackerId, listFieldsRemoved)
+            //     setComponentHide('.content .input-icon')  // Hide original input in Page: Result
+            //     setComponentHide('.page-content .content')
+            // }
 
         } else {
-            console.log("Hello world!!")
+            console.log("Hello world!! 11111")
         }
     })()
 })
