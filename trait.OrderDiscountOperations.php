372a650aa3e7564c354d78eaead655552a715b0c
Raul Perusquia <raul@inikoo.com>
2024-09-25 18:11:19 +0800

    GRA gold reward a for spain ES

--- trait.OrderDiscountOperations.php
+++ trait.OrderDiscountOperations.php
@@ -267,6 +267,11 @@ trait OrderDiscountOperations
 //                    $this->create_allowances_from_deal_component_data($deal_component_data);
 //                }else {
 
+                                if($this->data['Order Store Key'] != 3 and DNS_ACCOUNT_CODE=='ES'){
+                                    $this->deals['Order']['Terms'] = true;
+                                    $this->create_allowances_from_deal_component_data($deal_component_data);
+                                }else {
+
                     //print "$sql\n";
                     if ($result = $this->db->query($sql)) {
                         if ($_row = $result->fetch()) {
@@ -278,7 +283,7 @@ trait OrderDiscountOperations
                             }
                         }
                     }
-                //}
+                }
 
                 break;
 
