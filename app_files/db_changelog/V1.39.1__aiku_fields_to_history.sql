51fe918437b3362c4ffd1a939cff864ac7511bd3
Raul Perusquia <raul@inikoo.com>
2024-09-27 13:37:43 +0800

    add aiku fields to history

---
+++ app_files/db_changelog/V1.39.1__aiku_fields_to_history.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `History Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+ALTER TABLE `History Dimension` ADD `aiku_notes_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_notes_id`);
