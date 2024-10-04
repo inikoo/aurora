91a5ea17815c3f3bbb8c675b516b8d14d010f51a
Raul Perusquia <raul@inikoo.com>
2024-10-05 01:04:50 +0800

    add aiku_id to uploads

---
+++ app_files/db_changelog/V1.39.5__aiku_id_to_uploads.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Upload Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
+ALTER TABLE `Upload Record Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
