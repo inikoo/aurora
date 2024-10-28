8e4416d7a6af5a77fefaadaddee2faf77f6527be
Raul Perusquia <raul@inikoo.com>
2024-10-29 04:21:09 +0800

    captcha server side debug

--- EcomB2B/ar_web_register.php
+++ EcomB2B/ar_web_register.php
@@ -80,7 +80,31 @@ function register($db, $website, $data, $editor) {
 
         if ($website->settings('fu_secret') != '' ) {
 
-            print_r($raw_data);
+            if (empty($raw_data['cf-turnstile-response'])) {
+                echo json_encode(
+                    array(
+                        'state' => 400,
+                        'msg'   => (!empty($labels['_captcha_missing']) ? $labels['_captcha_missing'] : _('Please check on the reCAPTCHA box'))
+
+                    )
+                );
+                exit;
+            }
+
+            $ip = '';
+            if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+                $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+
+            } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+                $ip = $_SERVER['REMOTE_ADDR'];
+
+            }
+
+
+            $secretKey = $website->settings('fu_secret');
+
+
+            print_r($secretKey);
             exit;
         }
 
