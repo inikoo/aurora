1a7a384df273bba7115846ca8923fffbfca634ed
Raul Perusquia <raul@inikoo.com>
2024-10-28 12:57:38 +0800

    add video field to product

---
+++ app_files/db_changelog/V1.40.1__add_video_to_product.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Product Dimension` ADD `Product Video` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL ;
+ALTER TABLE `Product Dimension` ADD `Product Video Bis` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL ;
