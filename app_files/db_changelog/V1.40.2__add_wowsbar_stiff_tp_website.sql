90d4733ba279b404e79a4a8aee1e81e40988255e
Raul Perusquia <raul@inikoo.com>
2024-11-01 14:07:59 +0800

    add wowsbar cms to aurora db

---
+++ app_files/db_changelog/V1.40.2__add_wowsbar_stiff_tp_website.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `Website Dimension` ADD `wowsbar_footer` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL ;
+ALTER TABLE `Website Dimension` ADD `wowsbar_notification` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL ;
+ALTER TABLE `Website Dimension` ADD `wowsbar_header` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL ;
