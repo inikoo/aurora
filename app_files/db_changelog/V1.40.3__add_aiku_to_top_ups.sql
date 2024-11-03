e585a5c646a06cc33177eba24b9adff4f424d06b
Raul Perusquia <raul@inikoo.com>
2024-11-03 12:29:21 +0800

    add iku stuff to db

---
+++ app_files/db_changelog/V1.40.3__add_aiku_to_top_ups.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `Top Up Dimension` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
---
+++ app_files/db_changelog/V1.40.4__add_aiku_to_credit_transaction_facts.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `Credit Transaction Fact` ADD `aiku_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (`aiku_id`);
