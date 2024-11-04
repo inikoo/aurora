19692e6a0ee9485d2af6ebae919718b23c4c5f63
Raul Perusquia <raul@inikoo.com>
2024-11-04 22:08:51 +0800

    stop deal

---
+++ cron/update_deals_status_from_dates.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 23 May 2018 at 14:43:24 GMT+8, Kuala Lumpur, Malaysia
+ Copyright (c) 2018, Inikoo
+
+ Version 3
+
+*/
+
+require_once __DIR__.'/cron_common.php';
+require_once 'utils/natural_language.php';
+require_once 'utils/order_functions.php';
+
+
+
+
+
+$sql = sprintf("SELECT `Deal Key` FROM `Deal Dimension`   where  `Deal Expiration Date` is not null  and `Deal Status` not in ('Finished') ");
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+
+        print_r($row);
+
+        $deal = get_object('Deal', $row['Deal Key']);
+
+        $deal_campaign = get_object('DealCampaign', $deal->data['Deal Campaign Key']);
+
+
+        if(!in_array($deal_campaign->get('Code'),['OR','VL','CU'] )){
+            $deal_campaign->get('Code')."\n";
+           // $deal->update_status_from_dates(false);
+           // foreach ($deal->get_deal_components('objects', 'all') as $component) {
+           //     $component->update_status_from_dates();
+           // }
+        }
+
+
+
+
+
+
+    }
+
+} else {
+    print_r($error_info = $db->errorInfo());
+    exit;
+}
+
+
+$sql = sprintf("SELECT `Deal Cmponent Key` FROM `Deal Cmponent Dimension`  where  `Deal Cmponent Expiration Date` is not null  and `Deal Cmponent Status` not in ('Finished') ");
+if ($result = $db->query($sql)) {
+    foreach ($result as $row) {
+
+
+        print_r($row);
+
+        $dealComponent = get_object('DealComponent', $row['Deal Component Key']);
+
+        $deal_campaign = get_object('DealCampaign', $dealComponent->data['Deal Component Campaign Key']);
+
+
+        if(!in_array($deal_campaign->get('Code'),['OR','VL','CU'] )){
+            $deal_campaign->get('Code')." <dc \n";
+
+            //$dealComponent->update_status_from_dates();
+        }
+
+    }
+
+}
+
+
