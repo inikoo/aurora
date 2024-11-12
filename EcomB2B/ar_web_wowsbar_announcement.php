8909da508c3febdae6fefff3d1f7e3e181d8d87e
aqordeon <aqordivika@yahoo.co.id>
2024-11-12 15:33:12 +0800

    feat: AR: Wowsbar Announcement

---
+++ EcomB2B/ar_web_wowsbar_announcement.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 30 June 2017 at 13:15:34 GMT+8, Cyberjaya, Malaysia
+ Copyright (c) 2017, Inikoo
+
+ Version 3
+
+*/
+
+$url = $_REQUEST['url'];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'content-type: application/json',
+    'Accept: application/json',
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
