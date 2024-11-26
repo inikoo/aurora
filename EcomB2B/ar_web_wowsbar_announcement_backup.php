39d9104d0aebef016a0168cf5b3340ba5f682b5f
Raul Perusquia <raul@inikoo.com>
2024-11-26 16:02:25 +0800

    top notify test

---
+++ EcomB2B/ar_web_wowsbar_announcement_backup.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ About:
+ Author: Raul Perusquia <raul@inikoo.com>
+ Created: 30 June 2017 at 13:15:34 GMT+8, Cyberjaya, Malaysia
+ Copyright (c) 2017, Inikoo
+
+ Version 3
+
+*/
+require_once '../vendor/autoload.php';
+require 'keyring/dns.php';
+require 'keyring/au_deploy_conf.php';
+
+$redis = new Redis();
+$redis->connect(REDIS_HOST, REDIS_READ_ONLY_PORT);
+
+if(empty($_REQUEST['url_KHj321Tu'])){
+    echo 'error A';
+    exit;
+}
+
+$url = $_REQUEST['url_KHj321Tu'];
+
+$curl = curl_init();
+
+$url_cache_key='wowsbar_announcement_'.$url;
+
+if ($redis->exists($url_cache_key)) {
+    $response = $redis->get($url_cache_key);
+    header('Content-type: application/json');
+    echo $response;
+    exit;
+}
+
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'content-type: application/json',
+    'Accept: application/json',
+  ),
+));
+
+$response = curl_exec($curl);
+$redis->set(
+    $url_cache_key,
+    $response,
+    [
+        'ex'=>60
+    ]);
+
+
+curl_close($curl);
+header('Content-type: application/json');
+echo $response;
