7b2cfea651c6f0ed077e01a3351f5fa788dc9d4b
Raul Perusquia <raul@inikoo.com>
2024-12-04 17:20:33 +0800

    GPSR fields

---
+++ app_files/db_changelog/V1.41.6__gpr_fields_in_product.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `Product Dimension`
+    ADD COLUMN `Product GPSR Manufacturer` text NULL,
+ADD COLUMN `Product GPSR EU Responsable` text NULL,
+ADD COLUMN `Product GPSR Warnings` text NULL,
+ADD COLUMN `Product GPSR Manual` text NULL,
+ADD COLUMN `Product GPSR Class Category Danger` text NULL,
+ADD COLUMN `Product GPSR Languages` text NULL;
