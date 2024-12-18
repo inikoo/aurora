89005eafa9ab15426ad324d5ad865df5d7c19159
Raul Perusquia <raul@inikoo.com>
2024-12-18 18:40:22 +0800

    test edit pictograms on part part 3

--- js/edit.js
+++ js/edit.js
@@ -2858,6 +2858,195 @@ function select_dropdown_handler(type, element) {
 
 }
 
+function save_toggle_switch_part(element){
+
+    var icon=$(element).find('i')
+
+    if(icon.hasClass('fa-toggle-on')){
+        var value='No'
+    }else if(icon.hasClass('fa-toggle-off')){
+        var value='Yes'
+    }else{
+
+        return
+    }
+    icon.removeClass('fa-toggle-on fa-toggle-off').addClass(' fa-spinner fa-spin')
+
+
+
+
+    var ajaxData = new FormData();
+
+    ajaxData.append("tipo", 'edit_field')
+    ajaxData.append("object", 'Part')
+
+    ajaxData.append("key", $('#fields').attr('key'))
+    ajaxData.append("field", $(element).attr('field'))
+
+    ajaxData.append("value", value)
+
+
+    $.ajax({
+               url: "/ar_edit.php", type: 'POST', data: ajaxData, dataType: 'json', cache: false, contentType: false, processData: false,
+               complete: function () {
+               }, success: function (data) {
+
+            if (data.state == '200') {
+
+
+                if(value=='Yes'){
+                    icon.addClass('fa-toggle-on').removeClass(' fa-spinner fa-spin')
+                    icon.next('span').removeClass('discreet')
+                    $('.'+$(element).attr('field')).removeClass('error discreet')
+
+                }else{
+                    icon.addClass('fa-toggle-off').removeClass(' fa-spinner fa-spin')
+                    icon.next('span').addClass('discreet')
+                    $('.'+$(element).attr('field')).addClass('error discreet')
+
+                }
+
+                if (data.other_fields) {
+                    for (var key in data.other_fields) {
+
+                        //   console.log(data.other_fields[key])
+
+                        update_field(data.other_fields[key])
+                    }
+                }
+
+                if (data.deleted_fields) {
+                    for (var key in data.deleted_fields) {
+                        delete_field(data.deleted_fields[key])
+                    }
+                }
+
+                for (var key in data.update_metadata.class_html) {
+                    $('.' + key).html(data.update_metadata.class_html[key])
+                }
+
+
+                for (var key in data.update_metadata.hide) {
+                    $('.' + data.update_metadata.hide[key]).addClass('hide')
+                }
+
+                for (var key in data.update_metadata.show) {
+
+                    $('.' + data.update_metadata.show[key]).removeClass('hide')
+                }
+
+                //  $('#save_button', window.parent.document).removeClass('save').find('i').removeClass('fa-spinner fa-spin')
+
+            } else if (data.state == '400') {
+                swal({
+                         title: data.title, text: data.msg, confirmButtonText: "OK"
+                     });
+            }
+
+
+
+        }, error: function () {
+
+        }
+           });
+
+
+}
+
+function save_toggle_switch_product(element){
+
+    var icon=$(element).find('i')
+
+    if(icon.hasClass('fa-toggle-on')){
+        var value='No'
+    }else if(icon.hasClass('fa-toggle-off')){
+        var value='Yes'
+    }else{
+
+        return
+    }
+    icon.removeClass('fa-toggle-on fa-toggle-off').addClass(' fa-spinner fa-spin')
+
+
+
+
+    var ajaxData = new FormData();
+
+    ajaxData.append("tipo", 'edit_field')
+    ajaxData.append("object", 'Product')
+
+    ajaxData.append("key", $('#fields').attr('key'))
+    ajaxData.append("field", $(element).attr('field'))
+
+    ajaxData.append("value", value)
+
+
+    $.ajax({
+               url: "/ar_edit.php", type: 'POST', data: ajaxData, dataType: 'json', cache: false, contentType: false, processData: false,
+               complete: function () {
+               }, success: function (data) {
+
+            if (data.state == '200') {
+
+
+                if(value=='Yes'){
+                    icon.addClass('fa-toggle-on').removeClass(' fa-spinner fa-spin')
+                    icon.next('span').removeClass('discreet')
+                    $('.'+$(element).attr('field')).removeClass('error discreet')
+
+                }else{
+                    icon.addClass('fa-toggle-off').removeClass(' fa-spinner fa-spin')
+                    icon.next('span').addClass('discreet')
+                    $('.'+$(element).attr('field')).addClass('error discreet')
+
+                }
+
+                if (data.other_fields) {
+                    for (var key in data.other_fields) {
+
+                        //   console.log(data.other_fields[key])
+
+                        update_field(data.other_fields[key])
+                    }
+                }
+
+                if (data.deleted_fields) {
+                    for (var key in data.deleted_fields) {
+                        delete_field(data.deleted_fields[key])
+                    }
+                }
+
+                for (var key in data.update_metadata.class_html) {
+                    $('.' + key).html(data.update_metadata.class_html[key])
+                }
+
+
+                for (var key in data.update_metadata.hide) {
+                    $('.' + data.update_metadata.hide[key]).addClass('hide')
+                }
+
+                for (var key in data.update_metadata.show) {
+
+                    $('.' + data.update_metadata.show[key]).removeClass('hide')
+                }
+
+                //  $('#save_button', window.parent.document).removeClass('save').find('i').removeClass('fa-spinner fa-spin')
+
+            } else if (data.state == '400') {
+                swal({
+                         title: data.title, text: data.msg, confirmButtonText: "OK"
+                     });
+            }
+
+
+
+        }, error: function () {
+
+        }
+           });
+
+
+}
 
 function save_toggle_switch(element){
 
