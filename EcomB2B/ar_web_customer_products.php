5a82b3a8a9b953da2a17d6ba23c8d28cce067dc0
Raul Perusquia <raul@inikoo.com>
2025-01-13 20:24:48 +0800

    remove GR

--- EcomB2B/ar_web_customer_products.php
+++ EcomB2B/ar_web_customer_products.php
@@ -344,15 +344,15 @@ function category_products($data, $db, $customer_key, $order)
         }
 
 
-        if(    !in_array( $store->id,[18,22])   and DNS_ACCOUNT_CODE=='AWEU'){
-            $gr_member = true;
-        }
-        if(    $store->id != 3 and DNS_ACCOUNT_CODE=='ES'){
-            $gr_member = true;
-        }
-        if($store->id==1 and DNS_ACCOUNT_CODE=='AW'){
-            $gr_member = true;
-        }
+//        if(    !in_array( $store->id,[18,22])   and DNS_ACCOUNT_CODE=='AWEU'){
+//            $gr_member = true;
+//        }
+//        if(    $store->id != 3 and DNS_ACCOUNT_CODE=='ES'){
+//            $gr_member = true;
+//        }
+//        if($store->id==1 and DNS_ACCOUNT_CODE=='AW'){
+//            $gr_member = true;
+//        }
 
 
         if ($gr_member and count($families) > 0) {
