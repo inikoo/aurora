28ceaca756c42c202caf6915c8b9efea3d9fcfe3
Raul Perusquia <raul@inikoo.com>
2025-09-26 11:30:41 +0800

    Update `api_unsubscribe.php` to enhance unsubscription handling and include authentication via `WebAuth`.

--- api_unsubscribe.php
+++ api_unsubscribe.php
@@ -1,14 +1,23 @@
 <?php
 
 
+$website_key = $_REQUEST['website_key'];
 
-$s=$_REQUEST['s'];
-$a=$_REQUEST['a'];
+
+include_once('class.WebAuth.php');
+$auth = new WebAuth($db);
+
+$website = get_object('Website', $website_key);
+
+
+list($unsubscribe_subject_type, $unsubscribe_subject_key) = $auth->get_customer_from_unsubscribe_link(($_REQUEST['s'] ?? ''), ($_REQUEST['a'] ?? ''));
 
 
 $response = array(
-    'state'   => 'Unsubscribed',
-    'msg'     => 'test',
+    'unsubscribe_subject_type' => $unsubscribe_subject_type,
+    'unsubscribe_subject_key'  => $unsubscribe_subject_key,
+    'state'                    => 'Unsubscribed',
+    'msg'                      => 'test',
 );
 echo json_encode($response);
 exit;
