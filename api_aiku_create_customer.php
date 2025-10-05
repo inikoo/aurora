21c58165b514db8c4c7833cb236e6521c9c9f105
Raul Perusquia <raul@inikoo.com>
2025-10-06 02:37:21 +0800

    Add newsletter and email marketing preferences to customer data, refactor address fields, and implement cryptographically secure random string generation.

--- api_aiku_create_customer.php
+++ api_aiku_create_customer.php
@@ -7,12 +7,14 @@ $store = get_object('Store', $shop_key);
 
 
 $data = array(
-    'Customer Main Contact Name'   => $_REQUEST['contact_name'],
-    'Customer Company Name'        => $_REQUEST['company_name'],
-    'Customer Registration Number' => $_REQUEST['identity_document_number'],
-    'Customer Tax Number'          => $_REQUEST['tax_number'],
-    'Customer Main Plain Email'    => $_REQUEST['email'],
-    'Customer Main Plain Mobile'   => $_REQUEST['phone'],
+    'Customer Main Contact Name'    => $_REQUEST['contact_name'],
+    'Customer Company Name'         => $_REQUEST['company_name'],
+    'Customer Registration Number'  => $_REQUEST['identity_document_number'],
+    'Customer Tax Number'           => $_REQUEST['tax_number'],
+    'Customer Main Plain Email'     => $_REQUEST['email'],
+    'Customer Main Plain Mobile'    => $_REQUEST['phone'],
+    'Customer Send Newsletter'      => $_REQUEST['send_newsletter'] ? 'Yes' : 'No',
+    'Customer Send Email Marketing' => $_REQUEST['send_marketing'] ? 'Yes' : 'No',
 
 );
 
@@ -22,29 +24,38 @@ $data['Customer Store Key']             = $store->id;
 $data['Customer Billing Address Link']  = 'Contact';
 $data['Customer Delivery Address Link'] = 'Billing';
 
-$address_fields = array(
-    'Address Recipient'            => $_REQUEST['contact_name'],
-    'Address Organization'         => $_REQUEST['company_name'],
-    'Address Line 1'               => $_REQUEST['address_line_1'],
-    'Address Line 2'               => $_REQUEST['address_line_2'],
-    'Address Sorting Code'         => $_REQUEST['sorting_code'],
-    'Address Postal Code'          => $_REQUEST['postal_code'],
-    'Address Dependent Locality'   => $_REQUEST['dependent_locality'],
-    'Address Locality'             => $_REQUEST['locality'],
-    'Address Administrative Area'  => $_REQUEST['administrative_area'],
-    'Address Country 2 Alpha Code' => $_REQUEST['country_code'],
 
-);
+$data['Customer Contact Address locality']           = $_REQUEST['locality'];
+$data['Customer Contact Address postalCode']         = $_REQUEST['postal_code'];
+$data['Customer Contact Address addressLine1']       = $_REQUEST['address_line_1'];
+$data['Customer Contact Address addressLine2']       = $_REQUEST['address_line_2'];
+$data['Customer Contact Address administrativeArea'] = $_REQUEST['administrative_area'];
+$data['Customer Contact Address dependentLocality']  = $_REQUEST['dependent_locality'];
+$data['Customer Contact Address sortingCode']        = $_REQUEST['sorting_code'];
+$data['Customer Contact Address country']            = $_REQUEST['country_code'];
+
+
+//list($customer, $website_user) = $store->create_customer($data, array('Website User Password' => random_string_a(16)));
 
 
 $response = array(
     'customer_data' => $data,
-    'address_data'  => $address_fields,
-
+    'pwd'           => random_string_a(16)
 
 );
 echo json_encode($response);
 exit;
 
+function random_string_a(int $length = 32, string $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'): string
+{
+    $result   = '';
+    $maxIndex = strlen($alphabet) - 1;
+    for ($i = 0; $i < $length; $i++) {
+        $idx    = random_int(0, $maxIndex); // cryptographically secure
+        $result .= $alphabet[$idx];
+    }
+
+    return $result;
+}
 
 //$customer = new Public_Customer('new', $data, $address_fields);
