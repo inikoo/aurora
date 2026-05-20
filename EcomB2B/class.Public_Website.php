625502dfece9cce41b17034718ac65cc40755415
Raul Perusquia <raul@inikoo.com>
2026-05-20 11:08:17 +0800

    Add `success_url` and `failure_url` to payment payload construction

--- EcomB2B/class.Public_Website.php
+++ EcomB2B/class.Public_Website.php
@@ -398,7 +398,9 @@ class Public_Website
                         'id'                     => $response['id'],
                         'payment_session_secret' => $response['payment_session_secret'],
                         'payment_session_token'  => $response['payment_session_token'],
-                        'public_key'             => $payment_account->get('Payment Account Login')
+                        'public_key'             => $payment_account->get('Payment Account Login'),
+                        "success_url"           => $success_url,
+                        "failure_url"           => $failure_url,
                     ];
 
 
